1d1eecaef4440009fea431460c88ee0cbfea19fd
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-05-18 13:49:14 +0300

    Update get-buzzpatch.js

--- buzzpatch/get-buzzpatch.js
+++ buzzpatch/get-buzzpatch.js
@@ -157,3 +157,19 @@ window.onload  = function () {
         $('html,body').animate({scrollTop:$('#purchase').offset().top + 10 + "px"},{duration:1E3});
     });
 };
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+  'event': 'event-to-ga',
+  'eventCategory': 'Exp — Spray vs Patches',
+  'eventAction': 'loaded'
+});
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1271698,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
+    hj('trigger', 'spray_vs_patches');
