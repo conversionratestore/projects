4ef86c502a4d6c084395ff381f67b065cde51fa6
conversionratestore <82801260+conversionratestore@users.noreply.github.com>
2021-05-20 18:54:20 +0300

    Update script-2.js

--- signaturely/script-2.js
+++ signaturely/script-2.js
@@ -12,5 +12,33 @@ let build = setInterval(function() {
         </style>`;
         document.body.insertAdjacentHTML('afterbegin', style);
         document.querySelector('.elementor-button-content-wrapper .elementor-button-text').innerHTML = 'Sign 3 documents for FREE >';
+
+        document.querySelector('.elementor-button-wrapper').addEventListener('click', function () {
+            window.dataLayer = window.dataLayer || [];
+            dataLayer.push({
+                'event': 'event-to-ga',
+                'eventCategory': 'Exp — New CTA in header',
+                'eventAction': 'click on button Sign 3 Documents for Free',
+                'eventLabel': 'Header'
+            });
+        })
     }
 }, 100);
+
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:2078786,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
+hj('trigger', 'new_cta_in_header');
+
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp — New CTA in header',
+    'eventAction': 'loaded'
+});
