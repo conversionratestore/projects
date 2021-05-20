f84b45729b736afe2aad055e52c36c17a8b53867
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-05-20 12:11:59 +0300

    Update script-2.js

--- signaturely/script-2.js
+++ signaturely/script-2.js
@@ -1,12 +1,16 @@
-window.onload  = function () {
-let style = `
-<style>
-    .elementor-36 .elementor-element.elementor-element-2d03d92 {
-        width: 100%;}
-    .elementor-36 .elementor-element.elementor-element-2325564 .elementor-button {
-        font-size: 14px;
-        padding: 14px 22px;}
-</style>`;
-document.body.insertAdjacentHTML('afterbegin', style);
-document.querySelector('.elementor-button-content-wrapper .elementor-button-text').innerHTML = 'Sign 3 documents for FREE >';
-};
+let build = setInterval(function() {
+    console.log("s");
+    if(document.querySelector('.elementor-button-content-wrapper .elementor-button-text')) {
+        clearInterval(build);
+        let style = `
+        <style>
+            .elementor-36 .elementor-element.elementor-element-2d03d92 {
+                width: 100%;}
+            .elementor-36 .elementor-element.elementor-element-2325564 .elementor-button {
+                font-size: 14px;
+                padding: 14px 22px;}
+        </style>`;
+        document.body.insertAdjacentHTML('afterbegin', style);
+        document.querySelector('.elementor-button-content-wrapper .elementor-button-text').innerHTML = 'Sign 3 documents for FREE >';
+    }
+}, 100);
