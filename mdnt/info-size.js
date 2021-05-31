be9f10e9be9fdf0201000e186d0c96e9268c8b75
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2021-05-31 16:53:48 +0300

    Update info-size.js

--- mdnt/info-size.js
+++ mdnt/info-size.js
@@ -1,73 +1,98 @@
-document.head.insertAdjacentHTML(
-  'beforeend',
-  `
+window.onload = function () {
+  document.head.insertAdjacentHTML(
+    'beforeend',
+    `
     <style>
         .product-single__info-size {
-            margin: 25px 0 15px 0;
-            background: #FFFFFF;
-            border: 1px solid #C00303;
-            text-align: left;
+          margin: 25px 0 15px 0;
+          background: #FFFFFF;
+          border: 1px solid #C00303;
+          text-align: left;
         }
         .info-size__heading-wrapper {
-            background: #C00303;
+          background: #C00303;
         }
         .info-size__heading-wrapper h4{
-            margin: 0;
-            color: #FFFFFF;
-            letter-spacing: normal;
-            font-size: 16px;
-            text-transform: lowercase;
+          margin: 0;
+          color: #FFFFFF;
+          letter-spacing: normal;
+          font-size: 16px;
+          text-transform: lowercase;
         }
         .info-size__heading-wrapper, .info-size__text-wrapper {
-            padding: 12px 15px;
+          padding: 12px 15px;
         }
         .info-size__text-wrapper p {
-            margin: 0;
+          margin: 0;
         }
         p.info-size__text--small {
-            margin-top: 10px;
-            font-size: 12px;
+          margin-top: 10px;
+          font-size: 12px;            
+        }
+        p.info-size__text--small::first-letter {
+          text-transform: uppercase;
         }
         .content.sc-content-blaster {
-            padding: 10px 15px;
+          padding: 10px 15px;
         }
         .content.sc-content-blaster .product-single__info-size {
-            margin: 0;
+          margin: 0;
         }
         .content.sc-content-blaster .info-size__heading-wrapper, .info-size__text-wrapper {
-            padding: 12px;
+          padding: 12px;
         }
     </style>
-`
-);
-
-document
-  .querySelector(
-    '.variant-wrapper.variant-wrapper--dropdown.blaster-select--Size.js'
-  )
-  .insertAdjacentHTML(
-    'afterEnd',
-    `
-        <div class='product-single__info-size'>
-            <div class='info-size__heading-wrapper'>
-                <h4>90% users say this product is true to size.</h4>
-            </div>
-            <div class='info-size__text-wrapper'>
-                <p><strong>Model wears: </strong>M (160-170 Centimeters)</strong></p>
-                <p>Model’s height is 175cm / 68,9″  </p>
-                <p class='info-size__text--small'>Size up if you are between sizes.</p>
-            <div>
-        <div>
-    `
+  `
   );
 
-document.querySelector('.content.sc-content-blaster').insertAdjacentHTML(
-  'afterBegin',
-  `
+  fetch(
+    `https://shopify.blaster.ai/api/item-model-info?handle=${
+      location.href.split('products/')[1].split('?')[0]
+    }`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      template(data);
+    });
+
+  function template(item) {
+    document
+      .querySelector(
+        '.variant-wrapper.variant-wrapper--dropdown.blaster-select--Size.js'
+      )
+      .insertAdjacentHTML(
+        'afterEnd',
+        `
+        <div class='product-single__info-size'>
+            <div class='info-size__heading-wrapper'>
+                <h4>90% users say this product is ${item['designed-fit']}.</h4>
+            </div>
+            <div class='info-size__text-wrapper'>
+                <p><strong>Model wears: </strong>${item['item-size']} ${
+          item['item-length-cm']
+            ? `(${item['item-length-cm']}) Centimeters`
+            : ''
+        }</p>
+              <p>Model’s height is ${item['model-height-cm']}cm / ${
+          item['model-height-ft']
+        }</p>
+                <p class='info-size__text--small'>${
+                  item['in-between-sizes']
+                } if you are between sizes.</p>
+            <div>
+        <div>
+      `
+      );
+
+    document.querySelector('.content.sc-content-blaster').insertAdjacentHTML(
+      'afterBegin',
+      `
     <div class='product-single__info-size'>
         <div class='info-size__heading-wrapper'>
-            <h4>90% users say this product is true to size.</h4>
+            <h4>90% users say this product is ${item['designed-fit']}.</h4>
         </div>
     </div>
-  `
-);
+    `
+    );
+  }
+};
