4e37b6bc84f25912474ca2a8192408c2072789b8
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-03 13:31:45 +0300

    Update main.js

--- ahr/js/main.js
+++ ahr/js/main.js
@@ -1,13 +1,20 @@
-"use strict";
-
-document.querySelectorAll('.accordion_item').forEach(function (element) {
-  element.addEventListener('click', function () {
-    element.parentElement.classList.toggle('active');
-  });
+document.querySelectorAll('.accordion_item').forEach(element => {
+    element.addEventListener('click', () => {
+        element.parentElement.classList.toggle('active');
+    });
 });
-document.querySelectorAll('.btn').forEach(function (element) {
-  element.addEventListener('click', function (e) {
-    e.preventDefault();
-    console.log('s');
-  });
+document.querySelector('.burger').addEventListener('click', (e) => {
+    document.querySelector('.burger').classList.toggle('active');
+    document.querySelector('.navtop').classList.toggle('active');
+});
+document.querySelector('.form1 .btn').addEventListener('click', (e) => {
+    e.stopImmediatePropagation();
+    document.querySelector('.form1').classList.remove('active');
+    document.querySelector('.form2').classList.add('active');
+});
+document.querySelectorAll('.btn').forEach(element => {
+    element.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('.s-sixth').scrollIntoView({ behavior: 'smooth' })
+    });
 });
