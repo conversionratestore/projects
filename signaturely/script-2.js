f7cc3df4163953aac84c233c17c87e44b0172eff
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-03 14:58:02 +0300

    Update script-2.js

--- signaturely/script-2.js
+++ signaturely/script-2.js
@@ -26,7 +26,7 @@ let build = setInterval(function() {
             });
         })
     }
-}, 50);
+}, 100);
 
 (function(h,o,t,j,a,r){
     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
