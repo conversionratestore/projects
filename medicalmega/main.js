132d93224b9cbcdee1633b3a4d98a86b28b4d72c
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-07 14:07:34 +0300

    Update main.js

--- medicalmega/main.js
+++ medicalmega/main.js
@@ -686,11 +686,9 @@ window.onload  = function () {
                 
                 fetch('/cart.html')
                 .then((response) => {
+                    console.log(response);
                     return response.json();
                 })
-                .then((data) => {
-                    console.log(data);
-                });
 
             fetch('/cart.html', {
                 headers: {
