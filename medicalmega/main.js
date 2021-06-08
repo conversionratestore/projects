57a810a18d2813f640f2ca5e338d64e08adbdbe3
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-08 17:57:18 +0300

    Update main.js

--- medicalmega/main.js
+++ medicalmega/main.js
@@ -618,7 +618,7 @@ window.onload  = function () {
                     for (const key in productsStoredTemporarily) {
                         console.log(productsStoredTemporarily[key]);
                         if (productsStoredTemporarily[key].product_id != '' || productsStoredTemporarily[key].product_id != undefined || productsStoredTemporarily[key].product_id != null) {
-                            console.log('productsStoredTemporarily[key].product_id != ''');
+                            console.log('productsStoredTemporarily[key].product_id !=');
                             if (productsStoredTemporarily[key].product_id == productId) {
                                 productsStoredTemporarily[key].quantity = +item.nextElementSibling.value;
                                 console.log(productsStoredTemporarily[key].quantity);
