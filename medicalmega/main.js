e511d34aa84f122813413c1a4e4c72996c117a2b
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-08 18:18:14 +0300

    Update main.js

--- medicalmega/main.js
+++ medicalmega/main.js
@@ -621,7 +621,7 @@ window.onload  = function () {
                             if (productsStoredTemporarily[key].product_id == productId) {
                                 productsStoredTemporarily[key].quantity = item.nextElementSibling.value;
                                 console.log(productsStoredTemporarily[key].quantity);
-                            } else {
+                            } else if (productsStoredTemporarily[key].product_id != undefined && productsStoredTemporarily[key].product_id != productId) {
                                 productsStoredTemporarily.push({
                                     'product_id': productId,
                                     'quantity': item.nextElementSibling.value,
