4445a5ed3e5dddb7dafae85805306f71ef670550
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-09 17:23:38 +0300

    Update main.js

--- medicalmega/main.js
+++ medicalmega/main.js
@@ -772,14 +772,16 @@ window.onload  = function () {
 
     }
 
-    document.querySelector('.popup').addEventListener('change', () => {
+    document.querySelector('.popup .body').addEventListener('change', () => {
         document.querySelectorAll('.popup__product').forEach(el => {
             if (localStorage.getItem('productsStoredUpdate') != null && localStorage.getItem('productsStoredUpdate') != '') {
                 productsStoredUpdate = JSON.parse(localStorage.getItem('productsStoredUpdate'));
                 for (let i = 0; i < productsStoredUpdate.length; i++) {
-                    if (el.getAttribute('data-product-id') === productsStoredUpdate[i].product_id) {
-                        productsStoredUpdate[i].quantity = el.querySelector('.quantity').value;
-                        console.log(productsStoredUpdate[i].quantity + ' = ' + el.querySelector('.quantity').value + ' quantity productsStoredUpdate');
+                    if (el.getAttribute('data-product-id') == productsStoredUpdate[i].product_id) {
+                        productsStoredUpdate[i].push({
+                            'quantity': el.querySelector('.quantity').value;
+                        });
+                        console.log(productsStoredUpdate[i].quantity + ' = ' + el.querySelector('.quantity').value + ' пренадлежит Id ' + productsStoredUpdate[i].product_id);
                     } else {
                         productsStoredUpdate.push({
                             'product_id': el.getAttribute('data-product-id'),
@@ -801,6 +803,6 @@ window.onload  = function () {
                 localStorage.setItem('productsStoredUpdate', JSON.stringify(productsStoredUpdate));
                 console.log(' else last');
             }
-        }); 
+        });
     });
 };
