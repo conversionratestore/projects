81eed8809256745954e80ccf75cf37b4b22dd4ac
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-09 19:47:33 +0300

    Update script-c.js

--- signaturely/script-c.js
+++ signaturely/script-c.js
@@ -107,12 +107,14 @@ let mut = new MutationObserver(function (muts) {
 
             document.querySelector(".elementor-83 .elementor-element.elementor-element-62dcef9 .elementor-heading-title").innerHTML = `Need more?`;
             document.querySelector('.elementor-83 .elementor-element.elementor-element-62dcef9 .elementor-heading-title').insertAdjacentHTML('afterend', '<p class="title-span">Check other options</p>');
-            document.querySelector('.b-free .elementor-form').setAttribute('method','');
-            document.querySelector('.b-free .e-form__buttons').addEventListener('click', (e) => {
-                e.preventDefault();
-                let val = document.querySelector('.b-free input.elementor-field').value.replace('@','%40');
-                window.location = `https://app.signaturely.com/signup/?email=${val}`;
-            });
+              if (document.querySelector('.b-free .elementor-form')) {
+                document.querySelector('.b-free .elementor-form').setAttribute('method','');
+                document.querySelector('.b-free .e-form__buttons').addEventListener('click', (e) => {
+                    e.preventDefault();
+                    let val = document.querySelector('.b-free input.elementor-field').value.replace('@','%40');
+                    window.location = `https://app.signaturely.com/signup/?email=${val}`;
+                });
+            }
  
         }
 
