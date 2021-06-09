efddeb5ba2938b3f96e6907c6285aa7aaee02830
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-09 19:49:49 +0300

    Update script-c.js

--- signaturely/script-c.js
+++ signaturely/script-c.js
@@ -109,8 +109,8 @@ let mut = new MutationObserver(function (muts) {
             document.querySelector('.elementor-83 .elementor-element.elementor-element-62dcef9 .elementor-heading-title').insertAdjacentHTML('afterend', '<p class="title-span">Check other options</p>');
             if (document.querySelector('.b-free .elementor-form')) {
                 document.querySelector('.b-free .elementor-form').setAttribute('method','');
-                document.querySelector('.b-free #button-get').setAttribute('type','button');
-                document.querySelector('.b-free .e-form__buttons').addEventListener('click', (e) => {
+                document.querySelector('.b-free .elementor-button').setAttribute('type','button');
+                document.querySelector('.b-free .elementor-button').addEventListener('click', (e) => {
                     e.preventDefault();
                     let val = document.querySelector('.b-free input.elementor-field').value.replace('@','%40');
                     window.location = `https://app.signaturely.com/signup/?email=${val}`;
