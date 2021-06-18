80181b071a1c63e0649d58c3251eed6b24a30573
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-18 10:55:32 +0300

    Update pollMagicPatch.js

--- buzzpatch/pollMagicPatch.js
+++ buzzpatch/pollMagicPatch.js
@@ -2,8 +2,6 @@ window.onload  = function () {
     $('body').prepend(`
     <style>
     @media (max-width: 767px) {
-        .js-main header {
-            min-height: 750px;}
         #returns .conditions .d-flex {
             padding: 10px 12px; }
         #returns .js-light-border {
