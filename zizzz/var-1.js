a1df06c5e365edb605395e6a4776d53fe78db222
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-18 14:53:59 +0300

    Create var-1.js

---
+++ zizzz/var-1.js
@@ -0,0 +1,11 @@
+$('body').prepend(`
+<style>
+    .banner {
+        margin-top: 10px;
+        width: 100%;
+        height: 111px;
+        background: no-repeat center / cover;}
+</style>`);
+
+$('#mcetoc_1evub102g0').before(`<a href="#" class="banner" style="background-image: url(https://conversionratestore.github.io/projects/buzzpatch/img/banner.jpg)"></a>`);
+$('#mcetoc_1evub102g5').before(`<a href="#" class="banner" style="background-image: url(https://conversionratestore.github.io/projects/buzzpatch/img/banner2.jpg)"></a>`);
