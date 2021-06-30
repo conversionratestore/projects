4cdb907af8365101ad3dab7a599b315157fa7982
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-30 18:49:05 +0300

    Update main.js

--- medicalmega/main.js
+++ medicalmega/main.js
@@ -756,19 +756,9 @@ window.onload  = function () {
                         </form>
                         <div class="add-to-cart" ${!doc.querySelector('.type2 .product-price') ? 'disabled' : ''}><button type="button">add to cart</button><input type="number" value="1"></div>
                     </dd>`);
-                    if (document.querySelectorAll('.popup .add-to-cart button') && i === 0) {
+                    if (i === 0) {
                         addToCart()
                     }
-                    console.log( document.querySelectorAll('.popup .add-to-cart button'));
-         
-
-                    document.querySelectorAll('.add-to-cart').forEach( (item) => {
-                        item.addEventListener('change', () => {
-                            if (item.querySelector('input').value <= 1) {
-                                item.querySelector('input').value = 1;
-                            }
-                        });  
-                    });
                 }
             }
             http.send(null);
