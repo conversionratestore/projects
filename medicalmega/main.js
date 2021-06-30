8196b03dac276657ba2d814f5934ae903480d14b
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-06-30 12:45:06 +0300

    Update main.js

--- medicalmega/main.js
+++ medicalmega/main.js
@@ -702,9 +702,6 @@ window.onload  = function () {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
             },
-            scrollbar: {
-                el: '.swiper-scrollbar',
-            },
         });
     }, 1000);
 
