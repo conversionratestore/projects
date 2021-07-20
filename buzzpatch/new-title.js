cbaf4ade0462d5f9df2b43405ba5cf30a5c3b24f
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-07-20 16:41:08 +0300

    Update new-title.js

--- buzzpatch/new-title.js
+++ buzzpatch/new-title.js
@@ -26,3 +26,19 @@ window.onload  = function () {
     document.querySelector('.js-iphone .js-heading.js-mobile h1').innerHTML = `Keep mosquitoes away `;
     document.querySelector('.js-iphone .js-heading.js-mobile h1').insertAdjacentHTML('afterend',`<p class="title-span">For up to 72 hours with natural anti-mosquitoes patches</p>`)
 };
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:2247058,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
+hj('trigger', 'homepage_new_title');
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp — Homepage new title mobile',
+    'eventAction': 'loaded'
+});
