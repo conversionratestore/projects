45d10c2687535001ad3cd8dc42f936d278550771
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-07-23 16:47:30 +0300

    Add files via upload

--- privatefloor/js/checkout.js
+++ privatefloor/js/checkout.js
@@ -1,21 +1,47 @@
+$(document).ready(function () {
+    $('.checkout-head .toggle').on('click', function() {
+        $('.registerPanel').toggleClass('active');
+        $('.order-recipient').toggleClass('active');
+    });
 
-$('.checkout-head .toggle').on('click', function() {
-    $('.registerPanel').toggleClass('active');
-    $('.order-recipient').toggleClass('active');
-});
+    $('#order-recipient .checkout-footer .btn , .next-step2').on('click', function() {
+        if($('#order-recipient input').val() != '' && $('#order-recipient input').val() != isNaN) {
+            $('#order-recipient').css('display','none');
+            $('.express-checkout').css('display','none');
+            $('#shipping-address').css('display','block');
+            $('.cart-steps__main-block .step.step-active').removeClass('step-active');
+            $('.cart-steps__main-block .step').eq(1).removeClass('step-next').addClass('step-active');
+        }
+    });
 
-$('#order-recipient .checkout-footer .btn').on('click', function() {
-    if($('#order-recipient input').val() != '' && $('#order-recipient input').val() != isNaN) {
-        $('#order-recipient').css('display','none');
-        $('.express-checkout').css('display','none');
-        $('#shipping-address').css('display','block');
-        $('.cart-steps__main-block .step.step-active').removeClass('step-active');
-        $('.cart-steps__main-block .step').eq(1).removeClass('step-next').addClass('step-active');
+
+    $('.promocode-item').on('click', function () {
+        $(this).toggleClass('active');
+        $(this).siblings().slideToggle(300);
+    });
+
+    function showHide(item, itemActive) {
+        $(`${item}`).on('click', function() {
+            if ($(`${item} input`).prop('checked')) {
+                $(`${itemActive}`).addClass('active');
+            } else {
+                $(`${itemActive}`).removeClass('active');
+            }
+        });
+    }
+showHide('.ship_checkboxCompany', '.editAddressFormCart');
+showHide('.bill_checkboxCompany', '.bill_registerCompany');
+showHide('.bill_checkboxCompany', '.bill_eori');
+
+$('[data-button]').on('click', function() {
+    let _this = $(this);
+    let attr = $(this).data('button');
+    if(_this.find('input').prop('checked')) {
+        $(`[data-item='${attr}']`).addClass('active');
+    } else {
+        $(`[data-item='${attr}']`).removeClass('active');
     }
 });
 
-$('.promocode-item').on('click', function () {
-    $(this).toggleClass('active');
-    $(this).siblings().slideToggle(300);
 });
 
