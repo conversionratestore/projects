fa7ff0408ca88290859f8186b1170d0006ef8463
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-07-28 17:20:44 +0300

    Update var-2.js

--- jarrold/var-2.js
+++ jarrold/var-2.js
@@ -23,6 +23,7 @@ let mut = new MutationObserver(function (muts) {
                     basketList = [];
                     localStorage.setItem('basketList', '');
                 }
+                console.log(basketList)
             }
         }
         http.send(null);
