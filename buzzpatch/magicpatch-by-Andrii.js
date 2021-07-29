45cda57b35d26f0029dee5ae5638e7619cf81576
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2021-07-29 16:10:16 +0300

    Update magicpatch-by-Andrii.js

--- buzzpatch/magicpatch-by-Andrii.js
+++ buzzpatch/magicpatch-by-Andrii.js
@@ -425,7 +425,7 @@ let start = setInterval(() => {
         );
 
         document.querySelector('.js-heading .col-lg-7 img').src = 'https://conversionratestore.github.io/projects/buzzpatch/img/itch-relief-patches.png';
-        document.querySelector('.js-heading .col-lg-7 img').style.cssText = 'height: 200px; object-fit: cover;';
+        document.querySelector('.js-heading .col-lg-7 img').style.cssText = 'height: 220px; object-fit: cover;';
 
         document.querySelector('section.js-mobile').insertAdjacentHTML(
             'afterend',
