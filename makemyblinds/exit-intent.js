7ceda62e1b5d599dbae347621288a24888fe82b9
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-08-09 17:36:36 +0300

    Update exit-intent.js

--- makemyblinds/exit-intent.js
+++ makemyblinds/exit-intent.js
@@ -595,7 +595,7 @@ window.onload  = function () {
                 for (let key in greatForId) {
                     if (greatForId[key]["name"] == tdSplit[i]) {
                         console.log(tdSplit[i] + '==' + greatForId[key]["name"])
-                        document.querySelector('.product-great').insertAdjacentHTML('afterbegin', `<a href="https://www.makemyblinds.co.uk/blinds/${tdSplit[i]}" class="product-great-item" data-id="${greatForId[i].value}" data-name="${greatForId[i].name}">${tdSplit[i]}</a>`);
+                        document.querySelector('.product-great').insertAdjacentHTML('afterbegin', `<a href="https://www.makemyblinds.co.uk/blinds/${tdSplit[i]}" class="product-great-item" data-id="${greatForId[key]["value"]}" data-name="${greatForId[key]["name"]}">${tdSplit[i]}</a>`);
                     }
                 }
             }
