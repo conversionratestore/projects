38f770b7fbab8b861d1dc424b5b7bea7a3bc3e5d
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-08-11 15:49:00 +0300

    Update exit-intent.js

--- makemyblinds/exit-intent.js
+++ makemyblinds/exit-intent.js
@@ -675,8 +675,12 @@ window.onload  = function () {
         </div>`;
         if (index != 'popup') {
             document.querySelectorAll('.category-slider')[index].insertAdjacentHTML('afterbegin', card);
+            console.log('card: ' + card);
+            console.log('card index: ' + index);
+            console.log('card name: ' + name);
+            console.log('card link: ' + link);
             tnsInitialization('category-slider',2,3,4,false);
-
+        
         } else {
             document.querySelector('.popup .slider').insertAdjacentHTML('beforeend', card);
         }
