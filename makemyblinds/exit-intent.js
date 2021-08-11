643ee0cd21b077eebcada72ca5463e17d46d7ece
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-08-11 12:09:20 +0300

    Update exit-intent.js

--- makemyblinds/exit-intent.js
+++ makemyblinds/exit-intent.js
@@ -167,7 +167,7 @@ fetch('https://www.makemyblinds.co.uk/rest/V1/integration/admin/token', {
     Promise.all([request1,request2,request3,request4,request5,request6]).then(res => {
         let items = [...thermalItems, ...privacyItems, ...cordlessItems, ...noDrillItems, ...betterSleepItems, ...fireRetardantItems];
         // window.onload  = function () {
-        console.log('localStorage items: ' + items);
+        console.log('localStorage items: ' + JSON.stringify(items));
         localStorage.setItem('items', JSON.stringify(items));
         // };
     });
