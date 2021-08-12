6cdaa6cafd508e874abd9e7721dcd06526ed0ebc
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-08-12 09:41:43 +0300

    Update exit-intent.js

--- makemyblinds/exit-intent.js
+++ makemyblinds/exit-intent.js
@@ -650,7 +650,7 @@ window.onload  = function () {
             // console.log('index != popup and card: ' + name + ' в slider = ' + index)
             // if (document.querySelector(`.category-slider[data-id='${index}'] .slide`).length < 12) {
             //     console.log('length: ' + document.querySelector(`.category-slider[data-id='${index}'] .slide`).length)
-            //     tnsInitialization(`.category-slider[data-id='${index}']`,2,3,4,false);
+                tnsInitialization('.category-slider',2,3,4,false);
             // }
         } else {
             document.querySelector('.popup .slider').insertAdjacentHTML('beforeend', card);
@@ -842,7 +842,6 @@ window.onload  = function () {
         card(JSON.parse(localStorage.getItem('items'))[3]["idCategory"], itemsLocalStorage[3]["data"]["items"][i]["media_gallery_entries"][0]["file"], itemsLocalStorage[3]["data"]["items"][i]["name"],itemsLocalStorage[3]["data"]["items"][i]["price"],itemsLocalStorage[3]["data"]["items"][i]["custom_attributes"][3]["value"], itemsLocalStorage[3]["data"]["items"][i]["status"]);
         card(JSON.parse(localStorage.getItem('items'))[4]["idCategory"], itemsLocalStorage[4]["data"]["items"][i]["media_gallery_entries"][0]["file"], itemsLocalStorage[4]["data"]["items"][i]["name"],itemsLocalStorage[4]["data"]["items"][i]["price"],itemsLocalStorage[4]["data"]["items"][i]["custom_attributes"][3]["value"], itemsLocalStorage[4]["data"]["items"][i]["status"]);
         card(JSON.parse(localStorage.getItem('items'))[5]["idCategory"], itemsLocalStorage[5]["data"]["items"][i]["media_gallery_entries"][0]["file"], itemsLocalStorage[5]["data"]["items"][i]["name"],itemsLocalStorage[5]["data"]["items"][i]["price"],itemsLocalStorage[5]["data"]["items"][i]["custom_attributes"][3]["value"], itemsLocalStorage[5]["data"]["items"][i]["status"]);
-        tnsInitialization(`.category-slider`,2,3,4,false);
     }
 
     if (document.querySelector('.badge-fast-track')) {
