ee16f41f42fad4409512131d8073da0bccce2dfc
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-08-12 14:48:40 +0300

    Update exit-intent.js

--- makemyblinds/exit-intent.js
+++ makemyblinds/exit-intent.js
@@ -278,6 +278,7 @@ fetch('https://www.makemyblinds.co.uk/rest/V1/integration/admin/token', {
         tnsInitialization('.category-slider',2,3,4,false);
 
         if (document.querySelectorAll('.category-slider').innerHTML == "") {
+            tnsInitialization('.category-slider',2,3,4,false);
             for (let i = 0; i < 12; i++) {
                 let itemsLocalStorage = JSON.parse(localStorage.getItem('items'))
                 card(JSON.parse(localStorage.getItem('items'))[0]["idCategory"], itemsLocalStorage[0]["data"]["items"][i]["media_gallery_entries"][0]["file"], itemsLocalStorage[0]["data"]["items"][i]["name"],itemsLocalStorage[0]["data"]["items"][i]["price"],itemsLocalStorage[0]["data"]["items"][i]["custom_attributes"][3]["value"], itemsLocalStorage[0]["data"]["items"][i]["status"]);
