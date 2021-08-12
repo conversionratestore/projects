942e54dc51b1d96d4b309c12a1b44d1f402674a8
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-08-12 11:25:09 +0300

    Update exit-intent.js

--- makemyblinds/exit-intent.js
+++ makemyblinds/exit-intent.js
@@ -271,7 +271,7 @@ fetch('https://www.makemyblinds.co.uk/rest/V1/integration/admin/token', {
     // });
     Promise.all([request1,request2,request3,request4,request5]).then(res => { //,request6
         localStorage.setItem('items', JSON.stringify(items));
-        tnsInitialization('.category-slider',2,3,4,false);
+      
         let mut = new MutationObserver(function (muts) {
             console.log('mutation')
             if (document.querySelectorAll('.category-slider')) {
@@ -285,6 +285,7 @@ fetch('https://www.makemyblinds.co.uk/rest/V1/integration/admin/token', {
                     card(JSON.parse(localStorage.getItem('items'))[4]["idCategory"], itemsLocalStorage[4]["data"]["items"][i]["media_gallery_entries"][0]["file"], itemsLocalStorage[4]["data"]["items"][i]["name"],itemsLocalStorage[4]["data"]["items"][i]["price"],itemsLocalStorage[4]["data"]["items"][i]["custom_attributes"][3]["value"], itemsLocalStorage[4]["data"]["items"][i]["status"]);
                     // card(JSON.parse(localStorage.getItem('items'))[5]["idCategory"], itemsLocalStorage[5]["data"]["items"][i]["media_gallery_entries"][0]["file"], itemsLocalStorage[5]["data"]["items"][i]["name"],itemsLocalStorage[5]["data"]["items"][i]["price"],itemsLocalStorage[5]["data"]["items"][i]["custom_attributes"][3]["value"], itemsLocalStorage[5]["data"]["items"][i]["status"]); 
                 }
+                tnsInitialization('.category-slider',2,3,4,false);
             }
         });
 
