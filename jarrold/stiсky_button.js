be7a815d32f0b9680b54d8228164c83cca2f272e
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-08-13 10:21:52 +0300

    Update stiсky_button.js

--- jarrold/stiсky_button.js
+++ jarrold/stiсky_button.js
@@ -524,4 +524,24 @@ function createButtonList() {
       );
     });
   });
-}
+};
+
+(function (h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
+    function () {
+      (h.hj.q = h.hj.q || []).push(arguments);
+    };
+  h._hjSettings = { hjid: 2369936, hjsv: 6 };
+  a = o.getElementsByTagName("head")[0];
+  r = o.createElement("script");
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+window.hj =
+  window.hj ||
+  function () {
+    (hj.q = hj.q || []).push(arguments);
+  };
+hj("trigger", "sticky_button_mobile");
