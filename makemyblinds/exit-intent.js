da201425e96b03671efeafe5f816f770bb0a7e90
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-08-13 15:02:09 +0300

    Update exit-intent.js

--- makemyblinds/exit-intent.js
+++ makemyblinds/exit-intent.js
@@ -348,17 +348,17 @@ let mut = new MutationObserver(function (muts) {
         childList: true,
         subtree: true
     });
-    if (document.querySelector('.badge-circle.badge-dark-blue.badge-fast-track') && document.querySelectorAll('.product .product-great')) {
-        mut.disconnect();
-        console.log('mut.disconnect')
-        if (window.matchMedia("(min-width: 769px)").matches) {
-            console.log('mut.disconnect min 769')
-            document.querySelector('.product .product-great').style.padding = `0 95px 0 5px`;
-        } else {
-            console.log('mut.disconnect max 768')
-            document.querySelector('.product .product-great').style.padding = `0 60px 0 5px`;
-        }
-    } 
+    // if (document.querySelector('.badge-circle.badge-dark-blue.badge-fast-track') && document.querySelectorAll('.product .product-great')) {
+    //     mut.disconnect();
+    //     console.log('mut.disconnect')
+    //     if (window.matchMedia("(min-width: 769px)").matches) {
+    //         console.log('mut.disconnect min 769')
+    //         document.querySelector('.product .product-great').style.padding = `0 95px 0 5px`;
+    //     } else {
+    //         console.log('mut.disconnect max 768')
+    //         document.querySelector('.product .product-great').style.padding = `0 60px 0 5px`;
+    //     }
+    // } 
 });
 
 mut.observe(document, {
@@ -1097,16 +1097,16 @@ window.onload  = function () {
         document.querySelector('.your-box .price').innerHTML = document.querySelector('.product-info-price .price').innerHTML;
     });
 
-    // if (document.querySelector('.badge-circle.badge-dark-blue.badge-fast-track')) {
-    //     console.log('loaded true')
-    //     if (window.matchMedia("(min-width: 769px)").matches) {
-    //         console.log('loaded true min 769')
-    //         document.querySelector('.product .product-great').style.padding = `0 95px 0 5px`;
-    //     } else {
-    //         console.log('loaded true max 768')
-    //         document.querySelector('.product .product-great').style.padding = `0 60px 0 5px`;
-    //     }
-    // } 
+    if (document.querySelector('.badge-circle.badge-dark-blue.badge-fast-track') && document.querySelector('.product .product-great')) {
+        console.log('loaded true')
+        if (window.matchMedia("(min-width: 769px)").matches) {
+            console.log('loaded true min 769')
+            document.querySelector('.product .product-great').style.padding = `0 95px 0 5px`;
+        } else {
+            console.log('loaded true max 768')
+            document.querySelector('.product .product-great').style.padding = `0 60px 0 5px`;
+        }
+    } 
     eventsCategories('.category .card-title','Click on product from listing');
     eventsCategories('.tns-controls button','Click on arrows button listing');
     eventsCategories('.tns-nav button','Click on dots button listing');
