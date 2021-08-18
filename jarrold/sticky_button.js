9064676bebc9896e3d714c7006bbf395c76339de
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-08-18 16:00:04 +0300

    Update sticky_button.js

--- jarrold/sticky_button.js
+++ jarrold/sticky_button.js
@@ -638,7 +638,8 @@ function createButtonList() {
   }
 
   document.querySelectorAll(`${parentDiv} .${products}`).forEach((item) => {
-    if (!document.querySelector(".btn-open-list")) {
+    if (!document.querySelector(".btn-open-list") || !document.querySelector("#search-modal .btn-open-list")) {
+      console.log(`object`);
       item.querySelectorAll("li").forEach((i) => {
         i.style.justifyContent = "space-between";
         i.style.display = "flex";
