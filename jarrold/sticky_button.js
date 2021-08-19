aa586dc5a6a839a7d5432b69f851b184ee48510c
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-08-19 09:32:33 +0300

    Update sticky_button.js

--- jarrold/sticky_button.js
+++ jarrold/sticky_button.js
@@ -761,11 +761,11 @@ observer.observe(document.querySelector("#search-modal"), {
 });
 
 const callbackSort = function (mutationsList) {
-  if (document.querySelector(".sort #sortby")) {
+  if (document.querySelector("#sortby")) {
     console.log(`callbackSort`);
     observer.disconnect();
     createButtonListSearch();
-    observer.observe(document.querySelector(".sort #sortby"), {
+    observer.observe(document.querySelector("#sortby"), {
       childList: true,
       subtree: true,
     });
@@ -774,7 +774,7 @@ const callbackSort = function (mutationsList) {
 
 let observerSort = new MutationObserver(callbackSort);
 
-observerSort.observe(document.querySelector(".sort #sortby"), {
+observerSort.observe(document.querySelector("#sortby"), {
   childList: true,
   subtree: true,
 });
