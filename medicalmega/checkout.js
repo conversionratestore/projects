2dbfeb2dd2ebd3a8b7fdbc1fafc6bc728c4a0f05
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-08-19 13:40:59 +0300

    Update checkout.js

--- medicalmega/checkout.js
+++ medicalmega/checkout.js
@@ -560,7 +560,7 @@ window.onload  = function () {
                 'quantity': product.querySelector('.quantity').value,
                 'price': product.querySelector('.total-price').dataset.price,
                 'product_variant_id': product.dataset.variantId,
-                'img_src': product.querySelector('a img').getAttribute('src'),
+                'img_src': product.querySelector('.checkout-product_img img').getAttribute('src'),
                 'link': product.querySelectorAll('a')[0].getAttribute('href'),
                 'title': product.querySelector('.flex-between a').innerHTML,
             });
