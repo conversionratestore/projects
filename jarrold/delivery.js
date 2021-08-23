806f16bac9d01a3a375c138d3ade97e8ef12eea5
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-08-23 15:25:15 +0300

    Update delivery.js

--- jarrold/delivery.js
+++ jarrold/delivery.js
@@ -2,138 +2,147 @@ let styleSet = /*html*/ `
 <style>
 
 
-.upc{
-text-align:center;
+.upc {
+  text-align: center;
 }
 
+@media (min-width: 992px) {
+  .upc {
+    text-align: start;
+  }
 
-@media (min-width: 992px){
-.upc{
-text-align:start;
-max-width: 200px;
+  #product p.summ {
+    max-width: 300px;
+  }
 }
 
-.summ{
-max-width: 300px;
+@media (min-width: 992px) and (max-width: 1200px) {
+  .upc {
+    max-width: 200px;
+  }
+
+   #product p.summ {
+    max-width: 190px;
+  }
 }
 
-.controls{
-max-width: 300px;
-}
+@media (min-width: 992px) and (max-width: 1301px) {
+  .controls {
+    max-width: 200px;
+  }
 }
 
 .hidden {
   display: none;
 }
 
-.accent-text-random{
-color:#E62B8A;
+.accent-text-random {
+  color: #e62b8a;
 }
 
-.price-more{
-font-weight:700;
+.price-more {
+  font-weight: 700;
 }
 
-.delivery-box-mobile{
-justify-content:center;
-margin-top:28px;
-margin-bottom:28px;
-font-weight: 400;
-font-size: 14px;
-line-height: 1;
-align-items: center;
-text-align: center;
-color: #2C8081;
+.delivery-box-mobile {
+  justify-content: center;
+  margin-top: 28px;
+  margin-bottom: 28px;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1;
+  align-items: center;
+  text-align: center;
+  color: #2c8081;
 }
 
-  
-.text-block-more{
-line-height: 1.7;
-text-align: center;
+.text-block-more {
+  line-height: 1.7;
+  text-align: center;
 }
 
-.delivery-svg-mobile{
-margin-right: 0.7em;
-vertical-align: text-bottom;
+.delivery-svg-mobile {
+  margin-right: 0.7em;
+  vertical-align: text-bottom;
 }
 
-.delivery-svg-mobile-more{
-margin-right:5px;
-vertical-align: text-bottom;
+.delivery-svg-mobile-more {
+  margin-right: 5px;
+  vertical-align: text-bottom;
 }
 
-.information-box-mobile{
-padding:0;
-margin-top:20px;
-margin-bottom:28px;
-text-align: center;
-list-style: none;
-font-weight: 400;
-font-size: 12px;
-line-height: 1.17;
-color: #8E8E8E;
+.information-box-mobile {
+  padding: 0;
+  margin-top: 20px;
+  margin-bottom: 28px;
+  text-align: center;
+  list-style: none;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.17;
+  color: #8e8e8e;
 }
 
-.list-text:first-child{
-margin-bottom:8px;
+.list-text:first-child {
+  margin-bottom: 8px;
 }
 
-.params-span{
-font-weight: 600;
-font-size: 13px;
+.params-span {
+  font-weight: 600;
+  font-size: 13px;
 }
 
 /* Desktop */
 
 .price {
-position: relative;
+  position: relative;
 }
 
-.controls{
-align-items:center;
-}
-@media (max-width: 1200px){
-    .delivery-box{
-    width:0;
-    }
+.controls {
+  align-items: center;
 }
 
-.delivery-box{
-display: block;
-position: absolute;
-top: 0;
-right: 0;
-padding:20px;
-width:180px;
-border:2px dashed #E5E5E5;
 
-font-weight: 400;
-font-size: 14px;
-line-height: 1.4;
-align-items: center;
-text-align: center;
-color: #2C8081;
+
+.delivery-box {
+  display: block;
+  position: absolute;
+  top: 0;
+  width: 180px;
+  right: 0;
+  padding: 20px;
+  border: 2px dashed #e5e5e5;
+
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.4;
+  align-items: center;
+  text-align: center;
+  color: #2c8081;
 }
 
-.delivery-svg{
-vertical-align: baseline;
+
+
+.delivery-svg {
+  vertical-align: baseline;
 }
 
-.text-span{
-display: block;
-color:#E62B8A;
+.text-span {
+  display: block;
+  color: #e62b8a;
 }
 
-.information-box{
-text-align:start;
-margin:0;
-padding:0;
-list-style: none;
-font-weight: 400;
-font-size: 12px;
-line-height: 1.17;
-color: #8E8E8E;
+.information-box {
+  text-align: start;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.17;
+  color: #8e8e8e;
 }
+
 
 </style>
 `;
