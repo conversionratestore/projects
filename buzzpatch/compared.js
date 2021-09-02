a814846896299b8813b3aa6552512bf0efe24310
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-09-02 11:36:27 +0300

    Update compared.js

--- buzzpatch/compared.js
+++ buzzpatch/compared.js
@@ -312,3 +312,30 @@ document.querySelector(".compar-btn").addEventListener("click", function (e) {
     behavior: "smooth",
   });
 });
+
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+  event: "event-to-ga",
+  eventCategory: "Exp - Buzzpatch: comparison table",
+  eventAction: "loaded",
+});
+
+(function (h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
+    function () {
+      (h.hj.q = h.hj.q || []).push(arguments);
+    };
+  h._hjSettings = { hjid: 2247058, hjsv: 6 };
+  a = o.getElementsByTagName("head")[0];
+  r = o.createElement("script");
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+window.hj =
+  window.hj ||
+  function () {
+    (hj.q = hj.q || []).push(arguments);
+  };
+hj("trigger", "Buzzpatch_comparison_table");
