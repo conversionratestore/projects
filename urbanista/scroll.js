21036fabdf28b3f96e5a5e99dda7a21f460ee20c
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2021-09-10 01:50:56 +0300

    Update scroll.js

--- urbanista/scroll.js
+++ urbanista/scroll.js
@@ -573,7 +573,7 @@ setTimeout(() => {
 
     // images and text blocks
     document.querySelectorAll('.pd-col').forEach(block => {
-        if (!block.querySelector('h1')?.innerText === 'SPECS') {
+        if (!block.querySelector('h1')?.innerText === 'SPECS' && !block.querySelector('h1')?.innerText === 'FEATURES' && !block.querySelector('h1')?.innerText === 'DESIGN') {
             let imagesBlock = block.closest('.pd-row');
 
             document.querySelector('.product-view-badge')?.childNodes.length > 1 ? document.querySelector('.product-view-badge').after(imagesBlock) : document.querySelector('.slider_custom').after(imagesBlock);
