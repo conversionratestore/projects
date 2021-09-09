017f7747626977b3b18a010f64c9b481c589631d
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-09-09 17:58:47 +0300

    Update stickers.js

--- buzzpatch/stickers.js
+++ buzzpatch/stickers.js
@@ -1,4 +1,4 @@
-let styleBuzzpatchSticckers = /*html*/ `
+let styleBuzzpatchstickers = /*html*/ `
 <style>
 
 .acc-color{
@@ -6,14 +6,12 @@ color: #FF3C7F;
 font-weight: 500;
 }
 
-.sticckers-block p{
+.stickers-block p{
     margin:0;
 }
 
-.sticckers-block{
-
+.stickers-block{
 background: #F1F3F4 url("https://conversionratestore.github.io/projects/buzzpatch/img/bg_white.svg") no-repeat center -1px / 100%;
-
 font-weight: 400;
 font-size: 14px;
 line-height: 1.3;
@@ -23,8 +21,8 @@ padding: 90px 15px 72px 15px;
 margin: 0 0 72px 0;
 }
 
-.sticckers-main-title{
-margin-bottom:40px;
+.stickers-main-title{
+margin: 25px 0 40px 0;
 font-weight: 400;
 font-size: 36px;
 line-height: 1.2;
@@ -34,7 +32,75 @@ text-transform: uppercase;
 color: #0C0B0B;
 }
 
-.sticckers-title{
+.stickers-question-list{
+font-weight: 700;
+font-size: 18px;
+line-height: 1.3;
+color: #212529;
+
+}
+
+.stickers-question{
+margin-bottom: 20px !important;
+text-align:start;
+}
+
+.add-person-list{
+display:flex;
+width: calc((100%* 3) / 4);
+}
+
+.stickers-select{
+width:100% !important;
+background-color: #FFFFFF !important;
+border: 1px solid #D9D9D9 !important;
+box-sizing: border-box !important;
+border-radius: 5px !important;
+padding: 9px 5px 9px 20px !important;
+
+font-family: Proxima Nova;
+font-weight: 600 !important;
+font-size: 14px !important;
+line-height: 1.3 !important;
+color: #212529 !important;
+}
+
+.select-var{
+margin-bottom: 40px;
+}
+
+
+.add-person-flexbox:not(:last-child){
+margin-right:12px;
+}
+
+.add-person-btn{
+margin-top: 28px;
+margin-bottom: 8px;
+width: 44px;
+height: 44px;
+background: #FFFFFF;
+border: 1px solid #D9D9D9;
+border-radius: 50%;
+}
+
+.add-person-svg{
+vertical-align: unset;
+}
+
+.add-person-title{
+font-family: DINEngschrift LT;
+font-weight: 400 !important;
+font-size: 16px !important;
+line-height: 1 !important;
+text-align: center !important;
+letter-spacing: 0.05em !important;
+text-transform: uppercase !important;
+color: #0C0B0B !important;
+
+}
+
+.stickers-title{
 font-weight: 400;
 font-size: 26px;
 line-height: 1.2;
@@ -45,7 +111,7 @@ color: #212529;
 margin-bottom: 5px;
 }
 
-.sticckers-pack{
+.stickers-pack{
 font-weight: 400;
 font-size: 18px;
 line-height: 1.5;
@@ -54,7 +120,7 @@ color: #6F6F6F;
 margin-bottom: 20px !important;
 }
 
-.sticckers-price{
+.stickers-price{
 font-weight: 700 !important;
 font-size: 24px !important;
 line-height: 1.3 !important;
@@ -63,7 +129,7 @@ margin-top: 20px !important;
 margin-bottom: 5px !important;
 }
 
-.sticckers-foreach-text{
+.stickers-foreach-text{
 font-weight: 400 !important;
 font-size: 14px !important;
 line-height: 1.3 !important;
@@ -72,17 +138,14 @@ color: #212529;
 
 .flex-btn{
 display: flex;
-justify-content: space-evenly;
+justify-content: space-around;
 }
 
-.sticckers-btn{
+.stickers-btn{
 display: inline-block;
 width: 100%;
 max-width: 343px;
-padding: 20.5px 20px;
-margin-top: 40px;
-margin-bottom: 40px;
-
+padding: 25px 20px;
 font-family: DINEngschrift LT;
 font-weight: 400;
 font-size: 16px;
@@ -90,24 +153,27 @@ line-height: 1;
 text-align: center;
 letter-spacing: 0.05em;
 text-transform: uppercase;
-color: #FFFFFF;
+color: #FFFFFF !important;
 
 background: #FF3C7F;
 box-shadow: 4px 8px 12px rgba(0, 0, 0, 0.1);
 border-radius: 52px;
+border: 1px solid #FF3C7F;
 
 cursor: pointer;
 outline: none;
 }
 
-.sticckers-btn.other{
+.stickers-btn.other{
 background: #FFFFFF;
-color: #FF3C7F;
+color: #FF3C7F !important;
 border: 1px solid #FF3C7F;
 box-shadow:none;
+margin-left: 5px !important;
 }
 
-.sticckers-btn.small-btn{
+.stickers-btn.small-btn{
+margin: 40px 0;
 max-width: 165px;
 }
 
@@ -122,12 +188,37 @@ max-width: 165px;
     display: flex;
     text-align: start;
     justify-content: space-between;
+    align-items: center;
+}
+
+.flex-box-total:not(:last-child){
+margin-bottom: 20px;
+}
+
+.flex-text-total{
+max-width: 57%;
+font-family: Proxima Nova !important;
+font-weight: 600 !important;
+font-size: 14px !important;
+line-height: 130% !important;
+color: #212529 !important;
 }
 
 
+.flex-span-total{
+font-family: DINEngschrift LT !important;
+font-size: 20px !important;
+line-height: 1.2 !important;
+letter-spacing: 0.02em !important;
+text-transform: uppercase !important;
+color: #000000 !important;
+}
 
 .text-season{
 text-align:start;
+font-size: 14px !important;
+line-height: 1.3 !important;
+color: #000000 !important;
 }
 
 .text-season:not(:last-child){
@@ -137,84 +228,117 @@ margin-bottom:9px;
 </style>
 `;
 
-let buzzpatchSticckers = /*html*/ `
-<section class="sticckers-block">
-    <h1 class="sticckers-main-title">how many buzzpatch stickers do you need to protect your children?</h1>
-    <div>
-        <ol>
-        <li>
-            <p>How  many people do you need Buzzpatch stickers for?</p>
-            <p>
-                <select>
+let buzzpatchStickersOne = /*html*/ `
+<section class="stickers-block">
+    <h1 class="stickers-main-title">how many buzzpatch stickers do you need to protect your children?</h1>
+    <div class="stickers-block-first">
+        <ol class="stickers-question-list">
+            <li class="select-var">
+                <p class="stickers-question">How  many people do you need Buzzpatch stickers for?</p>
+                <select class="stickers-select">
                     <option selected value="">3 people</option>
                 </select>
-            </p>
-        </li>
-        <li>
-            <p>How  old are they?</p>
-            <p>Person 1
-                <select>
-                    <option selected value="">3</option>
-                </select>
-            </p>
-             <p>Person 2
-                <select>
-                    <option selected value="">12</option>
-                </select>
-            </p>
-             <p>Person 3
-                <select>
-                    <option selected value="">16</option>
-                </select>
-            </p>
+            </li>
+            <li class="select-var">
+                <p class="stickers-question">How  old are they?</p>
+                <div class="add-person-list">
+                    <div class="add-person-flexbox">
+                        <p class="add-person-block">Person 1</p>
+                        <select class="stickers-select">
+                            <option selected value="">3</option>
+                        </select>
+                    </div>
 
-            <button>add person</button>
-        </li>
-        <li>
-            <p>How  many months per year does the mosquito season last?</p>
-            <p>
-                <select>
+                    <div class="add-person-flexbox">
+                        <p class="add-person-block">Person 2</p>
+                        <select class="stickers-select">
+                            <option selected value="">12</option>
+                        </select>
+                    </div>
+
+                    <div class="add-person-flexbox">
+                        <p class="add-person-block">Person 3</p>
+                        <select class="stickers-select">
+                            <option selected value="">16</option>
+                        </select>
+                    </div>
+                </div>
+
+                <button class="add-person-btn">
+                    <svg class="add-person-svg" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="#0C0B0B"/>
+                    </svg>
+                </button>
+
+                <p class="add-person-title">add person</p>
+            </li>
+            <li class="select-var">
+                <p class="stickers-question">How  many months per year does the mosquito season last?</p>
+                <select class="stickers-select">
                     <option selected value="">3 months</option>
                 </select>
-            </p>
-        </li>
-    </ol>
-
-    <a href="#" class="sticckers-btn">calculate how many do i need</a>
-    </div>
-    <div>
-        <h2 class="sticckers-title">
-            You are recommended <span class="acc-color">3 packs</span> that contain <span class="acc-color">180</span> stickers
-        </h2>
-        <p class="sticckers-pack">60 stickers in 1 pack</p>
-        <img src="https://conversionratestore.github.io/projects/buzzpatch/img/stickers.svg" alt="stickers">
-
-        <p class="sticckers-price">$36.00 (40% OFF)</p>
-        <p class="sticckers-foreach-text">$12 for each pack</p>
-
-        <div class="flex-btn">
-            <button class="sticckers-btn small-btn">Buy 3 packs</button>
-            <button class="sticckers-btn small-btn other">see other packs</button>
-        </div>
-
-       <div class="parent-border">
-            <div class="flex-box-total">
-            <p>Mosquito season duration:</p>
-            <span>3 months</span>
-            </div>
-            <div class="flex-box-total">
-            <p>Total number of Buzzpatch stickers required:</p>
-            <span>180 stickers</span>
-            </div>
-       </div>
-       <div>
-           <p class="text-season">Person <span>1</span> requires <span class="acc-color">30 stickers</span> per season</p>
-           <p class="text-season">Person <span>2</span> requires <span class="acc-color">75 stickers</span> per season</p>
-           <p class="text-season">Person <span>3</span> requires <span class="acc-color">85 stickers</span> per season</p>
-       </div>
+            </li>
+        </ol>
+        <a class="stickers-btn btn-first">calculate how many do i need</a>
     </div>
 </section>
 `;
 
-document.head.insertAdjacentHTML("beforeend", styleBuzzpatchSticckers);
-document.querySelector("#faqs").insertAdjacentHTML("beforebegin", buzzpatchSticckers);
+let buzzpatchStickersPageTwo = /*html*/ `
+<div class="stickers-block-two">
+    <h2 class="stickers-title">You are recommended <span class="acc-color">3 packs</span> that contain <span class="acc-color">180</span> stickers</h2>
+    <p class="stickers-pack">60 stickers in 1 pack</p>
+    <img src="https://conversionratestore.github.io/projects/buzzpatch/img/stickers.svg" alt="stickers">
+
+    <p class="stickers-price">$36.00 (40% OFF)</p>
+    <p class="stickers-foreach-text">$12 for each pack</p>
+
+    <div class="flex-btn">
+        <a href="https://buzzpatch.com/a/secure/checkout/x89M9vTnQhNJyK4KKpcw" class="stickers-btn small-btn">Buy 3 packs</a>
+        <a href="#getNow" class="stickers-btn small-btn other">see other packs</a>
+    </div>
+
+    <div class="parent-border">
+        <div class="flex-box-total">
+            <p class="flex-text-total">Mosquito season duration:</p>
+            <span class="flex-span-total">3 months</span>
+        </div>
+        <div class="flex-box-total">
+            <p class="flex-text-total">Total number of Buzzpatch stickers required:</p>
+            <span class="flex-span-total">180 stickers</span>
+        </div>
+    </div>
+    <div>
+        <p class="text-season">Person <span>1</span> requires <span class="acc-color">30 stickers</span> per season</p>
+        <p class="text-season">Person <span>2</span> requires <span class="acc-color">75 stickers</span> per season</p>
+        <p class="text-season">Person <span>3</span> requires <span class="acc-color">85 stickers</span> per season</p>
+    </div>
+</div>
+`;
+
+document.head.insertAdjacentHTML("beforeend", styleBuzzpatchstickers);
+document.querySelector("#faqs").insertAdjacentHTML("beforebegin", buzzpatchStickersOne);
+
+document.querySelector(".btn-first").addEventListener("click", function () {
+  document.querySelector(".stickers-block-first").innerHTML = "";
+  document.querySelector(".stickers-main-title").insertAdjacentHTML("afterend", buzzpatchStickersPageTwo);
+});
+
+if (document.querySelector(".stickers-block-two")) {
+  document.querySelector(".other").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let href = this.getAttribute("href").substring(1);
+
+    const scrollTarget = document.getElementById(href);
+
+    const topOffset = 0;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  });
+}
