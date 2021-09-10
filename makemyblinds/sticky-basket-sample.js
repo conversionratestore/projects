34e72bdb6634cfd55e2e73673ec2660abf7edf6a
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-09-10 17:48:58 +0300

    Update sticky-basket-sample.js

--- makemyblinds/sticky-basket-sample.js
+++ makemyblinds/sticky-basket-sample.js
@@ -483,7 +483,7 @@ window.onload  = function () {
                     }
                     if (!el.closest('.control').querySelector('.mage-error[generated]')) {
                         for (let i = 0; i < inputs.length; i++) {
-                            if (!inputs[i].classList.contains('step-1') && document.querySelectorAll('#order_form input.step-1')[i].dataset.validate.includes('"required": true').value != '' && document.querySelectorAll('#order_form input.step-1')[i].dataset.validate.includes('"required": true').value != 'undefined') {
+                            if (!inputs[i].classList.contains('step-1')) {
                                 inputs[i].closest('.field').style.display = 'block';
                                 document.querySelector('#region').closest('.field').style.display = 'none';
                                 document.querySelector('#region_id').closest('.field').style.display = 'none';
