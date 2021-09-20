f904b0a3f54217ed7aa15abf8a58e4fcc8c2ef1f
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-09-20 16:44:51 +0300

    Update stickers.js

--- buzzpatch/stickers.js
+++ buzzpatch/stickers.js
@@ -162,7 +162,7 @@ align-items: center;
 justify-content: center;
 font-family: DINEngschrift LT;
 font-weight: 400;
-font-size: 16px;
+font-size: 16px !important;
 line-height: 1;
 text-align: center;
 letter-spacing: 0.05em;
