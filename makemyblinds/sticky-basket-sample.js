08f035c002432f9401a0d41d2fd5b321f0d3eec9
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-09-21 12:05:59 +0300

    Update sticky-basket-sample.js

--- makemyblinds/sticky-basket-sample.js
+++ makemyblinds/sticky-basket-sample.js
@@ -132,6 +132,29 @@ function samplesModal() {
         }
     }
 }
+
+function actionSave() {
+    if (document.querySelector('.product-mobile-title')) {
+        if (!document.querySelector(`.btn-remove[data-item-id="${document.querySelector('[name="product"]').value}"]`) && document.querySelector('.btn-white.saved')) {
+            document.querySelector('.btn-white').classList.remove('saved');
+            document.querySelector('.btn-white').innerHTML = 'ORDER FREE SAMPLE';
+            document.querySelector('.wishlist-mobile-wrap a').classList.remove('active');
+            document.querySelector('.wishlist-mobile-wrap a').setAttribute('data-action','add-to-wishlist');
+            document.querySelector('.wishlist-mobile-wrap a').setAttribute('title','Add to Wish List');
+        }
+    }
+    if (document.querySelectorAll('.hp-popular-blinds-item') && document.querySelectorAll('.product-item')) {
+        document.querySelectorAll('.product-item').forEach(el => {
+            let id = el.getAttribute('id').replace('item_','');
+            if (!document.querySelector(`.hp-popular-blinds-item._product-id-${id}`) && document.querySelector(`.hp-popular-blinds-item._product-id-${id} .action.towishlist.active`)) {
+                document.querySelector(`.hp-popular-blinds-item._product-id-${id} .action.towishlist`).classList.remove('active');
+                document.querySelector(`.hp-popular-blinds-item._product-id-${id} .action.towishlist`).setAttribute('title','Add to Wish List');
+                document.querySelector(`.hp-popular-blinds-item._product-id-${id} .action.towishlist`).setAttribute('data-action','add-to-wishlist');
+            }
+        })
+    }
+}
+
 window.onload  = function () {
     //styles
     document.body.insertAdjacentHTML('afterbegin',`
@@ -643,7 +666,6 @@ window.onload  = function () {
             });
         })
     }
-
 };
 
 //MutationObserver
@@ -683,13 +705,7 @@ let mut = new MutationObserver(function (muts) {
                 document.querySelector('.free-samples-modal .samples-block').classList.add('before');
             }
         })
-        if (!document.querySelector(`.btn-remove[data-item-id="${document.querySelector('[name="product"]').value}"]`) && document.querySelector('.btn-white.saved')) {
-            document.querySelector('.btn-white').classList.remove('saved');
-            document.querySelector('.btn-white').innerHTML = 'ORDER FREE SAMPLE';
-            document.querySelector('.wishlist-mobile-wrap a').classList.remove('active');
-            document.querySelector('.wishlist-mobile-wrap a').setAttribute('data-action','add-to-wishlist');
-            document.querySelector('.wishlist-mobile-wrap a').setAttribute('title','Add to Wish List');
-        }
+        actionSave();
     }
     mut.observe(document, {
         childList: true,
