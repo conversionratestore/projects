807725fa5a1db8e659a68e2dec40f9da2d4294e2
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-10-04 14:57:02 +0300

    Update payment_flow.js

--- samcart/payment_flow.js
+++ samcart/payment_flow.js
@@ -325,7 +325,7 @@ window.onload  = function () {
     // displayed btn
     if(document.querySelector(".apple-pay:not(.ng-hide)")){
         document.querySelector(".payment-btn-box").insertAdjacentHTML('beforeend', applePayBtn)
-        scrolling(".apple-pay-btn", ".apple-pay")
+        scrolling(".apple-pay-btn", "#digitalWalletRadio")
     }
     
     if(document.querySelector("#payPalRadio")){
@@ -340,7 +340,7 @@ window.onload  = function () {
     
     if(document.querySelector(".google-pay:not(.ng-hide)")){
         document.querySelector(".payment-btn-box").insertAdjacentHTML('beforeend', googlePayBtn)
-        scrolling(".google-pay-btn", ".google-pay")
+        scrolling(".google-pay-btn", "#digitalWalletRadio")
     }
     
     
