e67aeda93141389cb36c4f6fbae6273cb47f4fcd
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-10-05 16:14:02 +0300

    Create additional.js

---
+++ mammamiacovers/additional.js
@@ -0,0 +1,420 @@
+document.body.insertAdjacentHTML('afterbegin',`
+<style>
+    .faq__accordion__title {
+        padding: 0;
+    }
+    .swiper-button[disabled] svg{
+        fill: #999999;
+    }
+    .sizes-types .faq__accordion__item{
+        padding: 20px 0 10px;
+        border-top: 1px solid #999999;
+        border-bottom: 1px solid #999999;
+        margin-right: -17px;
+    }
+    .sizes-types .faq__accordion__title-text {
+        font-size: 20px;
+        line-height: 27px;
+        text-transform: uppercase;
+        color: #212121;
+        text-align: center;
+        margin: 30px 0;
+    }
+    .sizes-types .faq__accordion__item {
+        display: flex!important;  
+        position: relative;
+    }
+    .sizes-info {
+        position: sticky;
+        left: 0;
+        background-color: #fff;
+        z-index: 2;
+        margin: auto 0 0 0;
+        padding: 0 20px 22px;
+        width: 142px;
+        flex-shrink: 0;
+        list-style-type: none;
+    }
+    .sizes-info li {
+        font-size: 12px;
+        line-height: 14px;
+        text-transform: capitalize;
+        color: #212121;
+        padding: 14px 0 13px;
+        border-bottom: 1px solid #C6C3C7;
+    }
+    .sizes-info li:last-child {
+        border: none;
+    }
+    .sizes-types .sizes__list {
+        flex-wrap: nowrap;
+        padding: 0;
+        overflow-x: auto;
+        justify-content: inherit;
+    }
+    .sizes-types .sizes__item {
+        margin: 0;
+        padding: 10px 14px;
+        flex-shrink: 0;
+        border-radius: 3px;
+        border: 1px solid transparent;
+        transition: all 0.3s ease;
+        min-width: 122px;
+    }
+    .sizes-types span.sizes__text:last-child{
+        border: none;
+    }
+    .sizes-types .sizes__link--image {
+        width: 100%!important;
+        height: 80px!important;
+        margin-bottom: 10px;
+        border: none;  
+    }
+     .sizes-types .sizes__link--image img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+        border-radius: 3px;
+     }
+    .sizes-types .sizes__text {
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 14px;
+        text-transform: uppercase;
+        color: #212121;
+        transition: all 0.3s ease;
+    }
+    .sizes-types span.sizes__text {
+        min-height: 42px;
+        padding: 7px 0;
+        border-bottom: 1px solid #C6C3C7;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .sizes-types p.sizes__text {
+        color: #999999;
+        font-weight: 600;
+        min-height: 38px;
+        transition: all 0.3s ease;
+        margin: 0;
+    }
+    .sizes-types .sizes__link--image:active, .sizes-types .sizes__link--image:focus, .sizes-types .sizes__link--image:hover {
+        border: none!important;
+    }
+    .sizes-types .faq__accordion__item:before {
+        content: '';
+        position: absolute;
+        z-index: 2;
+        top: 0;
+        right: 0;
+        height: 100%;
+        width: 91px;
+        background: linear-gradient(to left, rgba(255,255,255,1) 15%, rgba(255,255,255,0.8) 40%, rgba(255,255,255,0));
+        pointer-events: none;
+    }
+    .sizes-types .sizes__item.tns-slide-active {
+        background: #F5F5F5;
+        border-color: #FFAC07;
+    }
+    .sizes-types .sizes__item.tns-slide-active p.sizes__text {
+        font-weight: 800;
+        font-size: 14px;
+        color: #212121;
+    }
+    .sizes-types .sizes__item.tns-slide-active .sizes__text {
+        font-weight: bold;
+    }
+    .btn-view {
+        font-weight: 600;
+        font-size: 12px;
+        line-height: 21px;
+        text-transform: capitalize;
+        color: #212121;
+        border: 1px solid #C6C3C7;
+        border-radius: 2px;
+        width: 100%;
+        background-color: transparent;
+        margin-bottom: 10px;
+        display: block;
+    }
+    .sizes-types .sizes__item.tns-slide-active .btn-view {
+        background-color: #DA3269;
+        border-color: #DA3269;
+        color: #fff;
+        font-weight: 800;
+    }
+    .sizes-types .name {
+        font-size: 11px;
+        line-height: 14px;
+        text-align: center;
+        color: #212121;
+        font-weight: 400;
+        transition: all 0.3s ease;
+        margin-bottom: 20px;
+        min-height: 28px;
+    }
+    .sizes-types .sizes__item.tns-slide-active .name {
+        color: #007185;
+        font-weight: 700;
+    }
+    .swiper-button {
+        position: absolute;
+        top: 100px;
+        background: none;
+        border: none;
+        height: fit-content;
+        z-index: 3;
+    } 
+    .swiper-button-prev {
+        left: 122px;
+    }
+    .swiper-button-next {
+        right: 4px;
+    }
+    #tns1 {
+        width: 100%!important;
+        padding-bottom: 10px;
+    }
+    .btn-close {
+        position: absolute;
+        width: 20px;
+        height: 20px;
+        top: -10px;
+        right: -10px;
+        z-index: 2;
+        background-color: #000;
+        border-radius: 50%;
+    }
+    .btn-close:before, .btn-close:after {
+        content: '';
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        background-color: #fff;
+        width: 45%;
+        height: 1px;
+        border: none;
+    }
+    .btn-close:before {
+        transform: translate(-50%,-50%) rotate(45deg);
+    }
+    .btn-close:after {
+        transform: translate(-50%,-50%) rotate(-45deg);
+    }
+    .popup-video {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        overflow-y: auto;
+        background-color: rgba(0,0,0,0.6);
+        z-index: 9999999999;
+        display: inline-flex;
+        opacity: 0;
+        pointer-events: none;
+        transition: all 0.3s ease;
+    }
+    .popup-video.active {
+        opacity: 1;
+        pointer-events: auto;
+    }
+    .popup-video_container {
+        position: relative;
+        margin: auto;
+        width: calc(100% - 20px);
+        height: auto;
+        padding: 10px;
+        background-color: #fff;
+        border-radius: 4px;
+    }
+    .product-description__container .product-description__item {
+        padding: 0;
+        background: none;
+        margin-bottom: 30px;
+    }
+    .product-description__container li strong {
+        line-height: 16px;
+        color: #A8034F;
+        display: flex;
+        align-items: center;
+        padding-left: 30px;
+        background: no-repeat left center / 20px 100%;
+        margin-bottom: 10px;
+    }
+    .product-description__container li {
+        font-size: 12px;
+        line-height: 18px;
+        color: #212121;
+    }
+    .measuring-guide {
+        border: 1px solid #999999;
+        border-radius: 5px;
+        width: 100%;
+        padding: 20px 30px;
+        margin: 30px 0 40px;
+    }
+    .measuring-guide h2 {
+        font-weight: 900;
+        font-size: 40px;
+        line-height: 47px;
+        text-transform: capitalize;
+        color: #212121;
+        margin-bottom: 10px;
+    }
+    .measuring-guide p {
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 22px;
+        color: #999999;
+    }
+    .measuring-guide .bottom {
+        display: flex;
+        align-items: center;
+        margin-top: 20px;
+    }
+    .measuring-guide .bottom img {
+        margin-right: 20px;
+        height: 20px;
+        width: 31px;
+        object-fit: contain;
+        flex-shrink: 0;
+    }
+    .measuring-guide .bottom p {
+        color: #212121;
+        font-weight: bold;
+        font-size: 12px;
+        line-height: 16px;
+    }
+    .img-free-returns {
+        width: 100%;
+        height: auto;
+        object-fit: contain;
+        margin-bottom: 40px;
+    }
+    @media only screen and (max-width: 360px)  {
+        .sizes-info {
+            padding: 0 10px 20px 10px;
+            width: 122px;
+        }
+        .swiper-button-prev {
+            left: 102px;
+        }
+        .sizes-types .sizes__item {
+            padding: 8px;
+        }
+    }
+</style>`);
+
+let linkCustom = document.createElement('link');
+linkCustom.href = 'https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/tiny-slider.css';
+linkCustom.rel = 'stylesheet';
+document.head.appendChild(linkCustom);
+
+let scriptCustom = document.createElement('script');
+scriptCustom.src = 'https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/min/tiny-slider.js';
+scriptCustom.async = false;
+document.head.appendChild(scriptCustom);
+
+document.querySelectorAll('#sizes')[1].classList.add('sizes-types');
+
+function tnsInitialization() {
+    let sliderCategories = tns({
+        container: document.querySelector('.sizes-types .sizes__list'),
+        items: 1,
+        autoplay: false,
+        axis: 'horizontal',
+        controls: true,
+        loop: false,
+        prevButton: document.querySelector('.swiper-button-prev'),
+        nextButton: document.querySelector('.swiper-button-next'),
+        autoplayButton: false,
+        autoplayButtonOutput: false,
+        mouseDrag: true,
+        nav: false,
+        preventScrollOnTouch: 'auto',
+        swipeAngle: false,
+    });
+};
+
+document.body.insertAdjacentHTML('beforeend',`
+<div class="popup-video">
+    <div class="popup-video_container">
+        <button type="button" class="btn-close"></button>
+    </div>
+</div>`);
+
+document.querySelector('.popup-video_container .btn-close').after(document.querySelector('.fluid-width-video-wrapper'));
+
+document.querySelector('.btn-close').addEventListener('click', () => {
+    document.querySelector('.popup-video').classList.remove('active')
+});
+
+let arrIcons = ['quality','ships','perfect-fit','pet','installation','washable','sanitized']
+
+for (let i = 0; i < document.querySelectorAll('.product-description__container li strong').length; i++) {
+    document.querySelectorAll('.product-description__container li strong')[i].setAttribute('style',`background-image: url(https://conversionratestore.github.io/projects/mammamiacovers/img/${arrIcons[i]}.svg)`)
+}
+
+document.querySelector('.product-single__content-text').insertAdjacentHTML('afterend',`
+<div class="measuring-guide">
+    <h2>Measuring guide</h2>
+    <p>Simply measure back side of your recliner</p>
+    <div class="measuring-guide_container"></div>
+    <div class="bottom">
+        <img src="https://conversionratestore.github.io/projects/mammamiacovers/img/measure.svg" alt="icon">
+        <p>Strech form fit fabric with elasticity of 120%</p>
+    </div>
+</div>
+<img class="img-free-returns" src="https://conversionratestore.github.io/projects/mammamiacovers/img/free-returns.png" alt="30 days free returns if size doesn't match">`);
+
+let mut = new MutationObserver(function (muts) {
+    if (document.querySelector('.sizes-types')) {
+        mut.disconnect();
+
+        document.querySelector('.sizes-types .sizes__list').insertAdjacentHTML('beforebegin',`
+        <ul class="sizes-info">
+            <li>Funiture type</li>
+            <li>Dimensions</li>
+            <li>Sanitized</li>
+            <li>Pet Friendly </li>
+            <li>Machine Washible</li>
+        </ul>`)
+
+        document.querySelector('.sizes-info').insertAdjacentHTML('afterend',`
+            <button class="swiper-button swiper-button-prev" type="button">
+                <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6.78856 1.07273L5.75893 0L0 6L5.75893 12L6.78856 10.9273L2.05925 6L6.78856 1.07273Z" fill="#212121"/>
+                </svg>
+            </button>
+            <button class="swiper-button swiper-button-next" type="button">
+                <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M-0.000470161 1.07273L1.02916 0L6.78809 6L1.02916 12L-0.000470161 10.9273L4.72883 6L-0.000470161 1.07273Z" fill="#212121"/>
+                </svg>
+            </button>`)
+
+        document.querySelectorAll('.sizes-types .sizes__item').forEach((el, i) => {
+            el.insertAdjacentHTML('afterbegin',`<p class="sizes__text">${el.querySelector('.sizes__text').innerText}</p>`)
+
+            el.querySelector('.sizes__link--image').insertAdjacentHTML('afterend',`
+                <a href="${el.querySelector('.sizes__link--image').getAttribute('href')}" class="btn-view">View</a>
+                <p class="name">${el.querySelector('.sizes__link--image').getAttribute('title').split(',')[0]}</p>`)
+            if (el.querySelector('.sizes__link--image').getAttribute('title').includes('(')) {
+                el.querySelector('.name').innerHTML = el.querySelector('.sizes__link--image').getAttribute('title').split('(')[0];
+            }
+            el.insertAdjacentHTML('beforeend',`
+                <span class="sizes__text wide"></span>
+                <span class="sizes__text"><img src="https://conversionratestore.github.io/projects/mammamiacovers/img/check.svg" alt="icon"></span>
+                <span class="sizes__text"><img src="https://conversionratestore.github.io/projects/mammamiacovers/img/check.svg" alt="icon"></span>
+                <span class="sizes__text"><img src="https://conversionratestore.github.io/projects/mammamiacovers/img/check.svg" alt="icon"></span>`)
+        })
+
+        tnsInitialization()
+    }
+})
+mut.observe(document, {
+    childList: true,
+    subtree: true
+});
