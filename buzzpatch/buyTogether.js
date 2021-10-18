dc39ad7edd7434fba947af306c56ce49bc12668a
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2021-10-18 14:29:04 +0300

    Update buyTogether.js

--- buzzpatch/buyTogether.js
+++ buzzpatch/buyTogether.js
@@ -179,10 +179,10 @@ let style = `
       </style>  
     `;
 let patchLinks = [
-    ['RB5EOORBafijZmGylohb', 'kTTOTJbbZ9ZzUt3uIzsG', 'vLWFoKhNuowErJ4w3UQa', 'Xi4NXSflSv8HtgyslK9X'],
-    ['07lzNEMSVz5nAcYFGDQt', '9P3eNacCDVh1onLRr5wt', 'wembOJsdtAXRXzu9G7PA', 'gGpMdZUWH43S24BdsClR'],
-    ['5t2W613N4shKrWPU3S0q', '9em4Mmn2IPw3sfh5YyvQ', 'r0mCczHs41UfeuQ1Se0j', 'rzgiqbdG1TjjjM2ljdmH'],
-    ['DlWIroch1aF0V4C9mg0W', 'mCBN3D0ebkI76nje0zxz', 'OevtrklIroSJWDtbtptU', '69noMHhBmQOE6gOuurad'],
+	['RB5EOORBafijZmGylohb', 'kTTOTJbbZ9ZzUt3uIzsG', 'vLWFoKhNuowErJ4w3UQa', 'Xi4NXSflSv8HtgyslK9X'],
+	['07lzNEMSVz5nAcYFGDQt', '9P3eNacCDVh1onLRr5wt', 'wembOJsdtAXRXzu9G7PA', 'gGpMdZUWH43S24BdsClR'],
+	['5t2W613N4shKrWPU3S0q', '9em4Mmn2IPw3sfh5YyvQ', 'r0mCczHs41UfeuQ1Se0j', 'rzgiqbdG1TjjjM2ljdmH'],
+	['DlWIroch1aF0V4C9mg0W', 'mCBN3D0ebkI76nje0zxz', 'OevtrklIroSJWDtbtptU', '69noMHhBmQOE6gOuurad'],
 ];
 
 let bundles = `
@@ -220,122 +220,122 @@ let bundles = `
     `;
 
 window.onload = function () {
-    document.body.insertAdjacentHTML('afterbegin', style);
+	document.body.insertAdjacentHTML('afterbegin', style);
 
-    document.querySelector('#purchase').insertAdjacentHTML('afterend', bundles);
+	document.querySelector('#purchase').insertAdjacentHTML('afterend', bundles);
 
-    document.querySelectorAll('.qty span.minus').forEach(item => {
-        item.addEventListener('click', function () {
-            let label = (this.closest('.prod').querySelector('.title').innerText);
-            window.dataLayer = window.dataLayer || [];
-            dataLayer.push({
-                'event': 'event-to-ga',
-                'eventCategory': 'Exp — PDP: Frequently Buy Together block',
-                'eventAction': 'Click on Minus button',
-                'eventLabel': label
-            });
+	document.querySelectorAll('.qty span.minus').forEach(item => {
+		item.addEventListener('click', function () {
+			let label = (this.closest('.prod').querySelector('.title').innerText);
+			window.dataLayer = window.dataLayer || [];
+			dataLayer.push({
+				'event': 'event-to-ga',
+				'eventCategory': 'Exp — PDP: Frequently Buy Together block',
+				'eventAction': 'Click on Minus button',
+				'eventLabel': label
+			});
 
-            let val = +item.nextElementSibling.value;
-            if (val > 1) {
-                item.nextElementSibling.value = val - 1;
-            }
-            if (this.closest('.prod')) {
-                setId(this);
-                totalSum();
-            }
-        });
-    });
+			let val = +item.nextElementSibling.value;
+			if (val > 1) {
+				item.nextElementSibling.value = val - 1;
+			}
+			if (this.closest('.prod')) {
+				setId(this);
+				totalSum();
+			}
+		});
+	});
 
-    document.querySelectorAll('.qty span.plus').forEach(item => {
-        item.addEventListener('click', function () {
-            let label = (this.closest('.prod').querySelector('.title').innerText);
-            window.dataLayer = window.dataLayer || [];
-            dataLayer.push({
-                'event': 'event-to-ga',
-                'eventCategory': 'Exp — PDP: Frequently Buy Together block',
-                'eventAction': 'Click on Plus button',
-                'eventLabel': label
-            });
+	document.querySelectorAll('.qty span.plus').forEach(item => {
+		item.addEventListener('click', function () {
+			let label = (this.closest('.prod').querySelector('.title').innerText);
+			window.dataLayer = window.dataLayer || [];
+			dataLayer.push({
+				'event': 'event-to-ga',
+				'eventCategory': 'Exp — PDP: Frequently Buy Together block',
+				'eventAction': 'Click on Plus button',
+				'eventLabel': label
+			});
 
-            let val = +item.previousElementSibling.value;
-            if (val < 4) {
-                item.previousElementSibling.value = val + 1;
-            }
-            if (this.closest('.prod')) {
-                setId(this);
-                totalSum();
-            }
-        });
-    });
+			let val = +item.previousElementSibling.value;
+			if (val < 4) {
+				item.previousElementSibling.value = val + 1;
+			}
+			if (this.closest('.prod')) {
+				setId(this);
+				totalSum();
+			}
+		});
+	});
 
-    document.querySelector('.total_sum+button').addEventListener('click', function () {
-        window.dataLayer = window.dataLayer || [];
-        dataLayer.push({
-            'event': 'event-to-ga',
-            'eventCategory': 'Exp — PDP: Frequently Buy Together block',
-            'eventAction': 'Click on Buy Both button',
-            'eventLabel': 'Frequently Bought Together'
-        });
+	document.querySelector('.total_sum+button').addEventListener('click', function () {
+		window.dataLayer = window.dataLayer || [];
+		dataLayer.push({
+			'event': 'event-to-ga',
+			'eventCategory': 'Exp — PDP: Frequently Buy Together block',
+			'eventAction': 'Click on Buy Both button',
+			'eventLabel': 'Frequently Bought Together'
+		});
 
-        let id1 = document.querySelector('.prod1').getAttribute('data-id');
-        let id2 = document.querySelector('.prod2').getAttribute('data-id');
+		let id1 = document.querySelector('.prod1').getAttribute('data-id');
+		let id2 = document.querySelector('.prod2').getAttribute('data-id');
 
-        let item1 = document.querySelector('.prod1 .qty input').value;
-        let item2 = document.querySelector('.prod2 .qty input').value;
+		let item1 = document.querySelector('.prod1 .qty input').value;
+		let item2 = document.querySelector('.prod2 .qty input').value;
 
-        window.open(`https://buzz-patch.myshopify.com/a/secure/checkout/${patchLinks[item1 - 1][item2 - 1]}`, '_self');
+		window.open(`https://buzz-patch.myshopify.com/a/secure/checkout/${patchLinks[item1 - 1][item2 - 1]}`, '_self');
 
-    });
+	});
 };
 let productsId = {
-    prod2: ['34137893142572', '39307585519660', '39264142393388', '39264134070316'],
-    prod1: ['34767547138092', '39307589058604', '39307593187372', '39307595546668']
+	prod2: ['34137893142572', '39307585519660', '39264142393388', '39264134070316'],
+	prod1: ['34767547138092', '39307589058604', '39307593187372', '39307595546668']
 };
 
 let packPrice = ['14.99', '13.5', '12', '10.5'];
 let price = ['14.99', '27', '36', '42'];
 
 function setId(a) {
-    let main = a.closest('.prod');
-    let qty = main.querySelector('input').value - 1;
-    let prod = (main.classList.contains('prod1')) ? 'prod1' : 'prod2';
+	let main = a.closest('.prod');
+	let qty = main.querySelector('input').value - 1;
+	let prod = (main.classList.contains('prod1')) ? 'prod1' : 'prod2';
 
-    main.setAttribute('data-id', productsId[prod][qty]);
-    main.setAttribute('data-price', price[qty]);
+	main.setAttribute('data-id', productsId[prod][qty]);
+	main.setAttribute('data-price', price[qty]);
 
-    main.querySelector('.price span').innerHTML = packPrice[qty];
-    if (prod === 'prod1') {
-        main.closest('.bundles').querySelector('.total_sum span:first-child').innerHTML = price[qty];
-    } else {
-        main.closest('.bundles').querySelector('.total_sum span:nth-child(2)').innerHTML = price[qty];
-    }
+	main.querySelector('.price span').innerHTML = packPrice[qty];
+	if (prod === 'prod1') {
+		main.closest('.bundles').querySelector('.total_sum span:first-child').innerHTML = price[qty];
+	} else {
+		main.closest('.bundles').querySelector('.total_sum span:nth-child(2)').innerHTML = price[qty];
+	}
 }
 
 function totalSum() {
-    let p1 = document.querySelector('.prod1').getAttribute('data-price');
-    let p2 = document.querySelector('.prod2').getAttribute('data-price');
-    document.querySelector('.total_sum span:last-child').innerHTML = `$${(+p1 + +p2).toFixed(2)}`;
+	let p1 = document.querySelector('.prod1').getAttribute('data-price');
+	let p2 = document.querySelector('.prod2').getAttribute('data-price');
+	document.querySelector('.total_sum span:last-child').innerHTML = `$${(+p1 + +p2).toFixed(2)}`;
 }
 
 (function (h, o, t, j, a, r) {
-    h.hj = h.hj || function () {
-        (h.hj.q = h.hj.q || []).push(arguments);
-    };
-    h._hjSettings = {hjid: 2247058, hjsv: 6};
-    a = o.getElementsByTagName('head')[0];
-    r = o.createElement('script');
-    r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    a.appendChild(r);
+	h.hj = h.hj || function () {
+		(h.hj.q = h.hj.q || []).push(arguments);
+	};
+	h._hjSettings = {hjid: 2247058, hjsv: 6};
+	a = o.getElementsByTagName('head')[0];
+	r = o.createElement('script');
+	r.async = 1;
+	r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+	a.appendChild(r);
 })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
 window.hj = window.hj || function () {
-    (hj.q = hj.q || []).push(arguments);
+	(hj.q = hj.q || []).push(arguments);
 };
-hj('trigger', 'frequently_buy_together_block');
+hj('event', 'frequently_buy_together_block');
 
 window.dataLayer = window.dataLayer || [];
 dataLayer.push({
-    'event': 'event-to-ga',
-    'eventCategory': 'Exp — PDP: Frequently Buy Together block',
-    'eventAction': 'loaded'
+	'event': 'event-to-ga',
+	'eventCategory': 'Exp — PDP: Frequently Buy Together block',
+	'eventAction': 'loaded'
 });
