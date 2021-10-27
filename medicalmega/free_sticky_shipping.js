00c37bf286b11ac406d916632c5cbe0bf06ae4a6
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-10-27 18:54:09 +0300

    Update free_sticky_shipping.js

--- medicalmega/free_sticky_shipping.js
+++ medicalmega/free_sticky_shipping.js
@@ -61,30 +61,29 @@ window.onload  = function () {
 
     //create range shipping
     function rangeShipping(item,insert) {
-        if (JSON.parse(localStorage.getItem('yourOrder'))[0].price != '0.00') {
-            let total = JSON.parse(localStorage.getItem('yourOrder'))[0].price;
-            document.querySelector(item).insertAdjacentHTML(insert, `
-            <div class="range_shipping">
-                <div class="range_shipping_right">
-                    <div class="flex_center_between">
-                        <p class="your_order"><span>Your Order: </span> ${total}</p> 
-                        <p class="left_for">
-                            <span>$${(150 - total).toFixed(2)} </span> 
-                            left for free delivery
-                        </p>
-                    </div>
-                    <div class="range">
-                        <span style="width: ${total * 100 / 150 + '%'}"></span>
-                    </div>
+        let total = JSON.parse(localStorage.getItem('yourOrder'))[0].price;
+        document.querySelector(item).insertAdjacentHTML(insert, `
+        <div class="range_shipping">
+            <div class="range_shipping_right">
+                <div class="flex_center_between">
+                    <p class="your_order"><span>Your Order: </span> ${total}</p> 
+                    <p class="left_for">
+                        <span>$${(150 - total).toFixed(2)} </span> 
+                        left for free delivery
+                    </p>
                 </div>
-            </div> `);
-            if (total < 150 && total >= 130) {
-                document.querySelector('.left_for').innerHTML = `<span>$${(150 - total).toFixed(2)} </span>  only left for free delivery`;
-            }
-            if (total >= 150) {
-                document.querySelector('.left_for').innerHTML = `You Have Free Shipping`
-            }
+                <div class="range">
+                    <span style="width: ${total * 100 / 150 + '%'}"></span>
+                </div>
+            </div>
+        </div> `);
+        if (total < 150 && total >= 130) {
+            document.querySelector('.left_for').innerHTML = `<span>$${(150 - total).toFixed(2)} </span>  only left for free delivery`;
         }
+        if (total >= 150) {
+            document.querySelector('.left_for').innerHTML = `You Have Free Shipping`
+        }
+        
     }
 
     //cart
@@ -140,45 +139,47 @@ window.onload  = function () {
     //PDP
     if (location.pathname.includes('/product')) {   
         document.body.insertAdjacentHTML('afterbegin',`
-    <style>
-        .range_shipping {
-            padding-left: 20px;
-            padding-right: 10px;
-            margin-bottom: 28px;
+        <style>
+            .range_shipping {
+                padding-left: 20px;
+                padding-right: 10px;
+                margin-bottom: 28px;
+            }
+            .shipping_box {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border: 1px solid #666666;
+                border-radius: 4px;
+                padding: 16px;
+                width: 252px;
+                margin: 0 auto 20px;
+                font-family: 'Arial', sans-serif;
+                box-sizing: border-box;
+            }
+            .shipping_box img {
+                width: 35px;
+                height: 26px;
+                object-fit: contain;
+                margin-right: 12px;
+            }
+            .shipping_box_title {
+                font-weight: bold;
+                font-size: 14px;
+                line-height: 16px;
+                color: #C23D31;
+                margin: 0;
+            }
+            .shipping_box_text {
+                font-size: 12px;
+                line-height: 16px;
+                color: #666666;
+                margin: 0;
+            }
+        </style>`)
+        if (localStorage.getItem('yourOrder') != null && JSON.parse(localStorage.getItem('yourOrder'))[0].price != '0.00') {
+            rangeShipping('.topcon','beforebegin');
         }
-        .shipping_box {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            border: 1px solid #666666;
-            border-radius: 4px;
-            padding: 16px;
-            width: 252px;
-            margin: 0 auto 20px;
-            font-family: 'Arial', sans-serif;
-            box-sizing: border-box;
-        }
-        .shipping_box img {
-            width: 35px;
-            height: 26px;
-            object-fit: contain;
-            margin-right: 12px;
-        }
-        .shipping_box_title {
-            font-weight: bold;
-            font-size: 14px;
-            line-height: 16px;
-            color: #C23D31;
-            margin: 0;
-        }
-        .shipping_box_text {
-            font-size: 12px;
-            line-height: 16px;
-            color: #666666;
-            margin: 0;
-        }
-    </style>`)
-        rangeShipping('.topcon','beforebegin');
         window.dataLayer = window.dataLayer || [];
         dataLayer.push({
          'event': 'event-to-ga',
@@ -228,7 +229,9 @@ window.onload  = function () {
                 padding: 0 18px 20px;
             }
         </style>`)
-        rangeShipping('.homeslider__container','afterend');
+        if (localStorage.getItem('yourOrder') != null && JSON.parse(localStorage.getItem('yourOrder'))[0].price != '0.00') {
+            rangeShipping('.homeslider__container','afterend');
+        }
         window.dataLayer = window.dataLayer || [];
         dataLayer.push({
          'event': 'event-to-ga',
