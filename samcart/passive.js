554b019c247e7c5e8bb1c94fe531f1bfe657bda6
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-10-28 14:10:40 +0300

    Update passive.js

--- samcart/passive.js
+++ samcart/passive.js
@@ -1007,6 +1007,7 @@ btn.forEach((btn) => {
             dataLayerPush('Click on I`m interested button','Launch in 7 days')
             document.querySelector('.popup .content').innerHTML = creatBlock('much-more','Launch in 7 days (from scratch) without a team or complicated tech') + createlistInfo('The secret to building the "laptop lifestyle"','Simplify your sales process for better results','Rapidly increase your conversions, sales, and revenue');
             showPopup('Launch in 7 days');
+            showFieldName();
             document.querySelector(`.popup .field-name input`).addEventListener('click', (event) => {
                 event.stopImmediatePropagation()
                 dataLayerPush('Click on Your name input', `Popup: Save Your Spot (Launch in 7 days)`);
@@ -1028,6 +1029,7 @@ btn.forEach((btn) => {
             dataLayerPush('Click on I`m interested button','Get (virtually) unlimited traffic')
             document.querySelector('.popup .content').innerHTML = creatBlock('bar-chart',`Get (virtually) unlimited traffic from today's #1 traffic source`) + createlistInfo('How to get more people to your course page','Generate dozens of new sales every day',`Secret tool to ethically steal your competitors' ads`);
             showPopup('Get (virtually) unlimited traffic');
+            showFieldName();
             document.querySelector(`.popup .field-name input`).addEventListener('click', (event) => {
                 event.stopImmediatePropagation()
                 dataLayerPush('Click on Your name input', `Popup: Save Your Spot (Get (virtually) unlimited traffic)`);
@@ -1049,6 +1051,7 @@ btn.forEach((btn) => {
             dataLayerPush('Click on I`m interested button','Generate sales around the clock')
             document.querySelector('.popup .content').innerHTML = creatBlock('sales','Generate sales around the clock using a simple "1 page funnel"') + createlistInfo('Convince anyone to buy your product','Best way to start your online business','Core 4 elements your page needs to sell');
             showPopup('Generate sales around the clock');
+            showFieldName();
             document.querySelector(`.popup .field-name input`).addEventListener('click', (event) => {
                 event.stopImmediatePropagation()
                 dataLayerPush('Click on Your name input', `Popup: Save Your Spot (Generate sales around the clock)`);
@@ -1070,6 +1073,7 @@ btn.forEach((btn) => {
             dataLayerPush('Click on I`m interested button','Become a trusted expert ')
             document.querySelector('.popup .content').innerHTML = creatBlock('expert','Become a trusted expert in a niche using the "1 Step Rule"') + createlistInfo('How to get people to listen to you','Discover what to make your course about','The secret to creating a course people want to buy');
             showPopup('Become a trusted expert');
+            showFieldName()
             document.querySelector(`.popup .field-name input`).addEventListener('click', (event) => {
                 event.stopImmediatePropagation()
                 dataLayerPush('Click on Your name input', `Popup: Save Your Spot (Become a trusted expert)`);
@@ -1090,7 +1094,7 @@ btn.forEach((btn) => {
         if (getAttr == 'much-more' || getAttr == 'bar-chart' || getAttr == 'sales' || getAttr == 'expert') {
             if (!document.querySelector('.popup-interested')) {
                 document.querySelector('.popup').classList.add('popup-interested');
-                showFieldName()
+
                 document.querySelector('.popup-interested .field-name').insertAdjacentHTML('afterend', createElementEmail)
                 document.querySelector('.popup-interested .justify-content-between').after(document.querySelector('.popup-interested .btn-orange'))
             }
