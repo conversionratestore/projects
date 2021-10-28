ec5e8469a8aa6666ac9dcfa482c41eb2f81a0bf8
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-10-28 12:44:51 +0300

    Update passive.js

--- samcart/passive.js
+++ samcart/passive.js
@@ -1173,7 +1173,7 @@ if (detectMob() == false) {
 }
 
 let mut = new MutationObserver(function (muts) {
-    if (document.querySelector('.popup .field-name input')) {
+    if (document.querySelector(`.popup.active .field-name input`)) {
         mut.disconnect();
         let getAttr = document.querySelector('.popup').getAttribute('data-popup');
         console.log(getAttr + '1')
