257cdea27d62d44bab05d0e3a1e0797b22da1c64
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-10-29 16:31:28 +0300

    Update passive.js

--- samcart/passive.js
+++ samcart/passive.js
@@ -947,9 +947,16 @@ function postForm(name,email,time,sales) {
         })
     }).then(res => res.json()).then(data => {
         console.log(data)
-        console.log(data["short_id"])
-        console.log(data["id"])
-        window.location.href = `https://webclass.samcart.com/t/0e7aJr?id=${data["id"]}` //https://webclass.samcart.com/t/0e7aJr?id=${data["short_id"]} https://joinnow.live/embed/s/0e7aJr?layout=simple-0 `https://webclass.samcart.com/a/0e7aJr` //https://joinnow.live/t/TSa5s8?id=jyjVL
+        window.location.href = `https://webclass.samcart.com/a/0e7aJr?id=${data["short_id"]["attendee"]["short_id"]}`
+        fetch('https://api.joinnow.live/webinars/0e7aJr/registration-information', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: "GET",
+        }).then(res => res.json()).then(dataWebinars => {
+            console.log(dataWebinars)
+        })
+
     }).catch(err => {
         console.log('Failed fetch ', err);
     });
