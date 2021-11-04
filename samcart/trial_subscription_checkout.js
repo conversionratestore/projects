e2e09b062b0e0ce544871d88c2fc4f1c322325db
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-11-04 14:04:18 +0200

    Update trial_subscription_checkout.js

--- samcart/trial_subscription_checkout.js
+++ samcart/trial_subscription_checkout.js
@@ -304,25 +304,6 @@ window.onload  = function () {
             pushDataLayer(action,label);
             
             if (document.querySelector('.card-num-row')) {
-                document.querySelector('.card-num-row').addEventListener('click', (e) => {
-                    e.stopImmediatePropagation()
-                    console.log('click')
-                    action =  'Click on input Card Number',
-                    label = 'Contact form';
-                    pushDataLayer(action, label)
-                })
-                document.querySelector('.cvv-col').addEventListener('click', (e) => {
-                    e.stopImmediatePropagation()
-                    action =  'Click on input CVC',
-                    label = 'Contact form';
-                    pushDataLayer(action, label)
-                })
-                document.querySelector('.exp-col').addEventListener('click', (e) => {
-                    e.stopImmediatePropagation()
-                    action =  'Click on input MM YY',
-                    label = 'Contact form';
-                    pushDataLayer(action, label)
-                })
                 document.querySelector('#placeOrder').addEventListener('click', (e) => {
                     e.stopImmediatePropagation()
                     action =  'Click on Place Order Now button',
@@ -377,6 +358,7 @@ window.onload  = function () {
             action = 'Click on Credit Card payment method';
             pushDataLayer(action);
         })
+
         document.querySelector('[name="fname"]').addEventListener('click', () => {
             action =  'Click on input First Name',
             label = 'Contact form';
@@ -414,6 +396,25 @@ window.onload  = function () {
                 pushDataLayer(action, label)
             })
         }
+        document.querySelectorAll('.custom-control.custom-radio input')[0].addEventListener('click', () => {
+            if (document.querySelector('#card-number')) {
+                document.querySelector('.card-num-row').addEventListener('click', (e) => {
+                    action =  'Click on input Card Number',
+                    label = 'Contact form';
+                    pushDataLayer(action, label)
+                })
+                document.querySelector('.cvv-col').addEventListener('click', (e) => {
+                    action =  'Click on input CVC',
+                    label = 'Contact form';
+                    pushDataLayer(action, label)
+                })
+                document.querySelector('.exp-col').addEventListener('click', (e) => {
+                    action =  'Click on input MM YY',
+                    label = 'Contact form';
+                    pushDataLayer(action, label)
+                })
+            }
+        })
         document.querySelector('.tpl-6__offer__checkbox.custom-control.custom-checkbox').addEventListener('click', (e) => {
             e.stopImmediatePropagation();
             action = 'Click on Yes add the Facebook checkbox';
