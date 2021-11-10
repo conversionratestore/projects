cd696c180ca07037f9013f46ddd42912239aff53
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2021-11-10 19:55:38 +0200

    Update popup.js

--- uplead/popup.js
+++ uplead/popup.js
@@ -6,41 +6,7 @@
 // 	'available_credits_in_month': 1000,
 // 	'credits_left': 250,
 // })
-
-let btnInterval = setInterval(() => {
-	if (document.querySelector('.billing-switch [aria-checked=false]')) {
-		clearInterval(btnInterval)
-		document.querySelector('.billing-switch [aria-checked=false]').click()
-	}
-}, 500)
-
-let start = setInterval(() => {
-	if (window.google_tag_manager['GTM-MTN4VBZ']?.dataLayer?.get('bannerType')) {
-		clearInterval(start)
-
-		const percent = window.google_tag_manager['GTM-MTN4VBZ'].dataLayer.get('bannerType').split('percent')[1]
-		const plan = window.google_tag_manager['GTM-MTN4VBZ'].dataLayer.get('plan').toLowerCase()
-		let save
-
-		switch (plan) {
-			case 'essentials':
-				save = 298
-				break
-			case 'plus':
-				save = 598
-				break
-			case 'professional':
-				save = 1198
-				break
-			case 'elite':
-				save = 1798
-				break
-			default:
-				save = 0
-				break
-		}
-
-		const style = `
+const style = `
 	<style>
 		.modal-custom {
 			padding: 20px 0;
@@ -186,7 +152,40 @@ let start = setInterval(() => {
 		}
 	</style>
 `
-		const page = `
+
+let btnInterval = setInterval(() => {
+	if (document.querySelector('.billing-switch [aria-checked=false]')) {
+		clearInterval(btnInterval)
+		document.querySelector('.billing-switch [aria-checked=false]').click()
+	}
+}, 500)
+
+let show75 = setInterval(() => {
+	if (window.google_tag_manager['GTM-MTN4VBZ']?.dataLayer?.get('bannerType') === 'percent75') {
+		clearInterval(show75)
+
+		showPopup()
+	}
+}, 200)
+
+let show90 = setInterval(() => {
+	if (window.google_tag_manager['GTM-MTN4VBZ']?.dataLayer?.get('bannerType') === 'percent90') {
+		clearInterval(show90)
+
+		showPopup()
+	}
+}, 200)
+
+let show100 = setInterval(() => {
+	if (window.google_tag_manager['GTM-MTN4VBZ']?.dataLayer?.get('bannerType') === 'percent100') {
+		clearInterval(show100)
+
+		showPopup()
+	}
+}, 200)
+
+function returnPage(percent, save) {
+	return `
 <div class="modal-custom modal-custom_active">
 	<div class="popup-custom">
 	    <img class="clock" src="https://conversionratestore.github.io/projects/uplead/img/pig-icon.svg" alt="money box">
@@ -199,74 +198,97 @@ let start = setInterval(() => {
 	    <svg xmlns="http://www.w3.org/2000/svg" fill="#6E7191" viewBox="0 0 47.971 47.971" data-src="/c69c1c810022c4cf0e39706564b864d5.svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M28.228 23.986L47.092 5.122a3 3 0 0 0 0-4.242 3 3 0 0 0-4.242 0L23.986 19.744 5.12.88a3 3 0 0 0-4.242 0 3 3 0 0 0 0 4.242l18.865 18.864L.88 42.85a3 3 0 0 0 0 4.242c.586.585 1.354.878 2.12.878a2.99 2.99 0 0 0 2.121-.879l18.865-18.864L42.85 47.09a2.99 2.99 0 0 0 4.242 0 3 3 0 0 0 0-4.242L28.228 23.986z"></path></svg>  		
 	</div>
 </div>`
+}
 
-		document.head.insertAdjacentHTML('afterbegin', style)
-		document.body.insertAdjacentHTML('beforeend', page)
+function showPopup() {
+	const percent = window.google_tag_manager['GTM-MTN4VBZ'].dataLayer.get('bannerType').split('percent')[1]
+	const plan = window.google_tag_manager['GTM-MTN4VBZ'].dataLayer.get('plan').toLowerCase()
+	let save
 
-		const btn = document.querySelector('.btn-custom')
+	switch (plan) {
+		case 'essentials':
+			save = 298
+			break
+		case 'plus':
+			save = 598
+			break
+		case 'professional':
+			save = 1198
+			break
+		case 'elite':
+			save = 1798
+			break
+		default:
+			save = 0
+			break
+	}
 
-		document.addEventListener('click', closeModal)
-		btn.addEventListener('click', clickOnBtn)
+	
+	document.head.insertAdjacentHTML('afterbegin', style)
+	document.body.insertAdjacentHTML('beforeend', returnPage(percent, plan))
 
-		function closeModal(e) {
-			if (e.target.matches('.modal-custom') || e.target.matches('.popup-custom svg')) {
-				document.querySelector('.modal-custom').classList.remove('modal-custom_active')
+	const btn = document.querySelector('.btn-custom')
 
-				if (e.target.matches('.modal-custom')) {
-					window.dataLayer = window.dataLayer || []
-					dataLayer.push({
-						'event': 'event-to-ga',
-						'eventCategory': 'Exp — Pop up with motivation to upgrade',
-						'eventAction': 'Click on space out of pop up',
-					})
-				}
+	document.addEventListener('click', closeModal)
+	btn.addEventListener('click', clickOnBtn)
 
-				if (e.target.matches('.popup-custom svg')) {
-					window.dataLayer = window.dataLayer || []
-					dataLayer.push({
-						'event': 'event-to-ga',
-						'eventCategory': 'Exp — Pop up with motivation to upgrade',
-						'eventAction': 'Click on X to close pop up',
-					})
-				}
-			}
-		}
+	window.dataLayer = window.dataLayer || []
+	dataLayer.push({
+		'event': 'event-to-ga',
+		'eventCategory': 'Exp — Pop up with motivation to upgrade',
+		'eventAction': 'loaded',
+	})
+}
 
-		function clickOnBtn() {
-			location.href = '/subscriptions'
+function closeModal(e) {
+	if (e.target.matches('.modal-custom') || e.target.matches('.popup-custom svg')) {
+		document.querySelector('.modal-custom').classList.remove('modal-custom_active')
 
+		if (e.target.matches('.modal-custom')) {
 			window.dataLayer = window.dataLayer || []
 			dataLayer.push({
 				'event': 'event-to-ga',
 				'eventCategory': 'Exp — Pop up with motivation to upgrade',
-				'eventAction': 'Click on Save Button',
+				'eventAction': 'Click on space out of pop up',
 			})
 		}
 
-		(function (h, o, t, j, a, r) {
-			h.hj = h.hj || function () {
-				(h.hj.q = h.hj.q || []).push(arguments)
-			}
-			h._hjSettings = {hjid: 2615465, hjsv: 6}
-			a = o.getElementsByTagName('head')[0]
-			r = o.createElement('script')
-			r.async = 1
-			r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
-			a.appendChild(r)
-		})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
-		window.hj = window.hj || function () {
-			(hj.q = hj.q || []).push(arguments)
+		if (e.target.matches('.popup-custom svg')) {
+			window.dataLayer = window.dataLayer || []
+			dataLayer.push({
+				'event': 'event-to-ga',
+				'eventCategory': 'Exp — Pop up with motivation to upgrade',
+				'eventAction': 'Click on X to close pop up',
+			})
 		}
-		hj('trigger', 'tv_popup_with_motivation_to_upgrade')
-		hj('event', 'popup_with_motivation_to_upgrade')
-
-
-		window.dataLayer = window.dataLayer || []
-		dataLayer.push({
-			'event': 'event-to-ga',
-			'eventCategory': 'Exp — Pop up with motivation to upgrade',
-			'eventAction': 'loaded',
-		})
 	}
-}, 200)
+}
+
+function clickOnBtn() {
+	location.href = '/subscriptions'
+
+	window.dataLayer = window.dataLayer || []
+	dataLayer.push({
+		'event': 'event-to-ga',
+		'eventCategory': 'Exp — Pop up with motivation to upgrade',
+		'eventAction': 'Click on Save Button',
+	})
+}
+
+;(function (h, o, t, j, a, r) {
+	h.hj = h.hj || function () {
+		(h.hj.q = h.hj.q || []).push(arguments)
+	}
+	h._hjSettings = {hjid: 2615465, hjsv: 6}
+	a = o.getElementsByTagName('head')[0]
+	r = o.createElement('script')
+	r.async = 1
+	r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+	a.appendChild(r)
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+window.hj = window.hj || function () {
+	(hj.q = hj.q || []).push(arguments)
+}
+hj('trigger', 'tv_popup_with_motivation_to_upgrade')
+hj('event', 'popup_with_motivation_to_upgrade')
 
