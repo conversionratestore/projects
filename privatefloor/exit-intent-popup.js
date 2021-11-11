fb344421f330682ad9e04ee684875e8c7d13bc91
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-11-11 10:55:35 +0200

    Update exit-intent-popup.js

--- privatefloor/exit-intent-popup.js
+++ privatefloor/exit-intent-popup.js
@@ -643,7 +643,6 @@ let mut = new MutationObserver(function (muts) {
             // let runInterval = setInterval(() => {
             //     clearInterval(runInterval);
 
-                products = [];
                 document.querySelectorAll('.item').forEach((item) => {
                     let imgUrl = item.querySelector('.preview img').getAttribute('src'),
                         name = item.querySelector('.title').innerText.split('\n')[0],
