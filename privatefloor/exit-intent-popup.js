6afae1e7dd88815989c84d02c22649a35fd8d3cf
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-11-11 12:53:23 +0200

    Update exit-intent-popup.js

--- privatefloor/exit-intent-popup.js
+++ privatefloor/exit-intent-popup.js
@@ -602,8 +602,8 @@ let mut = new MutationObserver(function (muts) {
                             if (productsLocalStorage[i].id === id) {
                                 qty = +productsLocalStorage[i].qty + +qty;
                                 console.log('qty2: ' + qty)
-                                console.log('productsLocalStorage: ' + productsLocalStorage)
-                                console.log('products: ' + products)
+                                console.log('productsLocalStorage: ' + productsLocalStorage[i])
+                                console.log('products: ' + products[i])
                                 spliceProduct(productsLocalStorage,i)
                             }
                             console.log('qty3: ' + qty)
