39657f80590cf229a23938ab53f52cf6b448ae46
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-11-12 11:07:09 +0200

    Update exit-intent-popup.js

--- privatefloor/exit-intent-popup.js
+++ privatefloor/exit-intent-popup.js
@@ -607,7 +607,7 @@ let mut = new MutationObserver(function (muts) {
                 mut.disconnect()
                 document.querySelector('#btn-add-item-cart').addEventListener('click', (e) => {
                     e.stopImmediatePropagation()
-                    setPreviewAddToCart(document.querySelector('.slide.selected img'), response);
+                    setPreviewAddToCart(document.querySelector('.slide.selected img'), response.body);
                     let imgUrl = document.querySelector('.slide.selected img').getAttribute('data-mobile-src'),
                         name = document.querySelector('.slide.selected img').getAttribute('data-name-gtm'),
                         price = document.querySelector('.slide.selected img').getAttribute('data-price-gtm'),
