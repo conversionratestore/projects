0b80e50798444e43d9a1942528e43b978bfef140
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-11-17 13:00:04 +0200

    Update reorder-prevViewed.js

--- medicalmega/reorder-prevViewed.js
+++ medicalmega/reorder-prevViewed.js
@@ -627,7 +627,7 @@ mut.observe(document, optionMut);
 
 (function(h,o,t,j,a,r){
     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:1699330,hjsv:6};
+    h._hjSettings={hjid:1483840,hjsv:6};
     a=o.getElementsByTagName('head')[0];
     r=o.createElement('script');r.async=1;
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
