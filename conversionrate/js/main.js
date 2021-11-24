6ef5fab4af057a7ffc0009436a94c69d15bd1df8
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-11-24 16:58:59 +0200

    Update main.js

--- conversionrate/js/main.js
+++ conversionrate/js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+let metrics = document.querySelectorAll('.item_metric:nth-child(4n+4)'),
+    accordionItem = document.querySelectorAll('.accordion_item'),
+    plusBtn = document.querySelectorAll('.accordion_plus'),
+    accordionDrop = document.querySelectorAll('.accordion_drop');
+
+for (let i = 0; i < metrics.length; i++) {
+    metrics[i].insertAdjacentHTML('afterend','<div class="pre"></div>')
+}
+
+for (let i = 0; i < accordionItem.length; i++) {
+    accordionItem[i].addEventListener('click', () => {
+        plusBtn[i].classList.toggle('active');
+        accordionDrop[i].classList.toggle('active');
+    })
+}
