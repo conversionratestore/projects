d2d254d263ef09481cfb2779a6ee634ccd54f2f2
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-12-02 18:18:49 +0200

    Update checkout.js

--- medicalmega/checkout.js
+++ medicalmega/checkout.js
@@ -965,7 +965,7 @@ window.onload  = function () {
                 });
 
                 document.querySelectorAll('.num_line a')[1].querySelectorAll('span')[0].classList.add('circle_dark');
-                document.querySelectorAll('.num_line a')[1].querySelectorAll('span')[2].classList.remove('pink');
+                document.querySelectorAll('.num_line a')[1].querySelectorAll('span')[2].classList.add('pink');
                 document.querySelectorAll('.num_line a')[2].querySelectorAll('span')[0].classList.add('circle_pink');
                 document.querySelectorAll('.num_line a')[2].querySelectorAll('span')[2].classList.add('pink');
             }
