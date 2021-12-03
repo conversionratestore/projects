5da3b3015c23246c09db398aacfc2ad9d873f0c7
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-12-03 12:05:15 +0200

    Update signaturely_billing.js

--- signaturely/signaturely_billing.js
+++ signaturely/signaturely_billing.js
@@ -1,67 +1,64 @@
-checked()
-function checked() {
-  let checked = setInterval(() => {
-    if (document.querySelector(".react-toggle-screenreader-only")) {
-      clearInterval(checked)
-      if (!document.querySelector(".react-toggle-screenreader-only:checked")) {
-        document.querySelector(".react-toggle-screenreader-only").click()
-      }
+let checked = setInterval(() => {
+  if (document.querySelector(".react-toggle-screenreader-only")) {
+    clearInterval(checked)
+    if (!document.querySelector(".react-toggle-screenreader-only:checked")) {
+      document.querySelector(".react-toggle-screenreader-only").click()
     }
-  }, 50)
+  }
+}, 50)
 
-  window.dataLayer = window.dataLayer || []
-  dataLayer.push({
-    event: "event-to-ga",
-    eventCategory: "Exp - Signaturely toggle switch",
-    eventAction: "loaded",
-  })
-  ;(function (h, o, t, j, a, r) {
-    h.hj =
-      h.hj ||
-      function () {
-        ;(h.hj.q = h.hj.q || []).push(arguments)
-      }
-    h._hjSettings = { hjid: 2372209, hjsv: 6 }
-    a = o.getElementsByTagName("head")[0]
-    r = o.createElement("script")
-    r.async = 1
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
-    a.appendChild(r)
-  })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=")
-  window.hj =
-    window.hj ||
+window.dataLayer = window.dataLayer || []
+dataLayer.push({
+  event: "event-to-ga",
+  eventCategory: "Exp - Signaturely toggle switch",
+  eventAction: "loaded",
+})
+;(function (h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
     function () {
-      ;(hj.q = hj.q || []).push(arguments)
+      ;(h.hj.q = h.hj.q || []).push(arguments)
     }
-  hj("event", "signaturely_toggle_switch")
+  h._hjSettings = { hjid: 2372209, hjsv: 6 }
+  a = o.getElementsByTagName("head")[0]
+  r = o.createElement("script")
+  r.async = 1
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+  a.appendChild(r)
+})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=")
+window.hj =
+  window.hj ||
+  function () {
+    ;(hj.q = hj.q || []).push(arguments)
+  }
+hj("event", "signaturely_toggle_switch")
 
-  setTimeout(() => {
-    if (document.querySelector(".react-toggle-screenreader-only")) {
-      document.querySelector(".react-toggle-screenreader-only").addEventListener("click", () => {
-        window.dataLayer = window.dataLayer || []
-        dataLayer.push({
-          event: "event-to-ga",
-          eventCategory: "Exp - Signaturely toggle switch",
-          eventAction: "Click on switcher",
-        })
+setTimeout(() => {
+  if (document.querySelector(".react-toggle-screenreader-only")) {
+    document.querySelector(".react-toggle-screenreader-only").addEventListener("click", () => {
+      window.dataLayer = window.dataLayer || []
+      dataLayer.push({
+        event: "event-to-ga",
+        eventCategory: "Exp - Signaturely toggle switch",
+        eventAction: "Click on switcher",
       })
-    }
-  }, 50)
-}
+    })
+  }
+}, 50)
 
 //MutationObserver
 let observer = new MutationObserver(() => {
   if (document.querySelector(".button.button--primary")) {
     observer.disconnect()
-    checked()
-    observer.observe(document.querySelector(".button.button--primary"), {
+    document.querySelector(".react-toggle-screenreader-only").click()
+    observer.observe(document, {
       childList: true,
       subtree: true,
     })
   }
 })
 
-observer.observe(document.querySelector(".button.button--primary"), {
+observer.observe(document, {
   childList: true,
   subtree: true,
 })
