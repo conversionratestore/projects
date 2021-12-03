e7eea35517b2f22c6f0e4ac6a6e34ed07658bd39
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-12-03 14:02:53 +0200

    Update signaturely_billing.js

--- signaturely/signaturely_billing.js
+++ signaturely/signaturely_billing.js
@@ -1,61 +1,63 @@
-start()
+if ((document.URL = "https://app.signaturely.com/settings/billing/plan")) {
+  start()
 
-function start() {
-  let checked = setInterval(() => {
-    if (document.querySelector(".react-toggle-screenreader-only")) {
-      clearInterval(checked)
-      if (!document.querySelector(".react-toggle-screenreader-only:checked")) {
-        document.querySelector(".react-toggle-screenreader-only").click()
+  function start() {
+    let checked = setInterval(() => {
+      if (document.querySelector(".react-toggle-screenreader-only")) {
+        clearInterval(checked)
+        if (!document.querySelector(".react-toggle-screenreader-only:checked")) {
+          document.querySelector(".react-toggle-screenreader-only").click()
 
-        document.querySelector(".react-toggle-screenreader-only").removeEventListener("click", toggleSwitch)
-        document.querySelector(".react-toggle-screenreader-only").addEventListener("click", toggleSwitch)
+          document.querySelector(".react-toggle-screenreader-only").removeEventListener("click", toggleSwitch)
+          document.querySelector(".react-toggle-screenreader-only").addEventListener("click", toggleSwitch)
+        }
       }
-    }
-  }, 50)
-}
-
-function toggleSwitch() {
-  if (document.querySelector(".react-toggle-screenreader-only")) {
-    window.dataLayer = window.dataLayer || []
-    dataLayer.push({
-      event: "event-to-ga",
-      eventCategory: "Exp - Signaturely toggle switch",
-      eventAction: "Click on switcher",
-    })
+    }, 50)
   }
-}
 
-window.dataLayer = window.dataLayer || []
-dataLayer.push({
-  event: "event-to-ga",
-  eventCategory: "Exp - Signaturely toggle switch",
-  eventAction: "loaded",
-})
-;(function (h, o, t, j, a, r) {
-  h.hj =
-    h.hj ||
+  function toggleSwitch() {
+    if (document.querySelector(".react-toggle-screenreader-only")) {
+      window.dataLayer = window.dataLayer || []
+      dataLayer.push({
+        event: "event-to-ga",
+        eventCategory: "Exp - Signaturely toggle switch",
+        eventAction: "Click on switcher",
+      })
+    }
+  }
+
+  window.dataLayer = window.dataLayer || []
+  dataLayer.push({
+    event: "event-to-ga",
+    eventCategory: "Exp - Signaturely toggle switch",
+    eventAction: "loaded",
+  })
+  ;(function (h, o, t, j, a, r) {
+    h.hj =
+      h.hj ||
+      function () {
+        ;(h.hj.q = h.hj.q || []).push(arguments)
+      }
+    h._hjSettings = { hjid: 2372209, hjsv: 6 }
+    a = o.getElementsByTagName("head")[0]
+    r = o.createElement("script")
+    r.async = 1
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+    a.appendChild(r)
+  })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=")
+  window.hj =
+    window.hj ||
     function () {
-      ;(h.hj.q = h.hj.q || []).push(arguments)
+      ;(hj.q = hj.q || []).push(arguments)
     }
-  h._hjSettings = { hjid: 2372209, hjsv: 6 }
-  a = o.getElementsByTagName("head")[0]
-  r = o.createElement("script")
-  r.async = 1
-  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
-  a.appendChild(r)
-})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=")
-window.hj =
-  window.hj ||
-  function () {
-    ;(hj.q = hj.q || []).push(arguments)
-  }
-hj("event", "signaturely_toggle_switch")
+  hj("event", "signaturely_toggle_switch")
 
-let onClickBtn = setInterval(() => {
-  if (document.querySelector(".header__month-wrapper .button.button--primary")) {
-    clearInterval(onClickBtn)
-    document.querySelector(".header__month-wrapper .button.button--primary").addEventListener("click", () => {
-      start()
-    })
-  }
-}, 100)
+  let onClickBtn = setInterval(() => {
+    if (document.querySelector(".header__month-wrapper .button.button--primary")) {
+      clearInterval(onClickBtn)
+      document.querySelector(".header__month-wrapper .button.button--primary").addEventListener("click", () => {
+        start()
+      })
+    }
+  }, 100)
+}
