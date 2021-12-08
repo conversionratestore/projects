fc41aaa224513d274f624f8007867f693508479c
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2021-12-08 15:20:30 +0200

    Add files via upload

---
+++ buzzpatch/changeSpanText.js
@@ -0,0 +1,13 @@
+document.head.insertAdjacentHTML('beforeend', `<style>
+	.radio-inline span {
+		font-weight: 500;
+		font-size: 26px;
+		line-height: 14px;
+	}
+</style>`)
+
+document.querySelectorAll('.radio-inline span')[0].innerHTML = `180 patches<br>for $0.20 each`
+document.querySelectorAll('.radio-inline span')[1].innerHTML = `240 pathces<br>for $0.18 each`
+document.querySelectorAll('.radio-inline span')[2].innerHTML = `120 patches<br>for $0.23 each`
+document.querySelectorAll('.radio-inline span')[3].innerHTML = `60 patches<br>for $0.25 each`
+
