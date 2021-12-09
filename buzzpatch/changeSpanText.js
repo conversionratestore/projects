b8b55af0a6a019e27a89fa746472b5e6aa556bb5
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2021-12-09 13:25:46 +0200

    Update changeSpanText.js

--- buzzpatch/changeSpanText.js
+++ buzzpatch/changeSpanText.js
@@ -15,3 +15,25 @@ document.querySelectorAll('.radio-inline span')[1].innerHTML = `240 pathces<br>f
 document.querySelectorAll('.radio-inline span')[2].innerHTML = `120 patches<br>for $0.23 each`
 document.querySelectorAll('.radio-inline span')[3].innerHTML = `60 patches<br>for $0.25 each`
 
+window.dataLayer = window.dataLayer || []
+dataLayer.push({
+	'event': 'event-to-ga',
+	'eventCategory': 'Exp — Changed pricing section mobile',
+	'eventAction': 'loaded',
+})
+
+;(function (h, o, t, j, a, r) {
+	h.hj = h.hj || function () {
+		(h.hj.q = h.hj.q || []).push(arguments)
+	}
+	h._hjSettings = {hjid: 2247058, hjsv: 6}
+	a = o.getElementsByTagName('head')[0]
+	r = o.createElement('script')
+	r.async = 1
+	r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+	a.appendChild(r)
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+window.hj = window.hj || function () {
+	(hj.q = hj.q || []).push(arguments)
+}
+hj('event', 'changed_pricing_section')
