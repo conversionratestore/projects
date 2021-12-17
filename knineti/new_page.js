75258a434729a7b06b16bc829646187d71d8faca
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2021-12-17 10:21:44 +0200

    Update new_page.js

--- knineti/new_page.js
+++ knineti/new_page.js
@@ -1109,7 +1109,7 @@ function newPage() {
         document.querySelector(".payment_inform_box").after(document.querySelectorAll(".payment_inform_box .paymen_method .paypament-details .row")[5])
         document.querySelector(".submit_btn input").value = "Enroll Now"
       }
-    }, 500)
+    }, 700)
 
     document.querySelector(".content .page h4, .content .entry-content h4").textContent = "Contact information"
 
