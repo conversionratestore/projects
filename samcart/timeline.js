cee98c102b76ce154ea86c06d1d7030636941a0c
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2021-12-24 12:00:55 +0200

    Update timeline.js

--- samcart/timeline.js
+++ samcart/timeline.js
@@ -406,15 +406,14 @@ let mut = new MutationObserver(function (muts) {
 });
 mut.observe(document, optionMut);
 
-
- (function(h,o,t,j,a,r){
- h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
- h._hjSettings={hjid:2078786,hjsv:6};
- a=o.getElementsByTagName('head')[0];
- r=o.createElement('script');r.async=1;
- r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
- a.appendChild(r);
- })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
- window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
- hj('trigger', 'add_a_timeline_passive_income_video');
- hj('event', 'add_a_timeline_passive_income_heatmap');
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:2078786,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
+    hj('trigger', 'add_a_timeline_passive_income_video');
+    hj('event', 'add_a_timeline_passive_income_heatmap');
