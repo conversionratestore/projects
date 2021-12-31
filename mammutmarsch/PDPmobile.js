435060a9fd1736564389dd6b89fe00092e231a90
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2021-12-31 11:46:23 +0200

    Update PDPmobile.js

--- mammutmarsch/PDPmobile.js
+++ mammutmarsch/PDPmobile.js
@@ -665,13 +665,15 @@ const style = /*html*/ `
 </style>
 `
 
-document.body.insertAdjacentHTML('afterbegin', style)
+document.head.insertAdjacentHTML('afterbegin', style)
 
 window.onload = function () {
 	const script = document.createElement('script')
 	script.src = 'https://code.jquery.com/jquery-3.4.1.min.js'
 	script.async = false
 	document.getElementsByTagName('head')[0].appendChild(script)
+
+
 
 	let jqueryLoaded = setInterval(() => {
 		if (typeof jQuery === 'function') {
