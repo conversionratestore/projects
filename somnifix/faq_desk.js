6fa1a78cad272befaa3557f27083fbc6e558144e
dragonegor <39415597+dragonegor@users.noreply.github.com>
2022-01-04 11:19:25 +0200

    Update faq_desk.js

--- somnifix/faq_desk.js
+++ somnifix/faq_desk.js
@@ -1,6 +1,15 @@
 window.onload = function () {
     let style = /*html*/ `
  <style>
+ .wrapper {
+  display:flex;
+  justify-content:space-between;
+  align-items: flex-start;
+ }
+ 
+ .wrapper > * {
+  width: 49%;
+ }
 
 .has-padding-top{
    padding: 0 !important; 
@@ -12,6 +21,46 @@ window.onload = function () {
 
 /*----------------------------------------------- */
 
+.img {
+  display: flex;
+  justify-content:space-between;
+}
+
+.img1 {
+  width: 43%;
+  height: 35vw;
+  border-radius: 10px;
+  overflow: hidden;
+  max-height:520px;
+}
+
+.img1+div {
+  width: 53%;
+}
+
+.img2 {
+  width: 100%;
+  height: 15vw;
+  border-radius: 10px;
+  overflow: hidden;
+  margin: 40px 0 30px;
+  max-height: 265px;
+}
+
+.img3 {
+  width: 100%;
+  height: 20vw;
+  border-radius: 10px;
+  overflow: hidden;
+  max-height: 395px;
+}
+
+.img img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
 .containerFaq {
   color: #1e415f;
   margin-bottom: 44px;
@@ -19,17 +68,31 @@ window.onload = function () {
 }
 
 .containerFaq h2 {
-  text-align: center;
-  margin-bottom: 28px;
-  font-family: 'Roboto', sans-serif !important;
+  text-align: left;
+  margin-bottom: 60px;
+  font-family: 'Rubik', sans-serif !important;
   font-weight: 700;
-  font-size: 32px;
-  line-height: 1.16;
+  font-size: 48px;
+  line-height: 57px;
   letter-spacing: 0.02em;
   color: #1E415F;
+  padding-left: 40px;
+  position:relative;
 }
 
-.containerFaq > ul{
+.containerFaq h2::before {
+  display: block;
+  content: "";
+  position:absolute;
+  top: 0;
+  left: 0;
+  height: 45px;
+  width: 10px;
+  border-radius: 10px;
+  background-color: #4090D1;
+}
+
+.containerFaq ul{
   font-family: 'Roboto', sans-serif !important;
   text-align: start;
   font-weight: 400;
@@ -37,13 +100,18 @@ window.onload = function () {
   line-height: 1.19;
 }
 
-.containerFaq > ul li {
-  padding: 16px 22px 16px 16px;
-  background: #f1f7fc;
-  border-radius: 5px;
+.containerFaq ul li {
+  padding: 25px 30px;
+  background: #F5F6F7;
+  border-radius: 10px;
+  transition: background-color 0.3s;
+}
+.containerFaq ul li.show {
+  background: #F1F7FC;
 }
 
-.containerFaq > ul li:not(:last-child) {
+
+.containerFaq ul li:not(:last-child) {
   margin-bottom: 8px;
 }
 
@@ -54,19 +122,22 @@ window.onload = function () {
 }
 
 .faqBox p {
-    max-width: 287px;
-    font-family: 'Roboto', sans-serif !important;
-    font-weight: 400 !important;
-    font-size: 16px !important;
+    font-family: 'Rubik', sans-serif !important;
     line-height: 1.19 !important;
     color: #1E415F !important;
+    max-width: 100%;
+    font-size: 20px !important;
+    font-weight: 600 !important;
+    cursor: pointer;
+    transition: all 0.3s;
 }
 
 .faqBox svg {
-    flex-shrink: 0;
+   flex-shrink: 0;
   width: 12px;
   height: 8px;
   transition: transform 0.5s;
+  cursor: pointer;
 }
 
 .show .faqBox svg{
@@ -80,10 +151,11 @@ window.onload = function () {
   padding-top: 12px;
   border-top: 1px solid #FFFFFF;
   font-family: 'Roboto', sans-serif !important;
-  font-weight: 300 !important;
-  font-size: 14px !important;
+  font-weight: 400 !important;
+  font-size: 16px !important;
   line-height: 1.5 !important;
   color: #1E415F !important;
+  opacity: 0.5;
 }
 
 .textFaq > a{
@@ -96,11 +168,7 @@ window.onload = function () {
   }
   
   .faqBox p {
-    max-width: 100%;
-    font-size: 20px !important;
-    font-weight: 500 !important;
-    text-transform:uppercase;
-    cursor: pointer;
+    
   }
 }
   </style>
@@ -108,7 +176,26 @@ window.onload = function () {
 
     let faq = /*html*/ `
 <section class="containerFaq">
-    <h2>FAQ</h2>
+    
+    <div class="wrapper">
+    
+    <div>
+      <h2>Frequently Asked Questions</h2>
+      <div class="img">
+        <div class="img1">
+        <img src="https://conversionratestore.github.io/projects/somnifix/img/image1.png" alt="image">
+</div>
+        <div>
+            <div class="img2">
+                    <img src="https://conversionratestore.github.io/projects/somnifix/img/image2.png" alt="image">
+            </div>
+            <div class="img3">
+                    <img src="https://conversionratestore.github.io/projects/somnifix/img/image3.png" alt="image">
+            </div>
+        </div>
+      </div>
+    </div>
+
     <ul>
     <li>
           <div class="faqBox" data-controls>
@@ -318,6 +405,7 @@ window.onload = function () {
           </p>
         </li>
       </ul>
+      </div>
 </section>`
 
     document.body.insertAdjacentHTML("afterbegin", style)
