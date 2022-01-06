f0f9a5dcea20fe3b783aa67c1661d4ee1e3a6d25
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2022-01-06 12:01:41 +0200

    Update version1.js

--- vaha/version1.js
+++ vaha/version1.js
@@ -327,132 +327,132 @@ const productStyle = `
 const language = window.location.host.split('.')[0] === 'uk' ? 'en' : 'ge'
 
 const textArr = {
-	'en': {
-		priceX: '£ 50 / month',
-		priceS: '£ 31 / month',
-		month: `month`,
-		from: `Monthly From`,
-		fromWord: 'From',
-		klarna: `with Klarna or SplitIt`,
-		features: `Features`,
-		live: `650+ live and on-demand classes`,
-		motion: `Motion tracking workouts`,
-		personalised: `Personalised workouts`,
-		entertainment: `Entertainment through your favourite apps`,
-		access: `Access to personal trainers`,
-		specs: `Specifications`,
-		dimension: `Dimensions`,
-		weight: `Weight`,
-		floorStand: `Floor stand`,
-		secure: `Wall Secure`,
-		display: `Display`,
-		inch: ` inch touch<br>screen, Full HD`,
-		sound: `Sound`,
-		firstX4: `x4 - max SPL 105<br>dB with custom<br>sound board and<br>amplifier, 65 W`,
-		secondX4: `x4 - max SPL<br>85dB, 30 W`,
-		mounting: `Wall Mounting`,
-		starterbox: 'VAHA Starterbox',
-		accessories: `Accessories`,
-		voucher: `GARMIN voucher code to be redeemed after 30 Day Trial Resistance bands of different strengths<br><br>Camera cover<br>User manual`,
-		care: `Care`,
-		microfiber: `Microfiber cloth<br>and glass cleaner`,
-		heart: `Garmin voucher`,
-		elastic: `Elastic bands`,
-		warranty: '2 year warranty',
-		financing: `Financing`,
-		payment: `One-time payment`,
-		options: `Financing options:`,
-		choose: `Choose what works for you`,
-		rate: `0% annual interest rate`,
-		total: `Total price regardless of term: £1,195`,
-		incl: `incl. VAT`,
-		text: [
-			`Try it for 30 days for free.`,
-			`If you are not satisfied with VAHA, we will pick it up free of charge. Any payments already made will be refunded.`,
-			`Pay the full amount or via installments for 12, 24, or 39 months. The VAHA membership (from £39) is separate from the purchase of the VAHA mirror and is activated upon receipt.`,
-			`Sufficient creditworthiness is a prerequisite. The installment purchase offered by us is handled by our payment partner Klarna Bank AB (publ). Klarna, therefore, decides whether the installment purchase is accepted or not.`,
-		],
-		getWordX: 'GET VAHA X',
-		getWordS: 'GET VAHA S',
-		try: 'Try 30 days for free',
-		chooseVaha: 'Choose your Vaha',
-		link: '/product',
-		money: `No money<br>down`,
-		flatrate: 'Flatrate plans',
-		fees: `No hidden<br>fees`,
-		zero: '£0',
-		month36X: '£ 50 / month',
-		month36S: '£ 31 / month',
-		month24X: '£ 81 / month',
-		month24S: '£ 50 / month',
-		month12X: '£ 162 / month',
-		month12S: '£ 100 / month',
-		monthTimeX: 'From £ 1,950',
-		monthTimeS: 'From £ 1,195',
-	},
-	'ge': {
-		priceX: '€ 58 / monat',
-		priceS: '€ 36 / monat',
-		month: `monate`,
-		from: `Monatlich von`,
-		fromWord: 'ab',
-		klarna: `mit Klarna oder Splitit`,
-		features: `MERKMALE`,
-		live: `650+ live- und auf-anforderung-klassen`,
-		motion: `Workouts mit motion tracking`,
-		personalised: `Personalisierte workouts`,
-		entertainment: `Unterhaltung mit Ihren lieblingsapps`,
-		access: `Zugang zu persönlichen trainern`,
-		specs: `Spezifikationen`,
-		dimension: `Abmessungen`,
-		weight: `Gewicht`,
-		floorStand: `Ständer`,
-		secure: `Sichere wand`,
-		display: `Anzeige`,
-		inch: `-Zoll touch<br>screen, Full HD`,
-		sound: `Ton`,
-		firstX4: `x4 – max. SPL 105<br>dB mit spezieller<br>85 dB, 30 W soundkarte und<br>verstärker, 65 W`,
-		secondX4: `x4 - max SPL<br>85dB, 30 W`,
-		mounting: `Wandmontage`,
-		starterbox: 'VAHA Starterbox',
-		accessories: `Zubehör`,
-		voucher: `GARMIN Gutscheincode zum Einlösen nach der 30-Tage-Testversion Widerstandsbänder unterschiedlicher Stärke<br><br>Kameraabdeckung Benutzerhandbuch`,
-		care: `Pflege`,
-		microfiber: `Mikrofasertuch<br>und Glasreiniger`,
-		heart: `Pulsmessgerät`,
-		elastic: `Gummibänder`,
-		warranty: `2 jahre garantie`,
-		financing: `Finanzierung`,
-		payment: `Einmalige zahlung`,
-		options: `Optionen zur Finanzierung:`,
-		choose: `Wähle, was dir passt`,
-		rate: `0% Jahreszins`,
-		total: `Gesamtpreis unabhängig von Laufzeit: 2.268£`,
-		incl: `inkl. MwSt.`,
-		text: [
-			`30 Tage gratis und unverbindlich testen.*`,
-			`Die Zahlung erfolgt erst nach der Testphase und wenn du nicht mit VAHA zufrieden bist, einfach kommentarlos gratis wieder abholen lassen.`,
-			`Zahle via Direkt- oder Ratenkauf für 12, 24 oder 39 Monate. Die VAHA Mitgliedschaft (ab 39 £) ist losgelöst vom Kauf des VAHAs und wird beim Erhalt aktiviert.`,
-			`Eine ausreichende Bonität ist vorausgesetzt. Der von uns angebotene Ratenkauf wird von unserem Zahlungspartner ZAB Abrechnungsgesellschaft mbH abgewickelt. ZAB entscheidet daher, ob der Ratenkauf angenommen wird oder nicht.`,
-		],
-		getWordX: 'VAHA X erhalten',
-		getWordS: 'VAHA S erhalten',
-		try: '30 Tage kostenlos testen',
-		chooseVaha: 'Wählen Sie Ihr Vaha',
-		link: '/produkt',
-		money: `Keine<br>anzahlung`,
-		flatrate: 'Flatrate-Tarife',
-		fees: `Keine<br>versteckten<br>gebühren`,
-		zero: '€0',
-		month36X: '€ 58 / monat',
-		month36S: '€ 36 / monat',
-		month24X: '€ 94 / monat',
-		month24S: '€ 58 / monat',
-		month12X: '€ 189 / monat',
-		month12S: '€ 116 / monat',
-		monthTimeX: 'Ab € 2,268',
-		monthTimeS: 'Ab € 1,395',
-	},
+  'en': {
+    priceX: '£ 50 / month',
+    priceS: '£ 31 / month',
+    month: `month`,
+    from: `Monthly From`,
+    fromWord: 'From',
+    klarna: `with Klarna or SplitIt`,
+    features: `Features`,
+    live: `650+ live and on-demand classes`,
+    motion: `Motion tracking workouts`,
+    personalised: `Personalised workouts`,
+    entertainment: `Entertainment through your favourite apps`,
+    access: `Access to personal trainers`,
+    specs: `Specifications`,
+    dimension: `Dimensions`,
+    weight: `Weight`,
+    floorStand: `Floor stand`,
+    secure: `Wall Secure`,
+    display: `Display`,
+    inch: ` inch touch<br>screen, Full HD`,
+    sound: `Sound`,
+    firstX4: `x4 - max SPL 105<br>dB with custom<br>sound board and<br>amplifier, 65 W`,
+    secondX4: `x4 - max SPL<br>85dB, 30 W`,
+    mounting: `Wall Mounting`,
+    starterbox: 'VAHA Starterbox',
+    accessories: `Accessories`,
+    voucher: `GARMIN voucher code to be redeemed after 30 Day Trial Resistance bands of different strengths<br><br>Camera cover<br>User manual`,
+    care: `Care`,
+    microfiber: `Microfiber cloth<br>and glass cleaner`,
+    heart: `Garmin voucher`,
+    elastic: `Elastic bands`,
+    warranty: '2 year warranty',
+    financing: `Financing`,
+    payment: `One-time payment`,
+    options: `Financing options:`,
+    choose: `Choose what works for you`,
+    rate: `0% annual interest rate`,
+    total: `Total price regardless of term: £1,195`,
+    incl: `incl. VAT`,
+    text: [
+      `Try it for 30 days for free.`,
+      `If you are not satisfied with VAHA, we will pick it up free of charge. Any payments already made will be refunded.`,
+      `Pay the full amount or via installments for 12, 24, or 39 months. The VAHA membership (from £39) is separate from the purchase of the VAHA mirror and is activated upon receipt.`,
+      `Sufficient creditworthiness is a prerequisite. The installment purchase offered by us is handled by our payment partner Klarna Bank AB (publ). Klarna, therefore, decides whether the installment purchase is accepted or not.`,
+    ],
+    getWordX: 'GET VAHA X',
+    getWordS: 'GET VAHA S',
+    try: 'Try 30 days for free',
+    chooseVaha: 'Choose your Vaha',
+    link: '/product',
+    money: `No money<br>down`,
+    flatrate: '0% Financing Available',
+    fees: `No hidden<br>fees`,
+    zero: '£0',
+    month36X: '£ 50 / month',
+    month36S: '£ 31 / month',
+    month24X: '£ 81 / month',
+    month24S: '£ 50 / month',
+    month12X: '£ 162 / month',
+    month12S: '£ 100 / month',
+    monthTimeX: 'From £ 1,950',
+    monthTimeS: 'From £ 1,195',
+  },
+  'ge': {
+    priceX: '€ 58 / monat',
+    priceS: '€ 36 / monat',
+    month: `monate`,
+    from: `Monatlich von`,
+    fromWord: 'ab',
+    klarna: `mit Klarna oder Splitit`,
+    features: `MERKMALE`,
+    live: `650+ live- und auf-anforderung-klassen`,
+    motion: `Workouts mit motion tracking`,
+    personalised: `Personalisierte workouts`,
+    entertainment: `Unterhaltung mit Ihren lieblingsapps`,
+    access: `Zugang zu persönlichen trainern`,
+    specs: `Spezifikationen`,
+    dimension: `Abmessungen`,
+    weight: `Gewicht`,
+    floorStand: `Ständer`,
+    secure: `Sichere wand`,
+    display: `Anzeige`,
+    inch: `-Zoll touch<br>screen, Full HD`,
+    sound: `Ton`,
+    firstX4: `x4 – max. SPL 105<br>dB mit spezieller<br>85 dB, 30 W soundkarte und<br>verstärker, 65 W`,
+    secondX4: `x4 - max SPL<br>85dB, 30 W`,
+    mounting: `Wandmontage`,
+    starterbox: 'VAHA Starterbox',
+    accessories: `Zubehör`,
+    voucher: `GARMIN Gutscheincode zum Einlösen nach der 30-Tage-Testversion Widerstandsbänder unterschiedlicher Stärke<br><br>Kameraabdeckung Benutzerhandbuch`,
+    care: `Pflege`,
+    microfiber: `Mikrofasertuch<br>und Glasreiniger`,
+    heart: `Pulsmessgerät`,
+    elastic: `Gummibänder`,
+    warranty: `2 jahre garantie`,
+    financing: `Finanzierung`,
+    payment: `Einmalige zahlung`,
+    options: `Optionen zur Finanzierung:`,
+    choose: `Wähle, was dir passt`,
+    rate: `0% Jahreszins`,
+    total: `Gesamtpreis unabhängig von Laufzeit: 2.268£`,
+    incl: `inkl. MwSt.`,
+    text: [
+      `30 Tage gratis und unverbindlich testen.*`,
+      `Die Zahlung erfolgt erst nach der Testphase und wenn du nicht mit VAHA zufrieden bist, einfach kommentarlos gratis wieder abholen lassen.`,
+      `Zahle via Direkt- oder Ratenkauf für 12, 24 oder 39 Monate. Die VAHA Mitgliedschaft (ab 39 £) ist losgelöst vom Kauf des VAHAs und wird beim Erhalt aktiviert.`,
+      `Eine ausreichende Bonität ist vorausgesetzt. Der von uns angebotene Ratenkauf wird von unserem Zahlungspartner ZAB Abrechnungsgesellschaft mbH abgewickelt. ZAB entscheidet daher, ob der Ratenkauf angenommen wird oder nicht.`,
+    ],
+    getWordX: 'VAHA X erhalten',
+    getWordS: 'VAHA S erhalten',
+    try: '30 Tage kostenlos testen',
+    chooseVaha: 'Wählen Sie Ihr Vaha',
+    link: '/produkt',
+    money: `Keine<br>anzahlung`,
+    flatrate: 'Verfügbar ab 0% Finanzierung',
+    fees: `Keine<br>versteckten<br>gebühren`,
+    zero: '€0',
+    month36X: '€ 58 / monat',
+    month36S: '€ 36 / monat',
+    month24X: '€ 94 / monat',
+    month24S: '€ 58 / monat',
+    month12X: '€ 189 / monat',
+    month12S: '€ 116 / monat',
+    monthTimeX: 'Ab € 2,268',
+    monthTimeS: 'Ab € 1,395',
+  },
 }
 const obj = textArr[language]
 
@@ -777,103 +777,103 @@ const productPage = `
 document.head.insertAdjacentHTML('beforeend', productStyle)
 
 let start = setInterval(() => {
-	if (document.querySelectorAll('.basket-wrapper-b').length === 1) {
-		clearInterval(start)
+  if (document.querySelectorAll('.basket-wrapper-b').length === 1) {
+    clearInterval(start)
 
-		document.querySelector('.basket-wrapper-b').insertAdjacentHTML('afterend', productPage)
+    document.querySelector('.basket-wrapper-b').insertAdjacentHTML('afterend', productPage)
 
-		let btnInterval = setInterval(() => {
-			if (document.querySelectorAll('.btn-wrapper button')[1] &&
-				document.querySelector('.sidebar-content') &&
-				document.querySelectorAll('.basket-payments-button')[0]
-			) {
-				clearInterval(btnInterval)
+    let btnInterval = setInterval(() => {
+      if (document.querySelectorAll('.btn-wrapper button')[1] &&
+          document.querySelector('.sidebar-content') &&
+          document.querySelectorAll('.basket-payments-button')[0]
+      ) {
+        clearInterval(btnInterval)
 
-				document.querySelectorAll('.btn-wrapper button').forEach((btn, index) => {
-					btn.addEventListener('click', () => {
-						document.querySelectorAll('.basket-vaha-item')[index].click()
+        document.querySelectorAll('.btn-wrapper button').forEach((btn, index) => {
+          btn.addEventListener('click', () => {
+            document.querySelectorAll('.basket-vaha-item')[index].click()
 
-						if (index === 0) {
-							window.dataLayer = window.dataLayer || []
-							dataLayer.push({
-								'event': 'event-to-ga',
-								'eventCategory': 'Exp — The new comparison page',
-								'eventAction': 'Click on Get vaha x button',
-							})
-						} else if (index === 1) {
-							window.dataLayer = window.dataLayer || []
-							dataLayer.push({
-								'event': 'event-to-ga',
-								'eventCategory': 'Exp — The new comparison page',
-								'eventAction': 'Click on Get vaha s button',
-							})
-						}
+            if (index === 0) {
+              window.dataLayer = window.dataLayer || []
+              dataLayer.push({
+                'event': 'event-to-ga',
+                'eventCategory': 'Exp — The new comparison page',
+                'eventAction': 'Click on Get vaha x button',
+              })
+            } else if (index === 1) {
+              window.dataLayer = window.dataLayer || []
+              dataLayer.push({
+                'event': 'event-to-ga',
+                'eventCategory': 'Exp — The new comparison page',
+                'eventAction': 'Click on Get vaha s button',
+              })
+            }
 
-						document.querySelectorAll('.basket-payments-button')[2]
-							? document.querySelectorAll('.basket-payments-button')[2].click()
-							: document.querySelectorAll('.basket-payments-button')[0].click()
-					})
-				})
+            document.querySelectorAll('.basket-payments-button')[2]
+                ? document.querySelectorAll('.basket-payments-button')[2].click()
+                : document.querySelectorAll('.basket-payments-button')[0].click()
+          })
+        })
 
-				document.querySelector('.sidebar-content').addEventListener('scroll', evt => {
-					if (evt.target.scrollTop >= 50) {
-						document.querySelector('.btn-wrapper').classList.add('show')
-					}
-				})
-			}
-		}, 100)
-	}
+        document.querySelector('.sidebar-content').addEventListener('scroll', evt => {
+          if (evt.target.scrollTop >= 50) {
+            document.querySelector('.btn-wrapper').classList.add('show')
+          }
+        })
+      }
+    }, 100)
+  }
 }, 100)
 
 let chooseVahaInterval = setInterval(() => {
-	if (document.querySelector('.container-3dy0SD button span')) {
-		clearInterval(chooseVahaInterval)
+  if (document.querySelector('.container-3dy0SD button span')) {
+    clearInterval(chooseVahaInterval)
 
-		document.querySelector('.container-3dy0SD button span').innerText = obj.chooseVaha
+    document.querySelector('.container-3dy0SD button span').innerText = obj.chooseVaha
 
-		document.querySelector('.container-3dy0SD button span').addEventListener('click', () => {
-			window.dataLayer = window.dataLayer || []
-			dataLayer.push({
-				'event': 'event-to-ga',
-				'eventCategory': 'Exp — The new comparison page',
-				'eventAction': 'Click on Choose your Vaha button',
-			})
-		})
-	}
+    document.querySelector('.container-3dy0SD button span').addEventListener('click', () => {
+      window.dataLayer = window.dataLayer || []
+      dataLayer.push({
+        'event': 'event-to-ga',
+        'eventCategory': 'Exp — The new comparison page',
+        'eventAction': 'Click on Choose your Vaha button',
+      })
+    })
+  }
 }, 100)
 
 setTimeout(function () {
-	clearInterval(chooseVahaInterval)
+  clearInterval(chooseVahaInterval)
 }, 10000)
 
 ;(function (h, o, t, j, a, r) {
-	h.hj = h.hj || function () {
-		(h.hj.q = h.hj.q || []).push(arguments)
-	}
-	h._hjSettings = {hjid: 1754586, hjsv: 6}
-	a = o.getElementsByTagName('head')[0]
-	r = o.createElement('script')
-	r.async = 1
-	r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
-	a.appendChild(r)
+  h.hj = h.hj || function () {
+    (h.hj.q = h.hj.q || []).push(arguments)
+  }
+  h._hjSettings = {hjid: 1754586, hjsv: 6}
+  a = o.getElementsByTagName('head')[0]
+  r = o.createElement('script')
+  r.async = 1
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+  a.appendChild(r)
 })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
 window.hj = window.hj || function () {
-	(hj.q = hj.q || []).push(arguments)
+  (hj.q = hj.q || []).push(arguments)
 }
 hj('event', 'new_comparison_page')
 
 window.dataLayer = window.dataLayer || []
 dataLayer.push({
-	'event': 'event-to-ga',
-	'eventCategory': 'Exp — The new comparison page',
-	'eventAction': 'loaded',
+  'event': 'event-to-ga',
+  'eventCategory': 'Exp — The new comparison page',
+  'eventAction': 'loaded',
 })
 
 document.querySelector('.vaha-nav button')?.addEventListener('click', () => {
-	window.dataLayer = window.dataLayer || []
-	dataLayer.push({
-		'event': 'event-to-ga',
-		'eventCategory': 'Exp — The new comparison page',
-		'eventAction': 'Click on the sticky button Purchase',
-	})
+  window.dataLayer = window.dataLayer || []
+  dataLayer.push({
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp — The new comparison page',
+    'eventAction': 'Click on the sticky button Purchase',
+  })
 })
