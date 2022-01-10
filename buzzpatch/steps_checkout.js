2129e17f191a99837fcec824aaaeb89ee1f5a99e
dragonegor <39415597+dragonegor@users.noreply.github.com>
2022-01-10 16:44:29 +0200

    Update steps_checkout.js

--- buzzpatch/steps_checkout.js
+++ buzzpatch/steps_checkout.js
@@ -49,20 +49,4 @@ function start() {
         'eventCategory': 'Exp — Checkout improvements hypothesis 4',
         'eventAction': 'loaded'
     });
-
-    (function (h, o, t, j, a, r) {
-        h.hj = h.hj || function () {
-            (h.hj.q = h.hj.q || []).push(arguments)
-        };
-        h._hjSettings = {hjid: 2247058, hjsv: 6};
-        a = o.getElementsByTagName('head')[0];
-        r = o.createElement('script');
-        r.async = 1;
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-    window.hj = window.hj || function () {
-        (hj.q = hj.q || []).push(arguments)
-    };
-    hj('event', 'checkout_improvements_4');
 }
