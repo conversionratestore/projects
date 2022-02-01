412f356398e98d36a0ce7c7b03f2f005bbda9ae3
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-02-01 12:56:24 +0200

    Update pdp.js

--- medicalmega/pdp.js
+++ medicalmega/pdp.js
@@ -1553,6 +1553,11 @@ window.onload  = function () {
     })
   }
   document.querySelector('.advanced-search .btn').addEventListener('click', () => document.querySelector('.advance_search #search_submit').click())
+  document.querySelector('.advanced-search input').addEventListener('keypress', (e) => {
+    if (e.keyCode == '13') {
+      document.querySelector('.advance_search #search_submit').click()
+    }
+  })
 
   //shipping
   fetch("/cart.html", fetchOption("POST",`api=c&cart_action=cart&ctoken=${mm.ctoken}`)).then(res => res.json()).then(data => {
