5b55d6356e638ac2d4c1ed7b00a51d46c8234015
conversionratestore <82801260+conversionratestore@users.noreply.github.com>
2022-02-07 17:12:51 +0200

    Update test-quiz.js

--- somnifix/test-quiz.js
+++ somnifix/test-quiz.js
@@ -5,8 +5,8 @@ if (window.location.pathname.includes('blogs')) {
             blogPage()
         }
     }, 100)
-    
-    
+
+
 } else {
     let start = setInterval(function () {
         if(document.querySelectorAll('.shg-btn-wrapper')) {
@@ -133,3 +133,21 @@ document.querySelector('.to_quiz1').addEventListener('click', function () {
         'eventLabel': `On blog page: ${page}`
     });
 })
+
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp — Quiz hp and blog pages',
+    'eventAction': 'loaded'
+});
+
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1271698,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
+hj('event', 'quiz_hp_blog');
