848ffca125576c76aeb82665aabf778c8dbdf88c
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2022-02-08 10:13:00 +0200

    Update checkout-mobile.js

--- mammutmarsch/checkout-mobile.js
+++ mammutmarsch/checkout-mobile.js
@@ -1470,6 +1470,7 @@ function addDark() {
 
 addDark()
 
+console.log('eventAction loaded')
 
 window.dataLayer = window.dataLayer || [];
 dataLayer.push({
@@ -1477,3 +1478,13 @@ dataLayer.push({
 	'eventCategory': 'Exp: Checkout improvements',
 	'eventAction': 'loaded'
 });
+
+(function(h,o,t,j,a,r){
+	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+	h._hjSettings={hjid:1191175,hjsv:6};
+	a=o.getElementsByTagName('head')[0];
+	r=o.createElement('script');r.async=1;
+	r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+	a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+hj('event', 'checkout_improvements');
