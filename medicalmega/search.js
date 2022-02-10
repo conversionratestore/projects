5b13dd4169b2ac808ae5bfbe260f86c4f94c3338
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-02-10 09:52:26 +0200

    Update search.js

--- medicalmega/search.js
+++ medicalmega/search.js
@@ -969,10 +969,12 @@ window.onload = function() {
             if (data.categories[key].url) {
                 document.querySelector('.category_popular .altnav').insertAdjacentHTML('beforeend',`<li><a href="${data.categories[key].url}" data-id="${data.categories[key].category_id}">${data.categories[key].title}</a></li>`)
             }
-            if (document.querySelector('.categoryTop').innerText == data.categories[key].title) {
-                console.log(data.categories[key].category_id)
-                localStorage.setItem('idCategory', JSON.stringify(data.categories[key].category_id))
-                idCategory = data.categories[key].category_id;
+            if (window.location.pathname.includes('/category')) {
+                if (document.querySelector('.categoryTop').innerText == data.categories[key].title) {
+                    console.log(data.categories[key].category_id)
+                    localStorage.setItem('idCategory', JSON.stringify(data.categories[key].category_id))
+                    idCategory = data.categories[key].category_id;
+                }
             }
         }
 
