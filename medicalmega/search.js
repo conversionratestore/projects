3598a714e0dcb85e0291bf297f518ce4c578c355
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-02-11 12:39:07 +0200

    Update search.js

--- medicalmega/search.js
+++ medicalmega/search.js
@@ -849,7 +849,7 @@ window.onload = function() {
             let cnt_page = Math.ceil(count / cnt); //кол-во страниц
     
             console.log(cnt_page)
-            document.querySelector('.list_type1 span').innerHTML = `Displaying <b>${data.offset + 1} to ${data.limit * data.offset}</b> (of <b>${count}</b> products)`
+            document.querySelector('.list_type1 span').innerHTML = `Displaying <b>${data.offset + 1} to ${data.limit * selectedPage}</b> (of <b>${count}</b> products)`
             if (cnt_page > 1) {
     
                 //Initialization pagination
