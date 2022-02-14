a06625c7ef0ea16aecd238f7e5d0d5027367484d
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2022-02-14 15:34:37 +0200

    Update checkout-mobile.js

--- mammutmarsch/checkout-mobile.js
+++ mammutmarsch/checkout-mobile.js
@@ -1425,7 +1425,7 @@ function reorder() {
 
 }
 function isBtnForward() {
-	if ($('.woocommerce-message .button.wc-forward') && $('.subblock') && !$('.remove')) {
+	if ($('.woocommerce-message .button.wc-forward') && $('.subblock') && !$('.subblock .remove')) {
 
 		let remove = `<div class="remove"><img src="https://conversionratestore.github.io/projects/mammutmarsch/img/delete.svg" alt="remove"></div>`
 
