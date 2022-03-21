96e92e1c19394905522287c6763ec8da95c4f6d5
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-03-21 19:41:29 +0200

    Update search.js

--- medicalmega/search.js
+++ medicalmega/search.js
@@ -1228,7 +1228,7 @@ window.onload = function() {
 //                 loadingContainer.innerHTML = isSearchStalled ? 'Loading..' : ''
 //                 listingContainer.style = isSearchStalled ? 'display:none' : 'display:block'
                 let load = isSearchStalled ? 'loading' : 'load';
-                loadingContainer.classList.setAttribute('class',load)
+                loadingContainer.setAttribute('class',load)
                 
                 console.log(isSearchStalled)
                 
