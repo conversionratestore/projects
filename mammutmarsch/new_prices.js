9bedb170b3ad4708dcc0180930903ef9d54b1280
conversionratestore <82801260+conversionratestore@users.noreply.github.com>
2022-03-28 14:21:30 +0300

    Update new_prices.js

--- mammutmarsch/new_prices.js
+++ mammutmarsch/new_prices.js
@@ -2,31 +2,24 @@ let start = setInterval(function () {
     if(typeof jQuery === 'function' && (jQuery('.woocommerce-Price-amount') || jQuery('.starter_package_price') || jQuery('#coupon_code'))) {
         clearInterval(start)
 
-        const $ = jQuery
-        $('.woocommerce-Price-amount').each((__, item) => {
-            let price = +$(item).text().slice(0, -1).replace(',', '.')
-            price = price*0.9
-            $(item).text(price.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' }))
-        })
-
-        $('.starter_package_price, .additional_package_price, .checkout_total_price').each((__, item) => {
-            $(item).css('opacity', '0')
-        })
-
         setTimeout(function () {
+
+            const $ = jQuery
+            $('.woocommerce-Price-amount').each((__, item) => {
+                let price = +$(item).text().slice(0, -1).replace(',', '.')
+                price = price * 0.9
+                $(item).text(price.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'}))
+            })
+
             $('.starter_package_price, .additional_package_price, .checkout_total_price').each((__, item) => {
                 let price = +$(item).text().replace(',', '.')
-                price = price*0.9
+                price = price * 0.9
                 $(item).text(price.toString().replace('.', ','))
                 $(item).css('opacity', '1')
             })
+
+            $('#coupon_code').val('test')
+            $('.checkout_coupon input[type="submit"]').click()
         }, 500)
-
-
-
-        $('#coupon_code').val('test')
-        $('.checkout_coupon input[type="submit"]').click()
     }
 }, 100)
-
-
