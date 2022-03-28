c380fe8ec5b3ce0b739b798a72ef7d8018f809bd
conversionratestore <82801260+conversionratestore@users.noreply.github.com>
2022-03-28 12:06:30 +0300

    Create new_prices.js

---
+++ mammutmarsch/new_prices.js
@@ -0,0 +1,25 @@
+let start = setInterval(function () {
+    if(typeof jQuery === 'function') {
+        clearInterval(start)
+
+        const $ = jQuery
+        $('.woocommerce-Price-amount').each((__, item) => {
+            let price = +$(item).text().slice(0, -1).replace(',', '.')
+            price = price*0.9
+            $(item).text(price.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' }))
+        })
+
+
+        $('.starter_package_price, .additional_package_price, .checkout_total_price').each((__, item) => {
+            let price = +$(item).text().replace(',', '.')
+            price = price*0.9
+            $(item).text(price.toString().replace('.', ','))
+        })
+
+
+        $('#coupon_code').val('test')
+        $('.checkout_coupon input[type="submit"]').click()
+    }
+}, 100)
+
+
