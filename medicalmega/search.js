8ff8f72ac20fc92e43c45b48bbfd8bf086e29831
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-03-28 16:35:12 +0300

    Update search.js

--- medicalmega/search.js
+++ medicalmega/search.js
@@ -1095,7 +1095,7 @@ window.onload = function() {
                         for (let i = 0; i < hit.variants.length; i++) {
                             let variantsArr = hit.variants[i];
                             if (variantsArr.extra != '') {
-                                option = `<option value="${variantsArr.pv_id}" ${variantsArr.extra == 'Each' ? 'selected':''} data-price="${variantsArr.price}" data-qty="${hit.qty}"> ${variantsArr.extra} ${variantsArr.in_stock==false? ' (Out of stock)':''} </option>` + option;
+                                option = `<option value="${variantsArr.pv_id}" ${variantsArr.extra == 'Each' ? 'selected':''} data-price="${variantsArr.price}" data-qty="${variantsArr.qty}"> ${variantsArr.extra} ${variantsArr.in_stock==false? ' (Out of stock)':''} </option>` + option;
                             }
                         }
                         return option
