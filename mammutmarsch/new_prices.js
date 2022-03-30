33ee91ec79b5b890970e8bf6f7edb7df211012f6
conversionratestore <82801260+conversionratestore@users.noreply.github.com>
2022-03-30 13:29:34 +0300

    Update new_prices.js

--- mammutmarsch/new_prices.js
+++ mammutmarsch/new_prices.js
@@ -7,19 +7,21 @@ let start = setInterval(function () {
             const $ = jQuery
             $('.woocommerce-Price-amount').each((__, item) => {
                 let price = +$(item).text().slice(0, -1).replace(',', '.')
-                price = price * 0.9
+                price = price * 0.95
                 $(item).text(price.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'}))
             })
 
             $('.starter_package_price, .additional_package_price, .checkout_total_price').each((__, item) => {
                 let price = +$(item).text().replace(',', '.')
-                price = price * 0.9
+                price = price * 0.95
                 $(item).text(price.toString().replace('.', ','))
                 $(item).css('opacity', '1')
             })
 
-            $('#coupon_code').val('test')
+            $('#coupon_code').val('Price_Test_CRO')
             $('.checkout_coupon input[type="submit"]').click()
-        }, 500)
+        }, 300)
     }
 }, 100)
+
+
