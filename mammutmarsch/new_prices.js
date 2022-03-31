de7edea0a2421b15f2be9067f2a783b86878cc28
conversionratestore <82801260+conversionratestore@users.noreply.github.com>
2022-03-31 20:39:01 +0300

    Update new_prices.js

--- mammutmarsch/new_prices.js
+++ mammutmarsch/new_prices.js
@@ -41,5 +41,21 @@ setTimeout(function () {
     }
 
     document.querySelector('.temp').remove()
-}, 500)
+}, 500);
 
+(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1191175,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    hj('event', 'price_elasticity');
+
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp: Price elasticity',
+    'eventAction': 'loaded'
+});
