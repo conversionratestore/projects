4aafcdd767d666550cd63c2694a3d5804318dab9
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-04-04 17:35:52 +0300

    Update addLocation.js

--- onairparking/addLocation.js
+++ onairparking/addLocation.js
@@ -18,7 +18,7 @@ let optionMut = {
 
 let mut = new MutationObserver(function (muts) {
     console.log('mut')
-    if (document.querySelector('.flex.flex-col.w-full.col-span-2 p') != null && window.location.href.includes('/reservation/search') && document.querySelector('a.rounded-full.uppercase.mt-4.bg-primary.text-white.text-center.mt-4.px-4.text-xs.font-bold.py-2') != null && document.querySelector('.self-end.flex.flex-col.px-4.pb-4.col-span-3.disabled a.rounded-full.uppercase.mt-4.bg-primary.text-white.text-center.mt-4.px-4.text-xs.font-bold.py-2') != null) {
+    if (document.querySelectorAll('.flex.flex-col.w-full.col-span-2 p').length > 3 && window.location.href.includes('/reservation/search') && document.querySelector('a.rounded-full.uppercase.mt-4.bg-primary.text-white.text-center.mt-4.px-4.text-xs.font-bold.py-2') != null && document.querySelector('.self-end.flex.flex-col.px-4.pb-4.col-span-3.disabled a.rounded-full.uppercase.mt-4.bg-primary.text-white.text-center.mt-4.px-4.text-xs.font-bold.py-2') != null) {
         mut.disconnect();
         console.log('mut disc 1')
         document.body.insertAdjacentHTML('afterbegin',`
