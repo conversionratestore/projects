d36dd7ea83e9201014669d7978b7dbe11b61f32b
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-04-04 15:51:50 +0300

    Update addLocation.js

--- onairparking/addLocation.js
+++ onairparking/addLocation.js
@@ -17,7 +17,7 @@ let optionMut = {
 }
 
 let mut = new MutationObserver(function (muts) {
-    if (window.location.href.includes('/reservation/search') && document.querySelector('a.rounded-full.uppercase.mt-4.bg-primary.text-white.text-center.mt-4.px-4.text-xs.font-bold.py-2') != null && document.querySelector('.flex.flex-col.w-full.col-span-2.pb-5.pl-4.pr-0.self-center.mx-auto p') != null) {
+    if (window.location.href.includes('/reservation/search') && document.querySelector('a.rounded-full.uppercase.mt-4.bg-primary.text-white.text-center.mt-4.px-4.text-xs.font-bold.py-2') != null && document.querySelector('.flex.flex-col.w-full.col-span-2.pb-5.pl-4.pr-0.self-center.mx-auto p') != null && document.querySelector('.self-end.flex.flex-col.px-4.pb-4.col-span-3.disabled a.rounded-full.uppercase.mt-4.bg-primary.text-white.text-center.mt-4.px-4.text-xs.font-bold.py-2') != null) {
         mut.disconnect();
         document.body.insertAdjacentHTML('afterbegin',`
         <style>
