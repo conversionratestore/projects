a1562faf8f8426351862424d027321017211734e
conversionratestore <82801260+conversionratestore@users.noreply.github.com>
2022-04-06 23:26:48 +0300

    Update new_prices.js

--- mammutmarsch/new_prices.js
+++ mammutmarsch/new_prices.js
@@ -1,18 +1,32 @@
 document.body.insertAdjacentHTML('afterbegin', `<style class="temp">
 .woocommerce-Price-amount, .starter_package_price, .additional_package_price, .checkout_total_price {opacity: 0}</style>`)
+let exclude = ['100 KM MARSCH BERLIN', '100 KM MARSCH MÜNCHEN', 'MAMMUTMARSCH NRW']
 
-function create() {
     setTimeout(function () {
+        let cart = true
+        if(document.querySelector('.cart_list a:not(.remove)')) {
+            exclude.forEach(item => {
+                if (jQuery('.cart_list a:not(.remove)').text().toUpperCase().includes(item)) {
+                    cart = false
+                }
+            })
+        }
+
         function changePrice(item) {
             let price = +item.innerText.slice(0, -1).trim().replace(',', '.')
             price = price - 5
             item.innerText = price.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'})
         }
 
-        jQuery('.woocommerce-Price-amount').each((__, item) => {
-            let price = +jQuery(item).text().slice(0, -1).trim().replace(',', '.')
-            price = price - 5
-            jQuery(item).text(price.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'}))
+        document.querySelectorAll('.product h3 a').forEach(item => {
+            if(!item.innerText.toUpperCase().includes('100 KM MARSCH BERLIN')
+                && !item.innerText.toUpperCase().includes('100 KM MARSCH MÜNCHEN')
+                && !item.innerText.toUpperCase().includes('MAMMUTMARSCH NRW'))
+            item.closest('.product').querySelectorAll('.woocommerce-Price-amount').forEach(span => {
+                let price = +jQuery(span).text().slice(0, -1).trim().replace(',', '.')
+                price = price - 5
+                jQuery(span).text(price.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'}))
+            })
         })
 
         function eventPrice() {
@@ -20,44 +34,41 @@ function create() {
                 changePrice(item)
             })
 
-            document.querySelectorAll('.additional_package_price').forEach((item) => {
-                changePrice(item)
-            })
+            // document.querySelectorAll('.additional_package_price').forEach((item) => {
+            //     changePrice(item)
+            // })
 
             document.querySelectorAll('.checkout_total_price').forEach((item) => {
                 changePrice(item)
             })
+
+            document.querySelector('.starter_package_price')?.nextSibling?.remove()
+            document.querySelector('.additional_package_price')?.nextSibling?.remove()
+            document.querySelector('.checkout_total_price')?.nextSibling?.remove()
         }
 
-        eventPrice()
+        if (cart) {
+            jQuery('.cart_list .woocommerce-Price-amount, .total .woocommerce-Price-amount').each((__, item) => {
+                let price = +jQuery(item).text().slice(0, -1).trim().replace(',', '.')
+                price = price - 5
+                jQuery(item).text(price.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'}))
+            })
+        }
 
-        jQuery('input[type=radio]').change(function () {
+        if(!window.location.pathname.includes('100-km-marsch/berlin') &&
+            !window.location.pathname.includes('100-km-marsch/muenchen') &&
+            !window.location.pathname.includes('100-km-marsch/nrw')) {
             eventPrice()
-        })
+            jQuery('input[type=radio]').change(function () {
+                eventPrice()
+            })
+        }
 
-        document.querySelector('.starter_package_price')?.nextSibling?.remove()
-        document.querySelector('.additional_package_price')?.nextSibling?.remove()
-        document.querySelector('.checkout_total_price')?.nextSibling?.remove()
-
-        if (window.location.pathname.includes('checkout')) {
+        if (window.location.pathname.includes('checkout') && cart) {
             document.querySelector('#coupon_code').value = 'Price_Test_CRO'
             document.querySelector('.checkout_coupon input[type="submit"]').click()
         }
 
         document.querySelector('.temp').remove()
     }, 500)
-}
 
-let exclude = ['100 KM MARSCH BERLIN', '100 KM MARSCH MÜNCHEN', 'MAMMUTMARSCH NRW']
-
-let start = setInterval(function () {
-    if (document.querySelector('.cart_list a:not(.remove)')) {
-        clearInterval(start)
-
-        exclude.forEach(item => {
-            if(!document.querySelector('.cart_list a:not(.remove)').innerText.toUpperCase().includes(item)){
-                create()
-            }
-        })
-    }
-}, 50)
