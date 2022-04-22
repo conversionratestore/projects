751bf76c075f1d0d4d753205ac776259e5769812
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2022-04-22 13:29:37 +0300

    Update pdp_improv_QA.js

--- kingsbox/pdp_improv_QA.js
+++ kingsbox/pdp_improv_QA.js
@@ -1846,8 +1846,12 @@ function addBadge() {
 				</div>
 			`
 
-		document.querySelector('.product-images-container div')?.insertAdjacentHTML('beforeend', sellImg)
-		document.querySelector('.product-images-container-mobile .product-image-wrapper div')?.insertAdjacentHTML('beforeend', sellImg)
+		if(!document.querySelector('.product-images-container div .eu')) {
+			document.querySelector('.product-images-container div').insertAdjacentHTML('beforeend', sellImg)
+		}
+		if(!document.querySelector('.product-images-container-mobile .product-image-wrapper div .eu')) {
+			document.querySelector('.product-images-container-mobile .product-image-wrapper div').insertAdjacentHTML('beforeend', sellImg)
+		}
 	}
 
 }
