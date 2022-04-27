db8ef9ef8802cbb098801db8eb2d923c8971e56e
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-04-27 17:05:20 +0300

    Update listing.js

--- medicalmega/listing.js
+++ medicalmega/listing.js
@@ -987,6 +987,7 @@ border-radius: 100px;
     align-items: center;
     cursor: pointer;
     font-size: 12px;
+    line-height: 1;
     justify-content: space-between;
   }
   .ais-ClearRefinements-button:hover svg {
@@ -1684,7 +1685,7 @@ window.onload = function() {
     instantsearch.widgets.clearRefinements({
       container: '#clear-refinements',
       templates: {
-        resetLabel: `Clear All Filters <svg width="13" height="13" fill="#666666" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" /></svg>`,
+        resetLabel: `Clear All Filters ✕`,
       },
       excludedAttributes: [
         'categories.lvl0',
