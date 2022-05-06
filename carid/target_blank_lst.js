42d5e4ddef8aa9851178b23b586c1631cf2ed4a2
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2022-05-06 18:33:55 +0300

    Update target_blank_lst.js

--- carid/target_blank_lst.js
+++ carid/target_blank_lst.js
@@ -22,91 +22,27 @@ let startfunk = setInterval(() => {
       }
     }
 
-    let prodLstStyle = /*html*/ `
-    <style>
+    addTargetBlankLst()
 
-    .link_target_blank  {
-        position: absolute;
-        right: 0;
-        top: 0;
-        cursor: pointer;
-        background: rgb(245 222 179 / 50%);
-        width: 100%;
-        height: 70%;
-        z-index: 5;
-        display: none;
-      }
-
-      .link_target_blank_var{
-        position: absolute;
-        right: 0;
-        bottom: 0;
-        cursor: pointer;
-        background: rgb(245 222 179 / 50%);
-        width: 100%;
-        height:10%;
-        z-index: 5;
-        display: none;
-      }
-
-      .js-main-prod-list li .-clone .link_target_blank_var,
-      .js-main-prod-list li .-clone .link_target_blank{
-        display: none;
-      }
-
-       .js-main-prod-list li .link_target_blank_var,
-       .js-main-prod-list li .link_target_blank{
-        display: block;
-      }
-
-      </style>
-    `
-
-    document.head.insertAdjacentHTML("beforeend", prodLstStyle)
-
-    // if (document.querySelector("#prod-list .js-main-prod-list.prod_lst")) {
-    //   addTargetBlankLst()
-    // }
-
-    document.querySelectorAll(".js-main-prod-list li").forEach((el) => {
-      if (!el.querySelector("a.test_btn")) {
-        addTargetBlankGrd(el)
-      }
-    })
-
-    // function addTargetBlankLst() {
-    //   console.log(`#prod_lst`)
-    //   document.querySelectorAll(".js-main-prod-list.prod_lst .lst_a").forEach(function (el) {
-    //     if (!el.getAttribute("data-lst")) {
-    //       el.addEventListener("click", function (e) {
-    //         if (e.target === el) {
-    //           e.preventDefault()
-    //           e.stopPropagation()
-    //           pushDataLayer("Click on view details button")
-
-    //           window.open(this.href, "_blank")
-    //         }
-    //       })
-
-    //       el.setAttribute("data-lst", "prod_lst")
-    //     }
-    //   })
-    // }
-
-    function addTargetBlankGrd(li) {
-      li.insertAdjacentHTML("afterbegin", `<a class="link_target_blank test_btn" href="" target="_blank"></a>`)
-
-      li.insertAdjacentHTML("beforeend", `<a class="link_target_blank_var test_btn" href="" target="_blank"></a>`)
-
-      addClickOnLink(".test_btn")
-
-      function addClickOnLink(selector) {
-        li.querySelectorAll(selector).forEach(function (el) {
-          el.addEventListener("click", function (e) {
-            pushDataLayer(`Click on view details button`)
+    function addTargetBlankLst() {
+      document.querySelectorAll(".js-main-prod-list li").forEach(function (el) {
+        if (!el.getAttribute("data-lst")) {
+          el.addEventListener("mouseenter", function () {
+            el.querySelectorAll(".lst_a").forEach((a) => {
+              if (!a.getAttribute("data-test")) {
+                a.addEventListener("click", function (e) {
+                  e.preventDefault()
+                  e.stopPropagation()
+                  pushDataLayer("Click on view details button")
+                  window.open(a.href, "_blank")
+                })
+              }
+              a.setAttribute("data-test", "1")
+            })
           })
-        })
-      }
+          el.setAttribute("data-lst", "1")
+        }
+      })
     }
 
     // observer
@@ -114,15 +50,7 @@ let startfunk = setInterval(() => {
       if (document) {
         observer.disconnect()
 
-        // if (document.querySelector("#prod-list .js-main-prod-list.prod_lst")) {
-        //   addTargetBlankLst()
-        // }
-
-        document.querySelectorAll(".js-main-prod-list li").forEach((el) => {
-          if (!el.querySelector("a.test_btn")) {
-            addTargetBlankGrd(el)
-          }
-        })
+        addTargetBlankLst()
 
         observer.observe(document, {
           childList: true,
