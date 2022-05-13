b769eb9210fb8fdb900efa26d61dfd5b91d410e1
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2022-05-13 15:11:29 +0300

    Update patches5-1.js

--- buzzpatch/patches5-1.js
+++ buzzpatch/patches5-1.js
@@ -1,86 +1,99 @@
-const priceInfo = ['47.50', '62', '124.95', '78', '9.5']
-
-const myPack = `
-<div class="js-packs my_pack"> 
-	<input type="radio" name="radios" id="radios-5" value=""> 
-	<label class="radio-inline" for="radios-5"> 5 Packs<br><span>$9.5 Each</span></label> 
-</div>
+const style = `
+	<style>	
+		.custom_checked label {
+			color: #fff !important;
+			background: #FF3C81;
+		}
+	</style>
 `
 
-let isSecondChecked = setInterval(() => {
-	if (document.querySelectorAll('.js-packs input:checked')[2] && document.querySelector('.my_pack input')) {
-		clearInterval(isSecondChecked)
-		document.querySelector('.my_pack input').click()
-	}
-}, 100)
+document.head.insertAdjacentHTML('beforeend', style)
 
-const onCLick = () => {
-	document.querySelector('.my_pack input').addEventListener('click', () => {
-		document.querySelector('.prices .pr').innerText = priceInfo[0]
-		document.querySelector('.prices .ps').innerText = priceInfo[1]
-		document.querySelector('.prices .rp').innerText = priceInfo[2]
-		document.querySelector('.prices .rs').innerText = priceInfo[3]
+const priceInfo = ['47.50', '62', '124.95', '78', '9.5']
 
-		window.dataLayer = window.dataLayer || []
-		dataLayer.push({
-			'event': 'event-to-ga',
-			'eventCategory': 'Exp — Tiles 5 instead of 1',
-			'eventAction': 'Clicks on 5 packs tile v2',
-		})
-	})
-}
+let waitForFrPack = setInterval(() => {
+    if (document.querySelector('.js-packs input')) {
+        clearInterval(waitForFrPack)
 
-let waitForPatch2 = setInterval(() => {
-	if (document.querySelectorAll('.js-packs label')[3]) {
-		clearInterval(waitForPatch2)
+        document.querySelector('.js-packs input').click()
+    }
+}, 200);
 
-		document.querySelectorAll('.js-packs')[3].hidden = true
-		document.querySelectorAll('.js-packs')[3].insertAdjacentHTML('afterend', myPack)
 
-		let waitForMyPack = setInterval(() => {
-			if (document.querySelector('.my_pack')) {
-				clearInterval(waitForMyPack)
+let waitForElement = setInterval(() => {
+    if (document.querySelectorAll('.js-packs label')[3]) {
+        clearInterval(waitForElement)
 
-				onCLick()
-			}
-		}, 100)
-	}
+        document.querySelectorAll('.js-packs label')[3].innerHTML = ` 5 Packs<br><span>$${priceInfo[4]} Each</span>`
+
+        document.querySelectorAll('.js-packs')[3].classList.add('my_custom')
+
+        document.querySelector('.form-group').addEventListener('click', (e) => {
+            document.querySelector('.custom_checked')?.classList.remove('custom_checked')
+
+            if (e.target.closest('.js-packs.my_custom')) {
+                e.preventDefault()
+
+                if (document.querySelector('.js-packs input:checked')) {
+                    document.querySelector('.js-packs input:checked').checked = false
+                }
+
+                if (document.querySelector('.prices .rs')) {
+                    document.querySelector('.js-packs.my_custom').classList.add('custom_checked')
+
+                    document.querySelector('.prices .pr').innerText = priceInfo[0]
+                    document.querySelector('.prices .ps').innerText = priceInfo[1]
+                    document.querySelector('.prices .rp').innerText = priceInfo[2]
+                    document.querySelector('.prices .rs').innerText = priceInfo[3]
+                }
+
+                window.dataLayer = window.dataLayer || []
+                dataLayer.push({
+                    'event': 'event-to-ga',
+                    'eventCategory': 'Exp — Tiles 5 instead of 1',
+                    'eventAction': 'Clicks on 5 packs tile v2',
+                })
+            }
+        })
+    }
 }, 200)
 
 document.querySelector('#addToCart').addEventListener('click', function (e) {
-	if (document.querySelector('.my_pack input:checked')) {
-		e.preventDefault()
+    if (document.querySelector('.custom_checked')) {
+        e.preventDefault()
 
-		let formData = { 'items': [{ 'id': '40266425696300', 'quantity': 1 }] }
+        let formData = { 'items': [{ 'id': '40266425696300', 'quantity': 1 }] }
 
-			; (async () => {
-				try {
-					await fetch('/cart/clear.js', { method: 'POST', headers: { 'Content-Type': 'application/json' } })
+            ; (async () => {
+                try {
+                    await fetch('/cart/clear.js', { method: 'POST', headers: { 'Content-Type': 'application/json' } })
 
-					await fetch('/cart/add.js', {
-						method: 'POST',
-						headers: { 'Content-Type': 'application/json' },
-						body: JSON.stringify(formData),
-					}).then(() => {
-						window.location.href = '/checkout'
-					})
-				} catch (err) {
-					console.error(err)
-				}
-			})()
-	}
+                    await fetch('/cart/add.js', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(formData),
+                    }).then(() => {
+                        window.location.href = '/checkout'
+                    })
+                } catch (err) {
+                    console.error(err)
+                }
+            })()
+    } else {
+
+    }
 })
 
 let waitForClarity = setInterval(() => {
-	if (typeof clarity === 'function') {
-		clearInterval(waitForClarity)
-		clarity('set', 'tiles_5_instead_of_1', 'variant_1')
-	}
+    if (typeof clarity === 'function') {
+        clearInterval(waitForClarity)
+        clarity('set', 'tiles_5_instead_of_1', 'variant_1')
+    }
 }, 100)
 
 window.dataLayer = window.dataLayer || []
 dataLayer.push({
-	'event': 'event-to-ga',
-	'eventCategory': 'Exp — Tiles 5 instead of 1',
-	'eventAction': 'loaded',
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp — Tiles 5 instead of 1',
+    'eventAction': 'loaded',
 })
