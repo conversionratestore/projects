85a4e0d36bcbdfed2352d23f1c6451b30821a768
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2022-05-16 18:24:02 +0300

    Update patches5-1.js

--- buzzpatch/patches5-1.js
+++ buzzpatch/patches5-1.js
@@ -1,24 +1,15 @@
-const style = `
-	<style>	
-		.custom_checked label {
-			color: #fff !important;
-			background: #FF3C81;
-		}
-	</style>
-`
-
-document.head.insertAdjacentHTML('beforeend', style)
-
 const priceInfo = ['47.50', '62', '124.95', '78', '9.5']
 
-let waitForFrPack = setInterval(() => {
-    if (document.querySelector('.js-packs input')) {
-        clearInterval(waitForFrPack)
+let waitForCheckedMyPack = setInterval(() => {
+    if (document.querySelectorAll('.js-packs input')[3].checked && document.querySelector('.rs')) {
+        clearInterval(waitForCheckedMyPack)
 
-        document.querySelector('.js-packs input').click()
+        document.querySelector('.prices .pr').innerText = priceInfo[0]
+        document.querySelector('.prices .ps').innerText = priceInfo[1]
+        document.querySelector('.prices .rp').innerText = priceInfo[2]
+        document.querySelector('.prices .rs').innerText = priceInfo[3]
     }
 }, 200);
-
 
 let waitForElement = setInterval(() => {
     if (document.querySelectorAll('.js-packs label')[3]) {
@@ -26,40 +17,23 @@ let waitForElement = setInterval(() => {
 
         document.querySelectorAll('.js-packs label')[3].innerHTML = ` 5 Packs<br><span>$${priceInfo[4]} Each</span>`
 
-        document.querySelectorAll('.js-packs')[3].classList.add('my_custom')
-
-        document.querySelector('.form-group').addEventListener('click', (e) => {
-            document.querySelector('.custom_checked')?.classList.remove('custom_checked')
-
-            if (e.target.closest('.js-packs.my_custom')) {
-                e.preventDefault()
-
-                if (document.querySelector('.js-packs input:checked')) {
-                    document.querySelector('.js-packs input:checked').checked = false
-                }
-
-                if (document.querySelector('.prices .rs')) {
-                    document.querySelector('.js-packs.my_custom').classList.add('custom_checked')
+        document.querySelectorAll('.js-packs')[3].addEventListener('click', (e) => {
+            let waitForPrice = setInterval(() => {
+                if (document.querySelector('.rs').innerText === '10') {
+                    clearInterval(waitForPrice)
 
                     document.querySelector('.prices .pr').innerText = priceInfo[0]
                     document.querySelector('.prices .ps').innerText = priceInfo[1]
                     document.querySelector('.prices .rp').innerText = priceInfo[2]
                     document.querySelector('.prices .rs').innerText = priceInfo[3]
                 }
-
-                window.dataLayer = window.dataLayer || []
-                dataLayer.push({
-                    'event': 'event-to-ga',
-                    'eventCategory': 'Exp — Tiles 5 instead of 1',
-                    'eventAction': 'Clicks on 5 packs tile v2',
-                })
-            }
+            }, 100)
         })
     }
 }, 200)
 
 document.querySelector('#addToCart').addEventListener('click', function (e) {
-    if (document.querySelector('.custom_checked')) {
+    if (document.querySelectorAll('.js-packs input')[3].checked) {
         e.preventDefault()
 
         let formData = { 'items': [{ 'id': '40266425696300', 'quantity': 1 }] }
@@ -79,8 +53,6 @@ document.querySelector('#addToCart').addEventListener('click', function (e) {
                     console.error(err)
                 }
             })()
-    } else {
-
     }
 })
 
@@ -89,7 +61,7 @@ let waitForClarity = setInterval(() => {
         clearInterval(waitForClarity)
         clarity('set', 'tiles_5_instead_of_1', 'variant_1')
     }
-}, 100)
+}, 200)
 
 window.dataLayer = window.dataLayer || []
 dataLayer.push({
