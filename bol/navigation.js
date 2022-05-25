19207cb1e724d2d73226b924d4ae020d29a6eebe
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2022-05-25 14:23:00 +0300

    Update navigation.js

--- bol/navigation.js
+++ bol/navigation.js
@@ -1,168 +1,187 @@
-// function abjql(e) {
-//     var a = document.createElement('script');
-//     (a.type = 'text/javascript'),
-//         a.readyState
-//             ? (a.onreadystatechange = function () {
-//                 ('loaded' !== a.readyState && 'complete' !== a.readyState) ||
-//                     ((a.onreadystatechange = null), e());
-//             })
-//             : (a.onload = function () {
-//                 e();
-//             }),
-//         (a.src = '//code.jquery.com/jquery-3.3.1.min.js'),
-//         document.getElementsByTagName('head')[0].appendChild(a);
-// }
+const style = /*html*/`
+        <style>
+            #main_navbar .col-md-9.col-sm-6 {
+                padding-top: 0 !important;
+            }
 
+            #cssmenu .menu-wrapper {
+                display: flex;
+            }
 
-function abjqlr() {
-    var e = {
-        AboutUs: 'About Us',
-        BuiltBetter: 'Built Better',
-        Testimonials: 'Testimonials',
-        Dealers: 'Dealers',
-        ContactUs: 'Contact Us',
-        marine: 'Marine',
-        night_vision: 'Night Vision',
-        vehicle: 'Vehicle',
-        hunting: 'Hunting',
-        atv_utv: 'ATV/UTV',
-        emergency: 'Emergency',
-        mounting: 'Mounting',
+            #nav .parent.has-sub:nth-child(3),
+            .header_nav .col-md-12,
+            .pull-right {
+                display: none;
+            }
+
+            #nav nav > .container-fluid > .row {
+                display: flex;
+                align-items: center;
+            }
+
+            #cssmenu [role="search"] {
+                width: 250px;
+                margin: 2px 6px 0;
+            }
+
+            #main_navbar .col-md-9.col-sm-6 {
+                padding: 0;
+            }
+
+            .navbar .col-md-12:first-child {
+                padding-left: 0;
+            }
+
+            #cssmenu, #cssmenu #menu-button, #cssmenu ul, #cssmenu ul li, #cssmenu ul li a {
+                line-height: 20px !important;
+            }
+
+            #cssmenu>ul>li>a {
+                padding: 10px !important;
+            }
+
+            #cssmenu>ul>li.has-sub>a {
+                padding-right: 26px !important;
+            }
+
+            #cssmenu>ul>li.has-sub>a:after {
+                right: 10px !important;
+            }
+
+            #header #cssmenu .search_menu .search_box .txtbox {
+                height: 36px !important;
+                padding: 5px 65px 5px 25px;
+                border-radius: 2px;
+            }
+
+            #header #cssmenu .search_menu .search_box button {
+                height: 36px !important;
+                right: 1px;
+            }
+
+            #cssmenu .search_box .fa.fa-search {
+                top: 11px;
+            }
+
+            .container.header_nav {
+                width: 100% !important;
+                padding: 0;
+                margin: 0;
+                z-index: 1 !important;
+                background: linear-gradient(180deg, #E9E9E9 0%, #DEDEDE 100%), #D9D9D9;
+            }
+
+            .my_navbar {
+                max-width: 960px;
+                margin: auto;
+            }
+
+            .my_navbar ul {
+                display: flex;
+                justify-content: center;                
+                margin: 0;
+                padding: 0;
+            }
+
+            .my_navbar ul li {
+                display: flex;
+                align-items: center;
+                padding: 7px 17px;
+                transition: background-color 0.2s ease-in-out;
+                cursor: pointer;
+            }
+            
+            #cssmenu ul li {
+                transition: background-color 0.2s ease-in-out;
+            }
+
+            .my_navbar ul li:hover {
+                background-color: #C0C0C0;
+            }
+
+            .my_navbar ul li a {
+                display: flex;
+                align-items: center;
+                gap: 6px;
+            }
+
+            .my_navbar ul li span {
+                color: #000;
+            }
+
+            #header .menu_c {
+                border-radius: 2px;
+            }
+        </style>`
+
+const myListItems = [
+    ['Marine', 'https://www.blackoakled.com/collections/marine-led-light-bars', 'marine'],
+    ['Night Vision', 'https://www.blackoakled.com/products/extra-new-nitron-xd-marine-night-vision-camera', 'night_vision'],
+    ['Vehicle', 'https://www.blackoakled.com/pages/search-by-vehicle', 'vehicle'],
+    ['Hunting', 'https://www.blackoakled.com/collections/hunting-led-light-bars', 'hunting'],
+    ['ATV/UTV', 'https://www.blackoakled.com/pages/search-by-vehicle?utv=true', 'atv_utv'],
+    ['Emergency', 'https://www.blackoakled.com/collections/emergency-response-vehicles', 'emergency'],
+    ['Mounting', 'https://www.blackoakled.com/collections/led-light-bar-mounts-2', 'mounting'],
+]
+
+const itemsTemplate = item => `
+    <li>
+        <a href=${item[1]}>
+            <img src="https://conversionratestore.github.io/projects/bol/img/${item[2]}.svg" alt="${item[0]}" />
+            <span>${item[0]}</span>
+        </a>
+    </li>`
+
+const myNavbar = `
+    <nav class="my_navbar">
+        <ul>
+            ${myListItems.map(itemsTemplate).join('')}
+        </ul>
+    </nav>`
+
+document.head.insertAdjacentHTML('beforeend', style)
+
+let waitForStart = setInterval(() => {
+    if (document.querySelectorAll('.parent.has-sub')[3] && document.querySelector('#nav .col-md-12.pull-right')) {
+        clearInterval(waitForStart)
+
+        document.querySelector('#nav .col-md-12.pull-right').insertAdjacentElement('beforebegin', document.getElementById('cssmenu'))
+        document.querySelectorAll('#cssmenu .parent.has-sub')[2].insertAdjacentElement('afterend', document.querySelector('#nav .nav-search form'))
+
+        document.querySelectorAll('#cssmenu .parent.has-sub')[1].querySelector('a').innerText = 'Vehicle'
+        document.querySelectorAll('#cssmenu .parent.has-sub')[3].querySelector('a').innerText = 'About Us'
     }
+}, 200);
 
-    window.dataLayer = window.dataLayer || [], dataLayer.push({
-        event: "event-to-ga",
-        eventCategory: "Exp - PL Navigation v.2",
-        eventAction: "loaded"
-    })
+let waitForPlace = setInterval(() => {
+    if (document.querySelector('.header_nav')) {
+        clearInterval(waitForPlace)
 
-    var i = e,
-        l = {
-            marine: {
-                text: i.marine,
-                link: 'https://www.blackoakled.com/collections/marine-led-light-bars',
-            },
-            night_vision: {
-                text: i.night_vision,
-                link: 'https://www.blackoakled.com/products/extra-new-nitron-xd-marine-night-vision-camera',
-            },
-            vehicle: {
-                text: i.vehicle,
-                link: 'https://www.blackoakled.com/pages/search-by-vehicle',
-            },
-            hunting: {
-                text: i.hunting,
-                link: 'https://www.blackoakled.com/collections/hunting-led-light-bars',
-            },
-            atv_utv: {
-                text: i.atv_utv,
-                link: 'https://www.blackoakled.com/pages/search-by-vehicle?utv=true',
-            },
-            emergency: {
-                text: i.emergency,
-                link: 'https://www.blackoakled.com/collections/emergency-response-vehicles',
-            },
-            mounting: {
-                text: i.mounting,
-                link: 'https://www.blackoakled.com/collections/led-light-bar-mounts-2',
-            },
-        },
-        r =
-            '<div class="ab-test-menu"><span class="test-menu-control control-prev"></span>',
-        d = 1,
-        c = !1;
-    for (item in l) {
-        var u = l[item],
-            h =
-                'https://conversionratestore.github.io/projects/bol/img/' +
-                item +
-                '.svg';
-        (r += '<a href="' + u.link + '" class="test-item-event '),
-            location.href == u.link && 0 == c && ((r += 'active '), (c = !0)),
-            (r +=
-                'test-item-' +
-                d +
-                '" data-scrolgoto="' +
-                d +
-                '"><img src="' +
-                h +
-                '"/><span>' +
-                u.text +
-                '</span></a>'),
-            d++;
+        document.querySelector('.header_nav').insertAdjacentHTML('afterbegin', myNavbar)
     }
-    (r += '<span class="test-menu-control control-next"></span></div>'),
-        $('.header_nav .col-md-12').append(r),
-        $('.header_nav .col-md-12').addClass('at-wraper'),
-        $('.navbar-right .dropdown-grid.no-open-arrow.extra_img').remove();
-    var p =
-        '<ul><li class="parent has-sub"><a href="/pages/about-us" class="submenu-main-link">' +
-        i.AboutUs +
-        '</a><ul class="submenu"><li><a href="/pages/how-black-oak-ranks-superior">BuiltBetter</a></li><li><a href="/pages/customer-testimonials">Testimonials</a></li></ul></li><li><a href="/pages/wholesale">Dealers</a></li><li><a href="/pages/contact-us">ContactUs</a></li></ul>',
-        m = $('#cssmenu .parent.has-sub:eq(0)').clone(),
-        b = $('#cssmenu .parent.has-sub:eq(1)').clone(),
-        g = $('.navbar-right .nav-search').html();
-    function v() {
-        $('body').find('.ab-test-menu').scrollLeft(0),
-            $('body')
-                .find('.ab-test-menu a')
-                .each(function (e, a) {
-                    var t =
-                        $(this).offset().left -
-                        $('body').find('.ab-test-menu').offset().left;
-                });
-    }
-    $('.row.collapse.navbar-collapse.no-transition').html(
-        '<div class="col-md-12 search-and-menu"><div class="left-menu"><ul></ul></div><ul></ul>' +
-        g +
-        '<div class="right-menu">' +
-        p +
-        '</div></div>'
-    ),
-        m.appendTo('body .search-and-menu .left-menu ul'),
-        b.appendTo('body .search-and-menu .left-menu ul'),
-        $('.search-and-menu .left-menu [href="/pages/search-by-vehicle"]').html(
-            i.vehicle
-        ),
-        $('.header_nav .menu-outer-wrapper').remove(),
-        $('.header_nav').addClass('ab-test-header').removeClass('header_nav'),
-        $('body').find('.ab-test-menu').data('current_pos', 1),
-        v(),
-        $(window).resize(function () {
-            v();
-        }),
-        $(document).on('click', '.test-menu-control', function (e) {
-            var a,
-                t = $('body').find('.ab-test-menu').data('current_pos');
-            t < 0 && (t = 1),
-                t > $('body').find('.ab-test-menu a').length && (t = 1),
-                $(this).hasClass('control-next') ? t++ : t--,
-                (a = t),
-                $('body')
-                    .find('.ab-test-menu')
-                    .animate({
-                        scrollLeft: $('body')
-                            .find('.ab-test-menu a:eq(' + a + ')')
-                            .data('firstposition'),
-                    });
-        }),
-        $(document).on('click', '.ab-test-menu a', function () {
-            var e = $(this).find('span').text();
-            (window.dataLayer = window.dataLayer || []),
+}, 200);
+
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp - PL Navigation v.2',
+    'eventAction': 'loaded'
+});
+
+let waitForMyList = setInterval(() => {
+    if (document.querySelectorAll('.my_navbar li')[6]) {
+        clearInterval(waitForMyList)
+
+        document.querySelector('.my_navbar').addEventListener('click', (e) => {
+            if (e.target?.closest('li')) {
+                window.dataLayer = window.dataLayer || [];
                 dataLayer.push({
-                    event: 'event-to-ga',
-                    eventCategory: 'Exp - PL Navigation v.2',
-                    eventAction: 'click on navigation',
-                    eventLabel: e,
-                });
-        }),
-        $('body').append(
-            '<style>.ab-test-header{width:100%!important;background:linear-gradient(180deg,#e9e9e9,#dedede),#d9d9d9}.ab-test-header .col-md-12{overflow:hidden;padding:0}.ab-test-header .col-md-12,.ab-test-menu{display:flex;justify-content:space-between}.ab-test-menu{max-width:990px;margin:0 auto;position:relative}.ab-test-menu a{color:#231f20;display:flex;align-items:center;flex:0 0 auto;justify-content:center;padding:5px 17px;transition:background-color .2s ease-in-out}.ab-test-menu a:active,.ab-test-menu a:hover{background-color:#a9a9a9}.ab-test-menu a img{margin-right:6px;}.ab-test-menu a.active{background-color:#a9a9a9}span.test-menu-control{display:block;cursor:pointer;width:28px;height:28px;position:absolute}span.test-menu-control.control-next{right:0}.submenu{display:none;position:absolute}.has-sub{position:relative}.has-sub:after{position:absolute;top:22px;right:11px;width:8px;height:2px;display:block;background:0 0;content:"";vertical-align:middle;border-top:4px solid #fff;border-right:4px solid transparent;border-left:4px solid transparent}.has-sub>a:hover+.submenu{display:block}#main_navbar .col-md-9.col-sm-6{padding-top:20px;padding-left:0}.parent.has-sub .submenu-main-link+ul{display:none;position:absolute;top:auto;left:0}.parent.has-sub .submenu-main-link:hover+ul,.parent.has-sub:hover>.submenu-main-link+ul{display:block}.search-and-menu{display:flex;align-items:center;justify-content:space-between;padding:0}#header .search-and-menu form[role=search]{width:297px;margin:0 10px}#header .search-and-menu form[role=search] .menu_c{border-radius:2px;overflow:hidden}#header .search-and-menu form[role=search] .search_menu .search_box button{right:0}.search-and-menu ul{display:flex;margin:0;z-index:1;flex-wrap:wrap}.search-and-menu ul li a{padding:13px 17px 13px 10px;display:block;color:#fff}.search-and-menu ul li a:hover{text-decoration:underline;color:#fff}.search-and-menu ul li.has-sub a{padding-right:25px}.search-and-menu ul ul li{border:1px solid #676767}.search-and-menu ul ul li:hover a{color:#393939;background:#ddd!important;border-bottom:1px solid hsla(0,0%,59%,.15)!important}.search-and-menu ul ul li a{color:#393939;background:#ddd;border-bottom:1px solid hsla(0,0%,59%,.15);padding:11px 15px;width:195px}.search-and-menu ul ul .has-sub:after{border-top:4px solid #393939}.search-and-menu ul ul .has-sub:hover>ul{display:block}.search-and-menu ul ul ul{right:auto;left:100%!important;top:0!important}.search-and-menu ul ul ul li:hover a,.search-and-menu ul ul ul li a{background-color:#f3f3f3}</style>'
-        );
-}
-let waitforjQ = setInterval(() => {
-    if (window.jQuery) {
-        clearInterval(waitforjQ)
-        abjqlr()
+                    'event': 'event-to-ga',
+                    'eventCategory': 'Exp - PL Navigation v.2',
+                    'eventAction': 'click on navigation',
+                    'eventLabel': e.target.closest('li')?.querySelector('span')?.innerText
+                })
+            }
+        })
     }
 }, 200);
