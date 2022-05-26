0e066c72bff86960e97fcc6a8eac3e9c1382921b
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-05-26 12:13:48 +0300

    Update listing_and_pdp.js

--- medicalmega/listing_and_pdp.js
+++ medicalmega/listing_and_pdp.js
@@ -1986,7 +1986,32 @@ window.onload = function() {
           
           if (isSearchStalled === false ) {
             console.log(isSearchStalled)
-          
+
+            let intervalSubmit = setInterval(() => {
+              if (document.querySelector('#form-search .ais-SearchBox-submit') != null) {
+                clearInterval(intervalSubmit)
+                console.log( document.querySelector('#form-search .ais-SearchBox-submit'))
+                document.querySelector('#form-search .ais-SearchBox-submit').addEventListener('click', (e) => {
+                  e.stopImmediatePropagation()
+                  search.helper.state.hierarchicalFacetsRefinements['categories.lvl0'] = [];
+                  // document.querySelector('.ais-ClearRefinements-button').classList.add('action-clean');
+                  // document.querySelector('.ais-ClearRefinements-button').click()
+                  if (document.querySelector('.advanced-search.active') != null) {
+                    document.querySelector('.advanced-search').classList.remove('active');
+                  }
+                  toggleListing(true)
+
+                  query = document.querySelector('#form-search .ais-SearchBox-input').value;
+                  console.log(query)
+                  search._searchFunction(search.helper)
+                  search.refresh()
+                  
+                  actionDataLayer = `Click on submit button`;
+                  labelDataLayer = 'Search by Name';
+                  pushDataLayer(actionDataLayer, labelDataLayer)
+                });
+              }
+            })
             if (document.querySelector('#price_group li') != null) {
               let pricesContainer = document.querySelector('#price_group ul'),
               para = document.querySelectorAll('#price_group li');
@@ -2136,27 +2161,7 @@ window.onload = function() {
   ]); 
 
   search.start();
-  
-  console.log( document.querySelector('#form-search .ais-SearchBox-submit'))
-  document.querySelector('#form-search .ais-SearchBox-submit').addEventListener('click', (e) => {
-    e.stopImmediatePropagation()
-    search.helper.state.hierarchicalFacetsRefinements['categories.lvl0'] = [];
-    // document.querySelector('.ais-ClearRefinements-button').classList.add('action-clean');
-    // document.querySelector('.ais-ClearRefinements-button').click()
-    if (document.querySelector('.advanced-search.active') != null) {
-      document.querySelector('.advanced-search').classList.remove('active');
-    }
-    toggleListing(true)
 
-    query = document.querySelector('#form-search .ais-SearchBox-input').value;
-    console.log(query)
-    search._searchFunction(search.helper)
-    search.refresh()
-    
-    actionDataLayer = `Click on submit button`;
-    labelDataLayer = 'Search by Name';
-    pushDataLayer(actionDataLayer, labelDataLayer)
-  });
 
   let dataButton = document.querySelectorAll('[data-button]'), // btn for open or bloc
       closeBtn = document.querySelectorAll('[data-close]'); //btn close for hide popup or block
