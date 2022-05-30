c4f6c9435646a24702ecd6e6a47c000a563b9df5
conversionratestore <82801260+conversionratestore@users.noreply.github.com>
2022-05-30 13:13:41 +0300

    Create test-quiz_mobile.js

---
+++ somnifix/test-quiz_mobile.js
@@ -0,0 +1,157 @@
+if (window.location.pathname.includes('blogs')) {
+    let start = setInterval(function () {
+        if(document.querySelectorAll(`h2`)[1]) {
+            clearInterval(start)
+            blogPage()
+        }
+    }, 100)
+
+
+} else {
+    let start = setInterval(function () {
+        if(document.querySelectorAll('.shg-btn-wrapper')[1]) {
+            clearInterval(start)
+            mainPage()
+        }
+    }, 100)
+}
+
+
+
+function blogPage() {
+
+    const style = `
+        <style>
+          .blog_banner {
+            border: 1px solid #DDDDDD;
+            border-radius: 10px;
+            padding: 10px;
+            display: flex;
+            flex-direction: column;
+          }
+          
+          .blog_banner .title {
+            font-size: 24px;
+            font-weight: 700;
+            color: #1E415C;
+            margin: 10px 0 8px !important;
+            text-align: center;
+          }
+          
+          .blog_banner .title+p {
+            font-size: 14px;
+            color: #434B55;
+            margin: 0 0 14px;
+            text-align: center;
+          }
+          
+          .blog_banner a {
+            display: flex;
+            justify-content: center;
+            border-radius: 40px;
+            background: #1F405C;
+            font-size: 15px;
+            font-weight: 700;
+            color: white;
+            text-transform:uppercase;
+            padding: 15px 30px;
+          }
+        </style>
+    `
+
+    const banner = `
+        <div class="blog_banner">
+           <img src="https://conversionratestore.github.io/projects/somnifix/img/quiz/blog_banner.jpg" alt="banner">
+           <div>
+           <p class="title">How good is your sleep?</p>
+           <p>Take our quiz and determine your sleep quality</p>
+           <a href="https://somnifix.com/pages/quiz-11" class="to_quiz2">Let’s find out</a>
+           </div>
+        </div>
+    `
+    document.body.insertAdjacentHTML('afterbegin', style)
+
+
+    if(window.location.pathname.includes('cpap-machine-hack-overcoming-mouth-breathing')) {
+        document.querySelectorAll(`.article-content img`)[1].insertAdjacentHTML('afterend', banner)
+    } else {
+        document.querySelectorAll(`h2`)[1]?.insertAdjacentHTML('beforebegin', banner)
+    }
+
+    document.querySelector('.to_quiz2').addEventListener('click', function () {
+        let page = window.location.pathname.split('snews/')[1]
+
+        window.dataLayer = window.dataLayer || [];
+        dataLayer.push({
+            'event': 'event-to-ga',
+            'eventCategory': 'Exp — Quiz hp and blog pages (mobile)',
+            'eventAction': 'Click on the Lets find out button',
+            'eventLabel': `On blog page: ${page}`
+        });
+    })
+}
+
+function mainPage() {
+    const style = `
+    <style>
+        #s-b4e509bc-83be-4723-bbbf-02b6e7f40e88-btn-wrapper {
+          display: flex;
+          align-items: center;
+        }
+        
+        #s-b4e509bc-83be-4723-bbbf-02b6e7f40e88-btn-wrapper>a:first-child {
+          flex-shrink: 0;
+        }
+    
+       .to_quiz1 {
+          display: inline-flex;
+          justify-content: center;
+          align-items: center;
+          color: #68A1C0;
+          text-transform: uppercase;
+          font-size: 12px;
+          font-weight: 500;
+          padding: 7px 25px;
+          line-height: 1.2;
+          margin: 40px 0;
+          background: white;
+          width: 150px;
+          text-align: center;
+       }
+    </style>
+`
+
+    const mainLink = `
+    <a href="https://somnifix.com/pages/quiz-11" class="to_quiz1">Discover Your Sleep Quality</a>
+`
+    document.body.insertAdjacentHTML('afterbegin', style)
+    document.querySelectorAll('.shg-btn-wrapper')[1].insertAdjacentHTML('beforeend', mainLink)
+
+    document.querySelector('.to_quiz1').addEventListener('click', function () {
+        window.dataLayer = window.dataLayer || [];
+        dataLayer.push({
+            'event': 'event-to-ga',
+            'eventCategory': 'Exp — Quiz hp and blog pages (mobile)',
+            'eventAction': 'Click on the Define Your Sleep Quality button',
+            'eventLabel': 'On first screen homepage'
+        });
+    })
+}
+
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp — Quiz hp and blog pages (mobile)',
+    'eventAction': 'loaded'
+});
+
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1271698,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
+hj('event', 'quiz_hp_blog');
