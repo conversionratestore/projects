3b073219885c8556b9a9dc69ff157589703dea8a
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-06-14 18:10:22 +0300

    Update parking.js

--- onairparking/parking.js
+++ onairparking/parking.js
@@ -202,6 +202,23 @@ class Parking{
         this.price = price;
     }
 
+    renderStar() {
+        let stars = '',
+            starGold = `<svg viewBox="64 64 896 896" fill="#fadb14" focusable="false" data-icon="star" width="15px" height="15px" fill="currentColor" aria-hidden="true"><path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path></svg>`,
+            starGray = `<svg viewBox="64 64 896 896" fill="#ccc" focusable="false" data-icon="star" width="15px" height="15px" fill="currentColor" aria-hidden="true"><path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path></svg>`;
+       
+        let review = this.reviews.split('/')[0];
+        for (let i = 0; i < 5; i++) {
+            review[i];
+            if (i < review.length) {
+                stars += starGold;
+            } else {
+                stars += starGray;
+            }
+        }
+        return stars
+    }
+
     render() {
         let element = document.createElement('li');
         element.classList.add('flex');
@@ -213,7 +230,7 @@ class Parking{
             <div class="info_parking">
                 <div>
                     <a href="https://www.onairparking.com/parkingat/${this.url}">${this.name}</a>
-                    <div class="rate_parking">${this.reviews}</div>
+                    <div class="rate_parking flex" data-rate="${this.reviews.split('/')[1]}">${renderStar()}</div>
                 </div>
                 <ul>
                     <li>Distance: <b>${this.distance}</b></li>
@@ -267,7 +284,53 @@ function starInterval() {
             document.querySelector('h1').innerHTML = `${title.substring(title.indexOf(' ') + 1)} <span> From <br>${title.substring(0, title.indexOf(' '))} / day</span>`
 
             //add "Check availability" button
-            document.querySelector('button[data-test-id="park_now"]').insertAdjacentHTML('afterend',`<button type="button" class="h-14 mt-3 md:mt-0 md:ml-2 bg-secondary text-white text-base rounded-full p-4 hover:bg-opacity-75 focus:outline-none w-full md:w-48 flex flex-row items-center justify-center uppercase font-bold">Check availability</button>`)
+            document.querySelector('button[data-test-id="park_now"]').insertAdjacentHTML('afterend',`<button type="button" id="btn_check_availability" class="h-14 mt-3 md:mt-0 md:ml-2 bg-secondary text-white text-base rounded-full p-4 hover:bg-opacity-75 focus:outline-none w-full md:w-48 flex flex-row items-center justify-center uppercase font-bold">Check availability</button>`)
+            
+
+            let arr = document.querySelector('#__NEXT_DATA__').innerHTML.split(',"airport_initials":"SFO"')[0].split('"airport_id":');
+            id = arr[arr.length - 1]
+
+            document.querySelector('#btn_check_availability').addEventListener('click', () => {
+                let startDate = document.querySelector('[data-test-id="mob_start_date"]').value, 
+                    endDate = document.querySelector('[data-test-id="mob_end_date"]').value;
+
+                fetch(`https://www.onairparking.com/api/Facility/SearchAlternate`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    method: 'POST',
+                    body: `{"airport_id":${id},"start_date":"${startDate}","end_date":"${endDate}"}`
+    
+                }).then(res => res.json()).then(data => {
+                    console.log(data)
+                    let result = data.result;
+                    for (let i = 0; i < result.length; i++) {
+                        let url = `/parkingat/${result[i].facility_url_code}?checkin=${startDate}&checkout=${endDate}`,
+                            image ,
+                            name,
+                            reviews = result[i].facility_review + '/' + result[i].facility_review_avg,
+                            distance,
+                            shuttle,
+                            shuttleFrequency,
+                            freeCancellation,
+                            price = result[i].facility_selling_price;
+
+                        new Parking(url,image,name,reviews,distance,shuttle,shuttleFrequency,freeCancellation,price).render();
+                        
+                    }
+
+                    //bestReviews
+//                     let list = document.querySelectorAll("#list_parking > li");
+                    
+//                     list.forEach(item => {
+//                         if (item.length >= 5 && item.querySelector('.lowest_price') == null && document.querySelector('.best_reviews') == null) {               
+//                             item.querySelector('.img_parking').insertAdjacentHTML('afterbegin', bestReviews)
+//                         }
+//                     }
+                   
+                }) 
+    
+            })
             
             //set format date
             function setFormat(date) {
@@ -306,12 +369,6 @@ starInterval()
 
 // let list = document.querySelectorAll("#list_parking > li");
 
-//bestReviews
-// list.forEach(item => {
-//     if (item.length >= 5 && item.querySelector('.lowest_price') == null && document.querySelector('.best_reviews') == null) {               
-//         item.querySelector('.img_parking').insertAdjacentHTML('afterbegin', bestReviews)
-//     }
-// }
 //lowerPrice
 // let minNumber = [].reduce.call(list, (a, b) => 0 >= a.innerHTML.replace('$','') - b.innerHTML.replace('$','') ? a : b)
 
