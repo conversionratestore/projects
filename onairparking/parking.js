d640171a40f1e0d1ed9daca6f2ac4838e44adb8a
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-06-16 10:04:02 +0300

    Update parking.js

--- onairparking/parking.js
+++ onairparking/parking.js
@@ -296,7 +296,7 @@ class Parking{
         if (this.soldOut == 1 && this.unavailable != 1) {
             return `<small class="block mt-1.5">This facility is sold out for this period. <span class="block text-secondary underline cursor-pointer">Change the dates!</span></small>`
         } else if (this.unavailable == 1) {
-            return `<small class="block mt-1.5">This facility requires a minimum <span class="truncate">of <strong>${this.minDay}</strong> days.</span> <span class="text-secondary underline cursor-pointer block">Change the dates!</span></small>` : ''
+            return `<small class="block mt-1.5">This facility requires a minimum <span class="truncate">of <strong>${this.minDay}</strong> days.</span> <span class="text-secondary underline cursor-pointer block">Change the dates!</span></small>`
         } else {
             return ''
         }
