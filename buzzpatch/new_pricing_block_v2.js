006501975fbfda959ec92d92e50803b556f9a86c
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-06-21 17:16:56 +0300

    commit

--- buzzpatch/new_pricing_block_v2.js
+++ buzzpatch/new_pricing_block_v2.js
@@ -74,39 +74,40 @@ let html = `
         <div class="ps_wrap"> Save $<span class="rs">39</span> (<span class="ps">52</span>% OFF) </div> 
     </div>
 `;
+window.onload  = function () {
+    document.head.insertAdjacentHTML('beforeend',`<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">`); //add font
+    document.body.insertAdjacentHTML('afterbegin', style); //add style
+    document.querySelector('.package .form').insertAdjacentHTML('beforebegin', html); //add html prices
+    document.querySelector('#purchase #getNow img').style = `margin-bottom: -40px;`; //image PACKAGE
+    document.querySelector('#getNow p').style.display = 'none'; //hide text behind title
+    document.querySelector('#getNow img').srcset = `https://conversionratestore.github.io/projects/buzzpatch/img/package_patches.png`; 
+    document.querySelector('#addToCart').insertAdjacentHTML('afterend',`<img src="https://conversionratestore.github.io/projects/buzzpatch/img/trust.png" alt="trust icon">`) //add Trustpilot block behind the "PROCEED TO CHECKOUT" button
 
-document.head.insertAdjacentHTML('beforeend',`<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">`); //add font
-document.body.insertAdjacentHTML('afterbegin', style); //add style
-document.querySelector('.package .form').insertAdjacentHTML('beforebegin', html); //add html prices
-document.querySelector('#purchase #getNow img').style = `margin-bottom: -40px;`; //image PACKAGE
-document.querySelector('#getNow p').style.display = 'none'; //hide text behind title
-document.querySelector('#getNow img').srcset = `https://conversionratestore.github.io/projects/buzzpatch/img/package_patches.png`; 
-document.querySelector('#addToCart').insertAdjacentHTML('afterend',`<img src="https://conversionratestore.github.io/projects/buzzpatch/img/trust.png" alt="trust icon">`) //add Trustpilot block behind the "PROCEED TO CHECKOUT" button
+    let price = document.querySelector('.pr_wrap .pr'), 
+        priceOld = document.querySelector('.rp_wrap .rp'),
+        off = document.querySelector('.ps_wrap .ps'),
+        forPack = document.querySelector('.for_pack b'),
+        save = document.querySelector('.ps_wrap .rs');
 
-let price = document.querySelector('.pr_wrap .pr'), 
-    priceOld = document.querySelector('.rp_wrap .rp'),
-    off = document.querySelector('.ps_wrap .ps'),
-    forPack = document.querySelector('.for_pack b'),
-    save = document.querySelector('.ps_wrap .rs');
+    //change prices
+    document.querySelectorAll('.js-packs').forEach((pack,i) => {
+        pack.addEventListener('change', (e) => {
+            forPack.innerHTML = pack.querySelector('label').innerHTML.toLowerCase().split('<br')[0];
 
-//change prices
-document.querySelectorAll('.js-packs').forEach((pack,i) => {
-    pack.addEventListener('change', (e) => {
-        forPack.innerHTML = pack.querySelector('label').innerHTML.toLowerCase().split('<br')[0];
+            price.innerHTML = +document.querySelector('.prices .js-total .pr').innerHTML;
+            priceOld.innerHTML = document.querySelector('.prices .js-regular .js-strike .rp').innerHTML;
+            off.innerHTML = document.querySelector('.prices .js-total .ps').innerHTML;
+            save.innerHTML = document.querySelector('.prices .js-regular .rs').innerHTML;
 
-        price.innerHTML = +document.querySelector('.prices .js-total .pr').innerHTML;
-        priceOld.innerHTML = document.querySelector('.prices .js-regular .js-strike .rp').innerHTML;
-        off.innerHTML = document.querySelector('.prices .js-total .ps').innerHTML;
-        save.innerHTML = document.querySelector('.prices .js-regular .rs').innerHTML;
-
-        window.dataLayer = window.dataLayer || [];
-        dataLayer.push({
-            'event': 'event-to-ga',
-            'eventCategory': 'Exp: New pricing block',
-            'eventAction': `Clicks on package - ${forPack.innerHTML}`
-        });
+            window.dataLayer = window.dataLayer || [];
+            dataLayer.push({
+                'event': 'event-to-ga',
+                'eventCategory': 'Exp: New pricing block',
+                'eventAction': `Clicks on package - ${forPack.innerHTML}`
+            });
+        })
     })
-})
+};
 
 window.dataLayer = window.dataLayer || [];
 dataLayer.push({
