8f0d6b372e65f9211d499337c20979939385d0f5
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-06-24 11:02:40 +0300

    commit

--- mammutmarsch/t-shirt_upsell.js
+++ mammutmarsch/t-shirt_upsell.js
@@ -277,6 +277,8 @@ let interval = setInterval(() => {
         }) 
         document.querySelector('.modal .btn_skip').addEventListener('click', (e) => pushDataLayer('Scip offer selected')) //event
         document.querySelector('.modal .btn_add-order').addEventListener('click', (e) => pushDataLayer('T-shirt added to the order')) //event
+        
+        document.querySelector('.modal').style.height = window.innerHeight;
     }
 })
 
