c3e1dfec3086ea96e5c507240114aed3ddf58dc2
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2022-07-08 14:16:56 +0300

    Update zuck_fix.js

--- buzzpatch/zuck_fix.js
+++ buzzpatch/zuck_fix.js
@@ -526,7 +526,10 @@
             .concat(get(storyData, "photo"), ');"></u><div><strong>')
             .concat(get(storyData, "name"), '</strong><span class="time">')
             .concat(currentItemTime, '</span></div></div><div class="right"><span class="time">')
-            .concat(currentItemTime, '</span><span class="loading"></span><a class="close" tabIndex="2">&times;</a></div></div><div class="slides-pointers"><div>')
+            .concat(
+              currentItemTime,
+              '</span><span class="loading"></span><a class="close" tabIndex="2"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="white"/></svg></a></div></div><div class="slides-pointers"><div>'
+            )
             .concat(pointerItems, "</div></div>")
           storyViewer.innerHTML = html
           each(storyViewer.querySelectorAll(".close, .back"), function (i, el) {
