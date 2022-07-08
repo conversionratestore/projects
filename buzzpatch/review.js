d389f1721ab15921b8cd82aa67c9818a9aac126f
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2022-07-08 12:31:49 +0300

    Update review.js

--- buzzpatch/review.js
+++ buzzpatch/review.js
@@ -57,7 +57,7 @@ let startFunkReview = setInterval(() => {
         
         #zuck-modal-content .story-viewer .head .left{
           float: right !important;
-          margin: 14px 0 !important;
+          margin: 8px 0 !important;
           width: 33px !important;
         }
 
