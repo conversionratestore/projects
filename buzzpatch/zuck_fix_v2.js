0f6c445898aecf949b41c0286f19eedca2b37968
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2022-07-11 12:20:41 +0300

    Update zuck_fix_v2.js

--- buzzpatch/zuck_fix_v2.js
+++ buzzpatch/zuck_fix_v2.js
@@ -607,7 +607,7 @@
             )
             .concat(
               currentItemTime,
-              '</span><span class="loading"></span><a class="close" tabIndex="2">x</a></div></div><div class="slides-pointers"><div>'
+              '</span><span class="loading"></span><a class="close" tabIndex="2">&times;</a></div></div><div class="slides-pointers"><div>'
             )
             .concat(pointerItems, "</div></div>");
           storyViewer.innerHTML = html;
