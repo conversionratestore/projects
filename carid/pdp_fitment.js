4172de2efb2aff4ba05c081a4ca767cbf84b3558
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2022-07-25 09:47:19 +0300

    Add files via upload

---
+++ carid/pdp_fitment.js
@@ -0,0 +1,333 @@
+/** Initialization */
+
+/* shorthands */
+const query = document.querySelector.bind(document)
+const queryAll = document.querySelectorAll.bind(document)
+const getId = document.getElementById.bind(document)
+
+const intervalTimeout = 200
+
+const imgFolderUrl = 'https://conversionratestore.github.io/projects/carid/img/'
+
+const style = /*html*/`
+    <style>
+        /* PDP */
+
+        .prod_vehicle {
+            padding-top: 0;
+            display: flex;
+            align-items: center;
+        }
+
+        .fit_select_car p {
+            font-weight: 400;
+            font-size: 18px;
+            color: #000;
+            margin: 0;
+            line-height: normal;
+        }
+
+        .fit_select_car p:last-child {
+            margin-top: 5px;
+            font-size: 12px;
+        }
+
+        .fit_select_car span,
+        .js-product-options-link {
+            color: #3E61BC;
+            text-decoration: underline;   
+            font-weight: 700;    
+            cursor: pointer;     
+        }
+
+        .js-product-options-link {
+            font-weight: 400;     
+            font-size: 12px;    
+        }
+
+        .prod_vehicle {
+            display: none;
+        }
+
+        /* Popup */
+
+        .po_header {
+            text-transform: initial !important;
+            padding-bottom: 10px !important;
+        }
+
+        .po_header .header {
+            font-weight: 700;
+            font-size: 32px;
+            color: #FFFFFF;
+        }
+
+        .po_header .subheader {
+            font-weight: 300;
+            font-size: 14px;
+            color: #FFFFFF;
+        }
+
+        .fit_car {
+            position: absolute;
+            top: 18px;
+            right: 22px;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+        }
+
+        .fit_car.pdp {
+            position: relative;
+            top: auto;
+            right: auto;
+        }
+
+        .fit_select_car[hidden],
+        .fit_car[hidden],
+        .prod_vehicle[hidden] {
+            display: none;
+        }
+
+        .fit_car div:first-child { 
+            display: flex; 
+            margin-right: 9px;
+        }
+
+        .fit_car p {
+            font-weight: 400;
+            font-size: 12px;
+            line-height: 14px;
+            color: #000000;
+            margin: 0;
+        }
+
+        .fit_car p:last-child {
+            font-weight: 500;
+        }
+
+        .fit_car.pdp div:first-child {
+            margin-right: 14px;
+        }
+
+        .fit_car.pdp img {
+            height: 36px;
+            width: 36px;
+        }
+
+        .fit_car.pdp p {
+            font-size: 14px;
+            line-height: 17px;
+        }
+
+        .fit_car.pdp span {
+            color: #3E61BC;
+            font-weight: 600;
+            font-size: 12px;
+            cursor: pointer;
+        }
+    </style>
+`
+
+/**
+* Return the Fit Block HTML with or without selected Car model
+* @param {string} carModel - the Model of the selected Car.
+* @param {boolean} [isChangeBtn = true] - Is Change Model Button exist?
+* @param {boolean} [isPreselected = false] - Is Car Model PDP preselected?
+* @param {boolean} [isFitPopup = false] - Is it a Popup?
+* @param {boolean} [isUpsale = false] - Is it a Upsale?
+*/
+const addFitChangePDP = (params) => { // replace vehicle with a Fit block on the PDP
+    let carModel = params.carModel
+    let isChangeBtn = params.hasOwnProperty('isChangeBtn') ? params.isChangeBtn : true;
+    let isPreselected = params.hasOwnProperty('isPreselected') ? params.isPreselected : false;
+    let isFitPopup = params.hasOwnProperty('isFitPopup') ? params.isFitPopup : false;
+    let isUpsale = params.hasOwnProperty('isUpsale') ? params.isUpsale : 'false';
+
+
+    const fitToBlock = /*html*/` 
+    <div class="fit_car${isChangeBtn || isPreselected ? ' pdp' : ''}">
+        <div><img src="${imgFolderUrl}check-circle.svg" alt="check"></div>
+        <div>
+            <p>Guaranteed fit to</p>
+            <p>${carModel} ${isChangeBtn ? `<span>change</span>` : ''}</p>                        
+        </div>
+    </div>
+`
+
+    if (!isFitPopup) {
+        if (!query('.fit_car.pdp')) {
+            query('.fit_select_car') ? query('.fit_select_car').hidden = true : null
+
+            query('.prod_verify_vehicle').insertAdjacentHTML('afterbegin', fitToBlock)
+
+
+            const waitForChangeBtn = setInterval(() => { // open the Popup by clicking on the change button
+                if (query('.fit_car.pdp span')) {
+                    clearInterval(waitForChangeBtn)
+
+                    query('.fit_car.pdp span').addEventListener('click', openPopup)
+                }
+            }, intervalTimeout)
+        }
+    } else {
+        query('.po_prod').insertAdjacentHTML('afterbegin', fitToBlock)
+    }
+
+    isUpsale ? localStorage.setItem('upsale', 'true') : localStorage.setItem('upsale', 'false')
+}
+
+const drawPdpFit = () => { // add a Fit block to the PDP
+    const selectedCarModel = localStorage.getItem('car')
+    const selectedProduct = localStorage.getItem('product')
+
+    if ( // if car model and product fit each other 
+        query('.prod-title .name').innerText === selectedProduct
+        && query('.js-header-garage-mmy').innerText === selectedCarModel
+    ) {
+        addFitChangePDP({ carModel: selectedCarModel })
+    } else if (query('.prod_vehicle').innerText !== 'Vehicle Specific') { // if car is preselected on the PDP        
+        addFitChangePDP({ carModel: selectedCarModel, isChangeBtn: false, isPreselected: true })
+    } else if (localStorage.getItem('upsale') === 'true' && query('.js-header-garage-mmy').innerText === localStorage.getItem('car')) { // if upsale
+        addFitChangePDP({ carModel: selectedCarModel, isChangeBtn: false, isPreselected: true, isUpsale: 'true' })
+    } else {
+        const defaultPdpFit = /*html*/`
+            <div class="fit_select_car">
+                <p>Guaranteed Fitment</p>
+                <p><span>Select your vehicle</span> to check the fitment</p>
+            </div>
+        `
+
+        query('.prod_verify_vehicle').insertAdjacentHTML('afterbegin', defaultPdpFit)
+
+        const waitForDefaultPdpFit = setInterval(() => { // open the Popup by clicking on the 'select'
+            if (query('.fit_select_car span')) {
+                clearInterval(waitForDefaultPdpFit)
+
+                query('.fit_select_car span').addEventListener('click', openPopup)
+            }
+        }, intervalTimeout)
+    }
+}
+
+const openPopup = () => getId('selectBtnReact').click()
+const changePopupHeader = () => { // change a Popup header
+    const waitForHeader = setInterval(() => {
+        if (query('.po_header')) {
+            clearInterval(waitForHeader)
+
+            if (query('.po_header').innerText.includes('select')) {
+                const header = /*html*/`
+                <p class="header">SELECT YOUR VEHICLE</p>
+                <p class="subheader">Get the perfect fit & an accurate price quote</p>
+            `
+
+                query('.po_header').innerHTML = header
+            }
+        }
+    }, intervalTimeout)
+}
+const addFitToPopup = () => {
+    const waitForTitle = setInterval(() => {
+        if (query('.po-header-selected .title') && query('.po_prod')) {
+            clearInterval(waitForTitle)
+
+            const car = query('.po-header-selected .title').innerText
+            const product = query('.prod-title .name').innerText
+
+            console.log('Selected Car is' + car);
+            console.log('Selected Product is' + product);
+
+            localStorage.setItem('car', car)
+            localStorage.setItem('product', product)
+
+            addFitChangePDP({ carModel: car, isChangeBtn: false, isPreselected: false, isFitPopup: true }) // add a Fit block to the Popup    
+            addFitChangePDP({ carModel: car }) // add a Fit block to the PDP 
+        }
+    }, intervalTimeout)
+
+    const waitForAllBtn = setInterval(() => {
+        if (query('.po_notif_msg_a') && query('.fit_car')) {
+            clearInterval(waitForAllBtn)
+
+            query('.po_notif_msg_a').addEventListener('click', () => { // hide or show a Fit block by clicking on 'Click me'
+                if (query('.po_notif_msg_attention_grey_mark')) {
+                    query('.fit_car').hidden = false
+                } else {
+                    query('.fit_car').hidden = true
+                }
+            })
+        }
+    }, intervalTimeout)
+}
+
+const observerCallback = () => {
+    changePopupHeader()
+    addFitToPopup()
+}
+
+const observePopup = () => { // make changes when Popup is opened
+    const target = document.body
+    const config = {
+        childList: true,
+        subtree: true,
+    }
+
+    const disable = false
+
+    let observer = new MutationObserver(mutations => {
+        for (let mutation of mutations) {
+            for (let node of mutation.addedNodes) {
+                if (!(node instanceof HTMLElement)) continue
+
+                if (node.matches('#child_products_tbl') && disable === false) {
+                    observer.disconnect()
+
+                    observerCallback()
+
+                    observer.observe(target, config)
+                }
+
+                if (node.matches('.po')) {
+                    observer.disconnect()
+                    disable = true
+                }
+
+                if (node.matches('.upsale-products-content')) {
+                    query('.upsale-products-content').addEventListener('click', (e) => {
+                        if (e.target.matches('.-upsale')) {
+                            localStorage.setItem('upsale', 'true')
+                        }
+                    })
+                }
+            }
+        }
+    })
+
+    observer.observe(target, config)
+}
+
+/** Implementation */
+
+document.head.insertAdjacentHTML('beforeend', style) // add CSS
+
+drawPdpFit()
+observePopup()
+
+/** GO Events */
+
+window.dataLayer = window.dataLayer || []
+dataLayer.push({
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp: PDP fitment',
+    'eventAction': 'loaded',
+})
+
+let isClarity = setInterval(() => {
+    if (typeof clarity === 'function') {
+        clearInterval(isClarity)
+
+        clarity('set', `pdp_fitment`, 'variant_1')
+    }
+}, 100)
+
