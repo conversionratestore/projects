d382831fca00bc503560b2ad882097f162573995
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-07-26 14:49:44 +0300

    Update search.js

--- onairparking/search.js
+++ onairparking/search.js
@@ -150,6 +150,9 @@ let style = `
         text-decoration-line: underline;
         color: #3582E5;
     }
+    .btns-edit {
+        padding: 10px;
+    }
     .btns-edit button {
         background: #FFFFFF;
         box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.15), 0px 0px 20px rgba(0, 0, 0, 0.1);
