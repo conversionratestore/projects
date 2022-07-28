4b4cfeb6b26658b2e2c5a8d2b3d1ccada7a84610
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2022-07-28 12:40:28 +0300

    Update lamps_popup_coupon_v2.js

--- lamps/lamps_popup_coupon_v2.js
+++ lamps/lamps_popup_coupon_v2.js
@@ -805,11 +805,10 @@ let startFunk = setInterval(() => {
     }
 
     jQuery("body").on("click", `#add-item-to-cart, .category-products .products-grid .item .item-inner .details-area .actions .addtocart`, function () {
-      console.log(`>>CART`, document.querySelector("#cart-panel"))
       setTimeout(() => {
         console.log(`setTimeout for  renderToCart()`)
         renderToCart()
-      }, 4000)
+      }, 2000)
     })
 
     if (document.querySelector('.header-container .header-actions .action-links [data-account-trigger="true"] span').textContent !== "Account") {
