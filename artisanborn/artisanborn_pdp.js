e8806435e5c48b65beecd7303fce74e5182499da
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2022-08-09 17:26:27 +0300

    Update artisanborn_pdp.js

--- artisanborn/artisanborn_pdp.js
+++ artisanborn/artisanborn_pdp.js
@@ -667,8 +667,8 @@ let startFunk = setInterval(() => {
           <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M44 6.996L18.898 32.12L9.57 22.792L12.672 19.69L18.898 25.916L40.898 3.916L44 6.996ZM39.138 18.084C39.424 19.338 39.6 20.658 39.6 22C39.6 31.724 31.724 39.6 22 39.6C12.276 39.6 4.4 31.724 4.4 22C4.4 12.276 12.276 4.4 22 4.4C25.476 4.4 28.688 5.412 31.416 7.15L34.584 3.982C31.02 1.474 26.686 0 22 0C9.856 0 0 9.856 0 22C0 34.144 9.856 44 22 44C34.144 44 44 34.144 44 22C44 19.382 43.516 16.874 42.68 14.542L39.138 18.084Z" fill="#11891D"/>
           </svg>
-          <p>Thank you for sending the request!</p>
-          <p>We will contact you within 24 hours</p>
+          <p>Thank you for your request!</p>
+          <p>We’ll contact you within 24 hours</p>
           <span class="ok_btn">Ok</span>
         </div>
     </div>
