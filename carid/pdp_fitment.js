ca427edd8654bf1e90f176c202da83ff17471204
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2022-08-19 15:04:00 +0300

    a

--- carid/pdp_fitment.js
+++ carid/pdp_fitment.js
@@ -300,11 +300,6 @@ const changeCar = (isDefault) => {
 
             query('#child_products_tbl .fit_car')?.remove()
             query('.-po-change-vehicle').click()
-            
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth"
-            });
         }
     }, intervalTimeout)
 
