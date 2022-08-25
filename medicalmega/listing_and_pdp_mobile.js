23bb8f11320de26ff9706422ecfb369a53242ae5
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-08-25 11:15:33 +0300

    commit

--- medicalmega/listing_and_pdp_mobile.js
+++ medicalmega/listing_and_pdp_mobile.js
@@ -893,12 +893,13 @@ margin: 16px -20px; }
     height: 89.33vw;
     margin: 40px auto 8px;}
   #tns1 > .tns-item {
-    display: flex;
     height: 100%;
     border: 1px solid #E0E4E5;
     border-radius: 4px;}
   #tns1 > .tns-item img {
-    margin: auto;}
+    height: 100%;
+    width: 100%;
+    object-fit: contain;}
   .list {
     color: #344D57;
     margin: 70px 0 48px; }
