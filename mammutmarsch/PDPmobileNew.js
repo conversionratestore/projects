45228be133f6dd204b696dd269e0d960f1a15712
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-09-02 16:09:59 +0300

    commit

--- mammutmarsch/PDPmobileNew.js
+++ mammutmarsch/PDPmobileNew.js
@@ -417,3 +417,13 @@ dataLayer.push({
     'eventCategory': 'Exp: PDP improvements',
     'eventAction': 'loaded'
 });
+
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1191175,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+hj('event', 'PDP_3events');
