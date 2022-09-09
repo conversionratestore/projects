46bfeda783675e42f551302b03a8e2f6e8e2d451
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-09-09 12:39:22 +0300

    Update tos_checkbox.js

--- carid/tos_checkbox.js
+++ carid/tos_checkbox.js
@@ -4,3 +4,17 @@ let interval = setInterval(() => {
         APP.abTermsCheckboxVerification()
     }
 })
+
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+    'event': 'event-to-ga',
+    'eventCategory': 'Exp — Accept TOS checkbox',
+    'eventAction': 'loaded'
+});
+
+let isClarify = setInterval(() => {
+    if(typeof clarity == 'function') {
+        clearInterval(isClarify)
+        clarity("set", "tos_checkbox", "variant_1");
+    }
+}, 100)
