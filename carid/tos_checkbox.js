819ded9c39f028df987b757874a94de3eaa3a4e4
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-09-13 11:09:45 +0300

    commit

--- carid/tos_checkbox.js
+++ carid/tos_checkbox.js
@@ -115,8 +115,8 @@ let interval = setInterval(() => {
         APP.abTermsCheckboxVerification()
         StartStop()
     }
-    if (viewed == true && !window.location.href.includes('/cart.php?mode=checkout') && sessionStorage.getItem('tos_checkbox') != null && (sessionStorage.getItem('tos_checkbox') != '00:00:00' || sessionStorage.getItem('tos_checkbox') != '')) {
-        viewed = false
+    if (viewed == false && !window.location.href.includes('/cart.php?mode=checkout') && sessionStorage.getItem('tos_checkbox') != null && (sessionStorage.getItem('tos_checkbox') != '00:00:00' || sessionStorage.getItem('tos_checkbox') != '')) {
+        viewed = true
         window.dataLayer = window.dataLayer || [];
         dataLayer.push({
             'event': 'event-to-ga',
