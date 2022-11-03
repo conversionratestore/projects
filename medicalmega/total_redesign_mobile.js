824d41ae355edbe18ea70912873ec2442c729b03
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-11-03 12:15:08 +0200

    update

--- medicalmega/total_redesign_mobile.js
+++ medicalmega/total_redesign_mobile.js
@@ -1399,7 +1399,7 @@ window.onload = function() {
                     document.querySelector(typeState).innerHTML = statesUsa;
                     document.querySelector(typeState).previousElementSibling.children[0].innerHTML = 'State (Only applicable to US)'
                 } else {
-                    document.querySelector(typeState).innerHTML = '<option value="" selected="selected">-- Select State --</option>';
+                    document.querySelector(typeState).innerHTML = statesUsa;
                     document.querySelector(typeState).previousElementSibling.children[0].innerHTML = 'State (Only applicable to US)'
                 }
             })
