a02284408b4a62fe0a9c992d6889b59a2e74a80a
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-11-07 12:30:03 +0200

    commit

--- uplead/newLeadsForm.js
+++ uplead/newLeadsForm.js
@@ -140,6 +140,7 @@ let runTest = () => {
             //change text on button
             document.querySelector('section.elementor-element.elementor-element-954ec92 .elementor-container.elementor-column-gap-no .elementor-form .elementor-button .elementor-button-text').innerHTML = 'Find Leads Now';
             ladedTest()
+            document.querySelector(".exp-loading")?.remove()
         }
         /* end main page */
         
@@ -149,10 +150,11 @@ let runTest = () => {
             
             if (document.readyState == 'complete') {
                 clearInterval(start)
-                // setTimeout(() => {
-                //     window.location.href = 'https://app.uplead.com/trial-signup'
-                // }, 3000)
+                setTimeout(() => {
+                    window.location.href = 'https://app.uplead.com/trial-signup'
+                }, 3000)
                 ladedTest()
+                document.querySelector(".exp-loading")?.remove()
             }
                 
             document.addEventListener('click', (e) => {
@@ -375,6 +377,7 @@ let runTest = () => {
             })
 
             ladedTest()
+            document.querySelector(".exp-loading")?.remove()
         }
         /* end pop-up form */
     })
