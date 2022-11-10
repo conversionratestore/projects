e84151203193ac642e89e50313eff06f6b0feab4
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-11-10 13:46:01 +0200

    update placeholder

--- medicalmega/total_redesign_mobile.js
+++ medicalmega/total_redesign_mobile.js
@@ -897,6 +897,19 @@ window.onload = function() {
                 color: rgba(154, 166, 171, 0.8);
                 font-size: 16px;
                 line-height: 150%;}
+
+            .wrapper-checkout input[type="password"]::-webkit-input-placeholder {
+                // line-height: 1;
+                font-size: 18px;}
+            .wrapper-checkout input[type="password"]:-moz-placeholder {
+                // line-height: 1;
+                font-size: 18px;}
+            .wrapper-checkout input[type="password"]::-moz-placeholder {
+                // line-height: 1;
+                font-size: 18px;}
+            .wrapper-checkout input[type="password"]:-ms-input-placeholder {
+                // line-height: 1;
+                font-size: 18px;}
             .btn-back, .btn-guest {
                 font-weight: 700;
                 font-size: 14px;
@@ -2440,18 +2453,6 @@ window.onload = function() {
             color: rgba(154, 166, 171, 0.8);  
             font-size: 12px;
             line-height: 20px;}
-        [type="password"]::-webkit-input-placeholder {
-            // line-height: 1;
-            font-size: 15px;}
-        [type="password"]:-moz-placeholder {
-            // line-height: 1;
-            font-size: 15px;}
-        [type="password"]::-moz-placeholder {
-            // line-height: 1;
-            font-size: 15px;}
-        [type="password"]:-ms-input-placeholder {
-            // line-height: 1;
-            font-size: 15px;}
         @-webkit-keyframes autofill {
             to {
                 color: #091114;
