f8575ff64b61ec7b1f893fafae136273945c33c6
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-11-15 12:48:23 +0200

    fix

--- medicalmega/total_redesign_desktop.js
+++ medicalmega/total_redesign_desktop.js
@@ -679,6 +679,7 @@ window.onload = function() {
                         document.querySelector(element).style = '';
                         document.querySelector('.footer-cart .btn-next').addEventListener('click', (e) => {
                             e.stopImmediatePropagation();
+                            sessionStorage.setItem('routing', 1);
                             pushDataLayer('Click on Proceed to checkout button', labelForEvents(e.target))
                         })
                     }
@@ -889,6 +890,8 @@ window.onload = function() {
 
     if ((href.includes('login') || href.includes('/register.php') || href.includes('/checkout') || href.includes('/guest-checkout')) && !href.includes('/checkout/step4') && !href.includes('/guest-checkout4.php')) {
         //checkout
+        sessionStorage.setItem('routing', 0);
+
         let statesCanada = '<option value="" selected="selected">-- Select Province --</option><option value="AB">Alberta</option><option value="BC">British Columbia</option><option value="MB">Manitoba</option><option value="NB">New Brunswick</option><option value="NL">Newfoundland</option><option value="NS">Nova Scotia</option><option value="ON">Ontario</option><option value="PE">Prince Edward Island</option><option value="QC">Quebec</option><option value="SK">Saskatchewan</option><option value="YT">Yukon</option><option value="NT">Northwest Territories</option><option value="NV">Nunavut</option>';
         let statesUsa = '<option value="">-- Select State --</option><option value="AL">ALABAMA</option><option value="AK">ALASKA</option><option value="AS">AMERICAN SAMOA</option><option value="AZ">ARIZONA</option><option value="AR">ARKANSAS</option><option value="CA">CALIFORNIA</option><option value="CO">COLORADO</option><option value="CT">CONNECTICUT</option><option value="DE">DELAWARE</option><option value="DC">DISTRICT OF COLUMBIA</option><option value="FM">FEDERATED STATES OF MICRONESIA</option><option value="FL">FLORIDA</option><option value="GA">GEORGIA</option><option value="GU">GUAM</option><option value="HI">HAWAII</option><option value="ID">IDAHO</option><option value="IL">ILLINOIS</option><option value="IN">INDIANA</option><option value="IA">IOWA</option><option value="KS">KANSAS</option><option value="KY">KENTUCKY</option><option value="LA">LOUISIANA</option><option value="ME">MAINE</option><option value="MH">MARSHALL ISLANDS</option><option value="MD">MARYLAND</option><option value="MA">MASSACHUSETTS</option><option value="MI">MICHIGAN</option><option value="MN">MINNESOTA</option><option value="MS">MISSISSIPPI</option><option value="MO">MISSOURI</option><option value="MT">MONTANA</option><option value="NE">NEBRASKA</option><option value="NV">NEVADA</option><option value="NH">NEW HAMPSHIRE</option><option value="NJ">NEW JERSEY</option><option value="NM">NEW MEXICO</option><option value="NY">NEW YORK</option><option value="NC">NORTH CAROLINA</option><option value="ND">NORTH DAKOTA</option><option value="MP">NORTHERN MARIANA ISLANDS</option><option value="OH">OHIO</option><option value="OK">OKLAHOMA</option><option value="OR">OREGON</option><option value="PW">PALAU</option><option value="PA">PENNSYLVANIA</option><option value="PR">PUERTO RICO</option><option value="RI">RHODE ISLAND</option><option value="SC">SOUTH CAROLINA</option><option value="SD">SOUTH DAKOTA</option><option value="TN">TENNESSEE</option><option value="TX">TEXAS</option><option value="UT">UTAH</option><option value="VT">VERMONT</option><option value="VI">VIRGIN ISLANDS</option><option value="VA">VIRGINIA</option><option value="WA">WASHINGTON</option><option value="WV">WEST VIRGINIA</option><option value="WI">WISCONSIN</option><option value="WY">WYOMING</option>'
 
@@ -1807,6 +1810,7 @@ window.onload = function() {
         }
         
         if ((href.includes('/checkout/step1') || href.includes('/guest-checkout1.php')) && document.querySelector('.myAccount') == null) {
+            sessionStorage.setItem('routing', 0);
             document.querySelector('.col-left .head h4').innerHTML = obj['stepsName'][1];
             state_item = href.includes('guest-checkout1.php') ? b_state : state;
             countries_ship_item = href.includes('guest-checkout1.php') ? s_country.innerHTML : countries_ship;
@@ -1979,6 +1983,7 @@ window.onload = function() {
         if (href.includes('/checkout/step3') ) {
             document.querySelector('#checkoutForm h3').innerHTML = `Card Details <img src="${dir}payment-cards.svg" alt="icons">`
             document.querySelector('#cc_block > dl > div.ccInfo > dd:nth-child(3)').innerHTML = `Credit/Debit Card<span class="c-red-08"> *</span>`;
+            document.querySelector('#save_cc_info').insertAdjacentHTML('afterend','<span class="check2"></span>')
         }
         //set text for back button
         let setBack = () => {
@@ -5182,3 +5187,10 @@ window.onload = function() {
         mut.observe(document, optionMut);
     }
 };
+
+let intervalCart = setInterval(() => {
+    if (sessionStorage.getItem('routing') == 1 && href.includes('/cart.html')) {
+        clearInterval(intervalCart);
+        window.location.href = `https://medicalmega.com/checkout/step1`;
+    }
+})
