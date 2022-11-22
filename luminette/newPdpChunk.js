ce017fcdf3eb185dc478e7dcb322fa95bb805f37
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-11-22 11:49:47 +0200

    Update newPdpChunk.js

--- luminette/newPdpChunk.js
+++ luminette/newPdpChunk.js
@@ -47,8 +47,8 @@ const html = `
               </div>
             </li>
             <li class="col-xl-4 d-xl-block d-flex flex-row-reverse justify-content-center">
-              <div class="d-flex align-items-center days-test_head">
-                <p class="fs-22 fw-bold mr-2">100% moneyback guarantee </p>
+              <div class="d-flex align-items-md-center align-items-end days-test_head">
+                <div class="fs-22 fw-bold mr-2">100% money<div class="d-md-inline d-block">back guarantee</div> </div>
                 <div class="tooltip d-flex">
                   <img src="${dirImages}info.svg" alt="info"/>
                   <div class="tooltip-dropdown fs-16">
@@ -78,24 +78,24 @@ const html = `
     <div class='container-fluid container--size--lg lav-jumb__container'>
       <div class="row fs-16 text-center compare mx-auto">
         <div class="col-6 py-5">
-          <img class="mb-2 mt-1" src="${dirImages}glasses.svg" alt="glasses icon"/>
+          <img class="my-1" src="${dirImages}glasses.svg" alt="glasses icon"/>
           <p class="fs-22 fw-bold py-1">Luminette3</p>
         </div>
         <div class="col-6 py-5">
-          <img class="mb-2 mt-1" src="${dirImages}box.svg" alt="box icon"/>
+          <img class="my-1" src="${dirImages}box.svg" alt="box icon"/>
           <p class="fs-22 fw-bold py-1">Light Box</p>
         </div>
-        <div class="col-12 py-2 fs-18 fw-bold">Portable</div>
-        <div class="col-6 py-5 fw-medium"><p class="py-1">Wear while on-the-go</p></div>
-        <div class="col-6 py-5 fw-medium"><p class="py-1">Requires you to stop and sit still for 30 mins</p></div>
-        <div class="col-12 py-2 fs-18 fw-bold">Light colour spectrum</div>
-        <div class="col-6 py-5 fw-medium"><p class="py-1">Blue-enriched white light</p></div>
-        <div class="col-6 py-5 fw-medium"><p class="py-1">Full spectrum white light</p></div>
-        <div class="col-12 py-2 fs-18 fw-bold">Therapy effectiveness</div>
+        <div class="col-12 py-2 fs-18 fw-bold b-1">Portable</div>
+        <div class="col-6 py-5 fw-medium"><p class="py-1">Wear while <span class="text-ellipsis"> on-the-go</span></p></div>
+        <div class="col-6 py-5 fw-medium"><p class="py-1">Requires you to stop and sit still for <span class="text-ellipsis">30 mins</span></p></div>
+        <div class="col-12 py-2 fs-18 fw-bold b-1">Light colour spectrum</div>
+        <div class="col-6 py-5 fw-medium"><p class="py-1">Blue-enriched <span class="text-ellipsis">white light</span></p></div>
+        <div class="col-6 py-5 fw-medium"><p class="py-1">Full spectrum <span class="text-ellipsis">white light</span></p></div>
+        <div class="col-12 py-2 fs-18 fw-bold b-1">Therapy effectiveness</div>
         <div class="col-6 py-5 fw-medium"><p class="py-1">Provides effective therapy using <br>
             a less intense light source</p></div>
         <div class="col-6 py-5 fw-medium"><p class="py-1">Requires at least 10,000 lux to provide effective light therapy</p></div>
-        <div class="col-12 py-2 fs-18 fw-bold">Size</div>
+        <div class="col-12 py-2 fs-18 fw-bold b-1">Size</div>
         <div class="col-6 py-5 fw-medium"><p class="pt-1 pb-5">Small/ ultralight <br>
             only 51g (1.83 ounces)</p></div>
         <div class="col-6 py-5 fw-medium"><p class="pt-1 pb-5">	Large/ bulky</p></div>
@@ -109,40 +109,40 @@ const html = `
     <div class='container-fluid container--size--lg lav-jumb__container'>
       <div class="technical">
         <div class="row technical-block">
-          <div class="col-6 pl-0">
+          <div class="col-lg-6 pl-0">
             <h2>Technical <br>
             Details</h2>
           </div>
-          <div class="col-6 fs-18">
+          <div class="col-lg-6 fs-18">
             <div class="row pb-2 bb-1">
-              <div class="col-7 pl-0">Product Dimensions</div>
-              <div class="col-5 fw-semi pr-0">3"D x 9.06"W x6.26"H</div>
+              <div class="col-sm-7 col-6 pl-0">Product Dimensions</div>
+              <div class="col-sm-5 col-6 fw-semi pr-0 pl-1">3"D x 9.06"W x6.26"H</div>
             </div>
             <div class="row py-2 bb-1">
-              <div class="col-7 pl-0">Light Source Type</div>
-              <div class="col-5 fw-semi pr-0">LED</div>
+              <div class="col-sm-7 col-6 pl-0">Light Source Type</div>
+              <div class="col-sm-5 col-6 fw-semi pr-0 pl-1">LED</div>
             </div>
             <div class="row py-2 bb-1">
-              <div class="col-7 pl-0">Number of Light Sources</div>
-              <div class="col-5 fw-semi pr-0">3</div>
+              <div class="col-sm-7 col-6 pl-0">Number of Light Sources</div>
+              <div class="col-sm-5 col-6 fw-semi pr-0 pl-1">3</div>
             </div>
             <div class="row py-2 bb-1">
-              <div class="col-7 pl-0">Item Weight</div>
-              <div class="col-5 fw-semi pr-0">1.83 ounces (51g)</div>
+              <div class="col-sm-7 col-6 pl-0">Item Weight</div>
+              <div class="col-sm-5 col-6 fw-semi pr-0 pl-1">1.83 ounces (51g)</div>
             </div>
           </div>
         </div>
         <div class="safety-block">
           <div class="row">
-            <div class="col-6 pl-0">
+            <div class="col-lg-6 pl-0">
               <h2 class="pb-4">Safety</h2>
               <div class="d-flex mt-5">
-                <img class="mr-5" src="${dirImages}safety-1.svg" alt="safety icon" />
-                <img class="mr-5 ml-2" src="${dirImages}safety-2.svg" alt="safety icon" />
-                <img class="ml-2" src="${dirImages}safety-3.svg" alt="safety icon" />
+                <img class="mr-5 mb-3" src="${dirImages}safety-1.svg" alt="safety icon" />
+                <img class="mr-5 ml-2 mb-3" src="${dirImages}safety-2.svg" alt="safety icon" />
+                <img class="ml-2 mb-3" src="${dirImages}safety-3.svg" alt="safety icon" />
               </div>
             </div>
-            <div class="col-6 fs-18 pl-0">
+            <div class="col-lg-6 fs-18 px-0">
               <p class="fw-bold mb-3">Since 2007 Luminette®  carries the European 
               CE classification. </p>
               <p class="mb-3">Luminette® is classified as a device that is safe for the eyes in accordance with the international standard CEI 62471. In addition, the light emitted by Luminette®  is free from UV, infrared rays and does not contain short wavelength below 450 nm</p>
@@ -186,6 +186,9 @@ const styles = `
   }
   .bb-1 {
     border-bottom: 1px solid #517193;
+  }
+  .b-1 {
+    border: 1px solid #EFF2F5;
   }
   .c-blue {
     color: #517193;
@@ -287,7 +290,6 @@ const styles = `
   /* Section 2*/
   .compare-section {
     background: url(${dirImages}stripes_features.png) no-repeat center bottom / cover;
-    padding: 80px 0;
   }
   .compare {
     background: #F8F9FA;
@@ -306,15 +308,24 @@ const styles = `
     box-shadow: 0px 44px 66px rgba(59, 83, 108, 0.25);
     background: #FFFFFF;
   }
+  .technical h2 {
+    line-height: 54px;
+  }
   .safety-block {
     background: linear-gradient(101.51deg, #F8F9FA 0%, #FDFDFD 100%, #FDFDFD 100%);
   }
   .technical-block, .safety-block .row {
-    padding: 60px 0;
-    max-width: 960px;
+    padding: 60px 16px;
+    max-width: 992px;
     margin: 0 auto;
   }
   /* media */
+  @media (min-width: 768px) {
+    /* Section 2*/
+    .compare-section {
+      padding: 80px 0;
+    }
+  }
   @media (max-width: 1319px) {
     /* Section 1*/
     .days-test:before, .days-test:after {
@@ -326,28 +337,8 @@ const styles = `
     .days-test:before, .days-test:after {
       width: 36px;
     }
-    .circle {
-      margin: 7px 16px;
-      width: 12px;
-      height: 12px;
-    }
     .days-test_date, .days-test_head  {
       width: calc(50% - 28px);
-    }
-    .days-test_head, .days-test_head .fs-22 {
-      font-size: 18px;
-      line-height: 24px;
-    }
-    .days-test_date .fs-18 {
-      font-size: 16px;
-      line-height: 24px;
-    }
-    .days-test_date .fs-16 {
-      font-size: 14px;
-      line-height: 20px;
-    }
-    .days-test_head .fs-22.mr-2 {
-      margin-right: 4px;
     }
     .days-test_date {
       text-align: right;
@@ -369,9 +360,28 @@ const styles = `
     .days-test_list li:last-child .circle {
       background: #5D9EFF;
     }
+    .circle {
+      margin: 0 16px;
+    }
+  }
+
+  @media (max-width: 991px) {
+    /* Section 3*/
+    .technical-block h2 {
+      margin-bottom: 25px;
+    }
+    .safety-block h2 {
+      margin-bottom: 0;
+      padding-bottom: 0;
+    }
+  }
+  @media (max-width: 767px) {
+    .days-test:before, .days-test:after {
+      content: none;
+    }
     .tooltip > img {
       width: 14px;
-      height: 14px;
+      height: 22px;
     }
     .days-test h3 {
       font-size: 22px;
@@ -382,6 +392,71 @@ const styles = `
     }
     .line-dashed {
       margin-top: 16px;
+    }
+    .fs-22 {
+      font-size: 18px;
+      line-height: 24px;
+    }
+    .compare .fs-18, .days-test .fs-18, .technical-block .fs-18 {
+      font-size: 16px;
+      line-height: 24px;
+    }
+    .days-test_date .fs-16 {
+      font-size: 14px;
+      line-height: 20px;
+    }
+    .days-test_head .fs-22.mr-2 {
+      margin-right: 4px;
+    }
+    .circle {
+      margin: 7px 16px;
+      width: 12px;
+      height: 12px;
+    }
+    .tooltip {
+      position: initial;
+    }
+    .days-test_date {
+      width: calc(44% - 22px);
+    }
+    .days-test_head {
+      width: calc(56% - 22px);
+    }
+    .line-dashed:before {
+      left: calc(44% + 1px);
+    }
+    .days-test_list li {
+      position: relative;
+    }
+    .tooltip-dropdown {
+      right: 0;
+      width: 100%;
+    }
+    .tooltip-item:before {
+      right: auto;
+      left: calc(50% + 63px);
+    }
+    .days-test_list li:not(:nth-child(2)) .tooltip-dropdown {
+      top: calc(100% - 23px);
+    }
+    /* Section 2*/
+    .compare img {
+      width: 60px;
+      height: 60px;
+    }
+    /* Section 3*/
+    .safety-block img {
+      width: 64px;
+      height: 64px;
+      margin-right: 12px;
+      margin-left: 0;
+    }
+    .technical-block, .safety-block .row {
+      padding: 16px;
+    }
+    .technical h2 {
+      font-size: 32px;
+      line-height: 36px;
     }
   }
 `;
