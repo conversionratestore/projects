f8adc551ad99064cdec86c176063b7bf12e6895e
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-11-24 14:26:32 +0200

    update

--- signaturely/free.js
+++ signaturely/free.js
@@ -23,10 +23,11 @@ let modalLimitHTML = `
             </div>
         </div>
     </div>
-</div>`
+</div>`;
 
 //push DataLayer
 function pushDataLayer(action, label) {
+    console.log(action + " : " + label)
     window.dataLayer = window.dataLayer || [];
     dataLayer.push({
         'event': 'event-to-ga',
@@ -36,133 +37,145 @@ function pushDataLayer(action, label) {
     });
 }
 
-// change text on pages
-let changeText = setInterval(() => {
-    if (href.includes('/online-signature/draw') || href.includes('/online-signature/type')) {
-        if (document.querySelector('.elementor-heading-title') != null) {
-            clearInterval(changeText)
-            document.querySelector('.elementor-heading-title').innerHTML = 'Sign 1 document for free!';
-        }
-    }
+let stateUrl = href, 
+    url = href;
 
-    if (document.querySelectorAll('.elementor-container section > div.elementor-container.elementor-column-gap-default > div > div.elementor-column.elementor-col-50.elementor-top-column.elementor-element > div > div > div.elementor-element.elementor-widget.elementor-widget-heading > div > h2')) {
-        let h2Arr = document.querySelectorAll('.elementor-container section > div.elementor-container.elementor-column-gap-default > div > div.elementor-column.elementor-col-50.elementor-top-column.elementor-element > div > div > div.elementor-element.elementor-widget.elementor-widget-heading > div > h2');
-       
-        h2Arr.forEach(item => {
-            if (item.innerHTML == 'Get up to 3 documents signed for free, every month') {
-                clearInterval(changeText)
-                item.innerHTML = 'Sign 1 document for free!';
-            }
-        })
-    }
-
-    if (href.includes('/pricing') && document.querySelectorAll('section .popular-pricing') && document.querySelectorAll('.elementor-widget-wrap > .elementor-widget .elementor-accordion-item')) {
-        clearInterval(changeText)
-        document.querySelectorAll('section .popular-pricing').forEach(item => {
-            item.querySelectorAll('.elementor-text-editor')[1].innerHTML = 'Get your document signed today. It’s free!';
-            item.querySelector('ul.elementor-icon-list-items > li > .elementor-icon-list-text').innerHTML = 'Signature requests: 1';
-        })
-
-        document.querySelectorAll('.elementor-widget-wrap > .elementor-widget .elementor-accordion-item').forEach(item => {
-            let titleTab = item.querySelector('.elementor-accordion-title'),
-                contentTab = item.querySelector('.elementor-tab-content');
-            if (titleTab.innerHTML.includes('have 3 documents free')) {
-                titleTab.innerHTML = titleTab.innerHTML.replace('have 3 documents free','have only 1 document free')
-            }
-            if (contentTab.innerHTML.includes('request 3 documents for e-signature each month 100% free.')) {
-                contentTab.innerHTML = contentTab.innerHTML.replace('3 documents for e-signature each month','1 document for e-signature')
-            }
-            if (contentTab.innerHTML.includes('up to 3 documents for e-signature each month')) {
-                contentTab.innerHTML = contentTab.innerHTML.replace('up to 3 documents for e-signature each month','1 document for e-signature per account')
-            }
-        })
+let routing = setInterval(() => {
+    url = window.location.href;
+    if (stateUrl != url) {
+        stateUrl = url;
+        init();
     }
 })
 
-//change text in modal
-function initChangeTextInModal() {
-    let changeTextInModal = setInterval(() => {
-        if (document.querySelectorAll('.chakra-modal__body .chakra-text') && document.querySelectorAll('.chakra-modal__body h3') && document.querySelector('.chakra-modal__body') != null) {
-            clearInterval(changeTextInModal)
-            document.querySelectorAll('.chakra-modal__body h3').forEach(item => {
-                if (item.innerHTML == 'Signaturely (eSign Documents)') item.innerHTML = 'Need to sign a document?';
-            })
+init();
+
+function init() {
+    // change text on pages
+    let changeText = setInterval(() => {
+        //title form
+        if (href.includes('/online-signature/draw') || href.includes('/online-signature/type')) {
+            if (document.querySelector('.elementor-heading-title') != null) {
+                clearInterval(changeText)
+                document.querySelector('.elementor-heading-title').innerHTML = 'Sign 1 document for free!';
+            }
+        }
+        //title form
+        if (document.querySelectorAll('.elementor-container section > div.elementor-container.elementor-column-gap-default > div > div.elementor-column.elementor-col-50.elementor-top-column.elementor-element > div > div > div.elementor-element.elementor-widget.elementor-widget-heading > div > h2')) {
+            let h2Arr = document.querySelectorAll('.elementor-container section > div.elementor-container.elementor-column-gap-default > div > div.elementor-column.elementor-col-50.elementor-top-column.elementor-element > div > div > div.elementor-element.elementor-widget.elementor-widget-heading > div > h2');
         
-            document.querySelectorAll('.chakra-modal__body .chakra-text').forEach(item => {
-                if (item.innerHTML == 'Sign 3 documents for free using Signaturely!') item.innerHTML = 'Sign 1 document for free!';
+            h2Arr.forEach(item => {
+                if (item.innerHTML == 'Get up to 3 documents signed for free, every month') {
+                    clearInterval(changeText)
+                    item.innerHTML = 'Sign 1 document for free!';
+                }
             })
-            document.querySelector('.chakra-modal__close-btn').addEventListener('click', () => {
-                setTimeout(() => {
-                    initChangeTextInModal()
-                }, 1000)
+        }
+        //pricing
+        if (href.includes('/pricing') && document.querySelectorAll('section .popular-pricing') && document.querySelectorAll('.elementor-widget-wrap > .elementor-widget .elementor-accordion-item')) {
+            clearInterval(changeText)
+            document.querySelectorAll('section .popular-pricing').forEach(item => {
+                item.querySelectorAll('.elementor-text-editor')[1].innerHTML = 'Get your document signed today. It’s free!';
+                item.querySelector('ul.elementor-icon-list-items > li > .elementor-icon-list-text').innerHTML = 'Signature requests: 1';
             })
-        } 
+
+            document.querySelectorAll('.elementor-widget-wrap > .elementor-widget .elementor-accordion-item').forEach(item => {
+                let titleTab = item.querySelector('.elementor-accordion-title'),
+                    contentTab = item.querySelector('.elementor-tab-content');
+                if (titleTab.innerHTML.includes('have 3 documents free')) {
+                    titleTab.innerHTML = titleTab.innerHTML.replace('have 3 documents free','have only 1 document free')
+                }
+                if (contentTab.innerHTML.includes('request 3 documents for e-signature each month 100% free.')) {
+                    contentTab.innerHTML = contentTab.innerHTML.replace('3 documents for e-signature each month','1 document for e-signature')
+                }
+                if (contentTab.innerHTML.includes('up to 3 documents for e-signature each month')) {
+                    contentTab.innerHTML = contentTab.innerHTML.replace('up to 3 documents for e-signature each month','1 document for e-signature per account')
+                }
+            })
+        }
+        //billing plan
+        if (document.querySelector('.billing__table-row') != null && document.querySelector('.header__month-counter') != null && document.querySelector('.header__month-counter').innerHTML.includes('of 1 signature requests')) {
+            clearInterval(changeText)
+            document.querySelectorAll('.billing__table-row').forEach(el => {
+                if (el.querySelector('.billing__table-column--name').innerHTML == 'Documents per month') {
+                    el.querySelector('.billing__table-column--name').innerHTML = 'Signature requests';
+                    el.querySelector('.billing__table-column--name').nextElementSibling.innerHTML = '1';
+                }
+            })
+            document.querySelector('.billing__table--description').innerHTML = 'Sign 1 document for free!';
+        }
+    })
+
+    //change text in modal
+    function initChangeTextInModal() {
+        let changeTextInModal = setInterval(() => {
+            if (document.querySelectorAll('.chakra-modal__body .chakra-text') && document.querySelectorAll('.chakra-modal__body h3') && document.querySelector('.chakra-modal__body') != null) {
+                clearInterval(changeTextInModal)
+                document.querySelectorAll('.chakra-modal__body h3').forEach(item => {
+                    if (item.innerHTML == 'Signaturely (eSign Documents)') item.innerHTML = 'Need to sign a document?';
+                })
+            
+                document.querySelectorAll('.chakra-modal__body .chakra-text').forEach(item => {
+                    if (item.innerHTML == 'Sign 3 documents for free using Signaturely!') item.innerHTML = 'Sign 1 document for free!';
+                })
+                document.querySelector('.chakra-modal__close-btn').addEventListener('click', () => {
+                    setTimeout(() => {
+                        initChangeTextInModal()
+                    }, 1000)
+                })
+            } 
+        })
+    }
+
+    initChangeTextInModal() 
+
+    //change header text to caption limit
+    let headerinterval = setInterval(() => {
+        if (document.querySelector('.header__month-counter') != null) {
+            clearInterval(headerinterval)
+            let month = document.querySelector('.header__month-counter');
+            if (month.innerText.includes('0 of 3 signature requests') || month.innerText.includes('1 of 3 signature requests')) {
+                month.innerHTML = month.innerHTML.replace('of 3', 'of 1');
+                month.querySelector('span').remove()
+            }
+
+            if (month.innerText.includes('1 of 1 signature requests')) {
+                sessionStorage.setItem('documents', '1');
+            } else {
+                sessionStorage.setItem('documents', '0');
+            }
+        }
+    })
+
+    //init modal - limit to 1 document for users of the "free" plan 
+    function initModalInterval() {
+        let modalInterval = setInterval(() => {
+            if (document.querySelector('.interactModal__header-send button.button--primary') != null && document.querySelector('.fieldShape') != null && sessionStorage.getItem('documents') == '1') {
+                clearInterval(modalInterval)
+                document.querySelector('.interactModal__header-send button.button--primary').addEventListener('click', (e) => {
+                    e.stopImmediatePropagation();
+                    document.body.insertAdjacentHTML('beforeend', modalLimitHTML)
+                    pushDataLayer('View popup', 'Popup: Please upgrade your Signaturely account!') 
+                })
+            }
+        })  
+    }
+    initModalInterval()
+
+    //close modal
+    let closeModalInterval = setInterval(() => {
+        if (document.querySelector('.modal-limit .modal__close-button') != null) {
+            clearInterval(closeModalInterval)
+            document.addEventListener('click', (e) => {
+                if (document.querySelector('.modal-limit') != null && (!e.target.closest('.upgradeModal') || e.target.classList.contains('modal__close-button') || e.target.closest('.modal__close-button'))) {
+                    document.querySelector('.modal-limit').remove(); //remove modal
+                    pushDataLayer('Closing popup', 'Popup: Please upgrade your Signaturely account!') 
+                    initModalInterval() //init modal
+                }
+            })
+        }
     })
 }
-
-initChangeTextInModal() 
-
-//change text in billing plan
-let billingInterval = setInterval(() => {
-    if (document.querySelector('.billing__table-row') != null) {
-        clearInterval(billingInterval)
-        document.querySelectorAll('.billing__table-row').forEach(el => {
-            if (el.querySelector('.billing__table-column--name').innerHTML == 'Documents per month') {
-                console.log(el)
-                el.querySelector('.billing__table-column--name').innerHTML = 'Signature requests';
-                el.querySelector('.billing__table-column--name').nextElementSibling.innerHTML = '1';
-            }
-        })
-        document.querySelector('.billing__table--description').innerHTML = 'Sign 1 document for free!';
-    }
-})
-
-//change header text to caption limit
-let headerinterval = setInterval(() => {
-    if (document.querySelector('.header__month-counter') != null) {
-        clearInterval(headerinterval)
-        let month = document.querySelector('.header__month-counter');
-        if (month.innerText.includes('0 of 3 signature requests') || month.innerText.includes('1 of 3 signature requests')) {
-            month.innerHTML = month.innerHTML.replace('of 3', 'of 1');
-            month.querySelector('span').remove()
-        }
-
-        if (month.innerText.includes('1 of 1 signature requests')) {
-            sessionStorage.setItem('documents', '1');
-        } else {
-            sessionStorage.setItem('documents', '0');
-        }
-    }
-})
-
-//init modal - limit to 1 document for users of the "free" plan 
-function initModalInterval() {
-    let modalInterval = setInterval(() => {
-        if (document.querySelector('.interactModal__header-send button.button--primary') != null && document.querySelector('.fieldShape') != null && sessionStorage.getItem('documents') == '1') {
-            clearInterval(modalInterval)
-            document.querySelector('.interactModal__header-send button.button--primary').addEventListener('click', (e) => {
-                e.stopImmediatePropagation();
-                document.body.insertAdjacentHTML('beforeend', modalLimitHTML)
-                pushDataLayer('View popup', 'Popup: Please upgrade your Signaturely account!') 
-            })
-        }
-    })  
-}
-initModalInterval()
-
-//close modal
-let closeModalInterval = setInterval(() => {
-    if (document.querySelector('.modal-limit .modal__close-button') != null) {
-        clearInterval(closeModalInterval)
-        document.addEventListener('click', (e) => {
-            if (document.querySelector('.modal-limit') != null && (!e.target.closest('.upgradeModal') || e.target.classList.contains('modal__close-button') || e.target.closest('.modal__close-button'))) {
-                document.querySelector('.modal-limit').remove(); //remove modal
-                pushDataLayer('Closing popup', 'Popup: Please upgrade your Signaturely account!') 
-                initModalInterval() //init modal
-            }
-        })
-    }
-})
 
 let isClarity = setInterval(() => {
 	if(typeof clarity === 'function') {
