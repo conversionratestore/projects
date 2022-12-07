bef5334d6f11dba55182a5822a1d2bfc8df73a1a
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2022-12-07 14:26:11 +0200

    fix

--- signaturely/free.js
+++ signaturely/free.js
@@ -111,7 +111,7 @@ function init() {
                 }
             })
             document.querySelector('.billing__table--description').innerHTML = 'Sign 1 document for free!';
-            document.querySelector('.billing__plans-slider-dot') != null ? document.querySelector('.billing__plans-slider-dot').addEventListener('click', () => init()) : ''
+            document.querySelector('.billing__plans-slider-dots > div') != null ? document.querySelector('billing__plans-slider-dots > div').addEventListener('click', () => init()) : ''
         }
     })
 
