871043993b1eaccab7cdb2a32c93b7cd45fff5a0
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-01-16 17:54:26 +0200

    fix pdp

--- secretfoodtours/tour_page_test.js
+++ secretfoodtours/tour_page_test.js
@@ -637,7 +637,7 @@ let interval = setInterval(() => {
         document.querySelector('#plugin').insertAdjacentHTML('afterend', formHTML);
 
         //set price tour
-        if (document.querySelector('.price') != null && document.querySelectorAll('.price')[document.querySelectorAll('.price').length - 1].innerHTML.trim() != 'Available only for <a href="/private-bookings">private bookings</a>') {
+        if (document.querySelector('.price') != null && document.querySelectorAll('.price')[document.querySelectorAll('.price').length - 1].innerHTML.trim().includes('private bookings')) {
             let price = document.querySelectorAll('.price')[document.querySelectorAll('.price').length - 1];
             document.querySelector('.form_tour .pr').innerHTML = `${price.innerHTML}`
         } else {
