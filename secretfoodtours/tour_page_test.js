8d6794c5441b6186b48bde9f0df1ba0d52511aac
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-01-17 13:18:07 +0200

    update

--- secretfoodtours/tour_page_test.js
+++ secretfoodtours/tour_page_test.js
@@ -745,11 +745,10 @@ let interval = setInterval(() => {
                      console.log(item.target.classList)
                      if(item.target.classList.contains('show')) {
                          document.querySelector('.popup_form_tour').style.bottom = `${document.querySelector('.cookiealert').clientHeight}px`;
-                         document.querySelector('.whatsapp-link').style.bottom = `${document.querySelector('.cookiealert').clientHeight + document.querySelector('.popup_form_tour').clientHeight + 10}px!important`;
-
+                         document.querySelector('.whatsapp-link').style = `bottom: ${document.querySelector('.cookiealert').clientHeight + document.querySelector('.popup_form_tour').clientHeight + 10}px!important`;
                      } else {
                          document.querySelector('.popup_form_tour').style.bottom = '0';
-                         document.querySelector('.whatsapp-link').style.bottom = `${document.querySelector('.popup_form_tour').clientHeight + 10}px!important`;
+                         document.querySelector('.whatsapp-link').style = `bottom: ${document.querySelector('.popup_form_tour').clientHeight + 10}px!important`;
                         }
                     })
                  })
