b984cf479e15168260364f41d38ecc97d54c5a92
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-01-17 15:08:10 +0200

    update

--- secretfoodtours/tour_page.js
+++ secretfoodtours/tour_page.js
@@ -605,7 +605,7 @@ let intervalBook = setInterval(() => {
 
 let interval = setInterval(() => {
 
-    if (document.querySelector('.form_tour') == null && document.querySelector('#plugin') != null && document.querySelector('.what_we_do') != null) {
+    if (document.querySelector('.form_tour') == null && document.querySelector('#plugin') != null) {
         clearInterval(interval)
 
         document.body.insertAdjacentHTML('afterbegin', /* html */`
