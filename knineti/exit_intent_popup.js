5da00b12fda42e00b39a9b7fcbc6862a2bae7f6b
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2023-01-25 18:32:06 +0200

    remove deprecated property

--- knineti/exit_intent_popup.js
+++ knineti/exit_intent_popup.js
@@ -274,8 +274,7 @@ const getMobileOS = () => {
     if (/android/i.test(ua)) {
         return "Android"
     }
-    else if (/iPad|iPhone|iPod/.test(ua)
-        || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) {
+    else if (/iPad|iPhone|iPod/.test(ua)) {
         return "iOS"
     }
     return "Other"
