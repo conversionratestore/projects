b7e8938307f2f9f6825fd6abddcc730a2e7d5c27
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2023-02-09 11:59:30 +0200

    Update sticky_autofill_city.js

--- moneygeek/sticky_autofill_city.js
+++ moneygeek/sticky_autofill_city.js
@@ -358,33 +358,33 @@ form.css-8atqhb .chakra-form__error-message {
           if (document.querySelector("#sub-navigation")) {
             clearInterval(intNav)
             console.log(`for sticky`, document.querySelector("#sub-navigation"))
-            let s = setInterval(() => {
-              if (document.querySelector("#social-section")) {
-                clearInterval(s)
-                setTimeout(() => {
-                  if (window.innerWidth <= 1110) {
-                    document.querySelector("#sub-navigation").insertAdjacentHTML("beforeend", stickyBlock)
-                  } else {
-                    document.querySelector("#sub-navigation").insertAdjacentHTML("afterbegin", stickyBlock)
-                  }
-
-                  if (document.querySelector(".sticky_banner")) {
-                    const options = {
-                      root: null,
-                      threshold: 1,
-                    }
-
-                    let observerNewHeader = new IntersectionObserver((entries) => {
-                      if (!entries[0].isIntersecting) return
-                      pushDataLayer(`Sticky banner appearance`)
-                      observerNewHeader.disconnect()
-                    })
-
-                    observerNewHeader.observe(document.querySelector(".sticky_banner"), options)
-                  }
-                }, 1000)
+            // let s = setInterval(() => {
+            //   if (document.querySelector("#social-section")) {
+            // clearInterval(s)
+            setTimeout(() => {
+              if (window.innerWidth <= 1110) {
+                document.querySelector("#sub-navigation").insertAdjacentHTML("beforeend", stickyBlock)
+              } else {
+                document.querySelector("#sub-navigation").insertAdjacentHTML("afterbegin", stickyBlock)
               }
-            }, 10)
+
+              if (document.querySelector(".sticky_banner")) {
+                const options = {
+                  root: null,
+                  threshold: 1,
+                }
+
+                let observerNewHeader = new IntersectionObserver((entries) => {
+                  if (!entries[0].isIntersecting) return
+                  pushDataLayer(`Sticky banner appearance`)
+                  observerNewHeader.disconnect()
+                })
+
+                observerNewHeader.observe(document.querySelector(".sticky_banner"), options)
+              }
+            }, 1000)
+            //   }
+            // }, 10)
           }
         }, 100)
       }
