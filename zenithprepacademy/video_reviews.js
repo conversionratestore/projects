b8062a14960325f4fde960e5c116a95ec9f71034
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2023-03-30 11:46:56 +0300

    Update video_reviews.js

--- zenithprepacademy/video_reviews.js
+++ zenithprepacademy/video_reviews.js
@@ -1056,6 +1056,7 @@ a.schedule_new_btn {
           clearInterval(intervalVimeo)
           const iframe = document.querySelector(".mobile-iframe")
           const player = new Vimeo.Player(iframe)
+          console.log(player, `player>>>>>>>>>>>>>>>>`)
 
           if (player.setVolume) {
             player.setVolume(1)
