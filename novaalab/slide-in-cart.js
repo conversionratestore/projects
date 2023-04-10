4d4ed4e18da13cf7da883cfbffd44804d175b15f
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-04-10 13:16:29 +0300

    update

--- novaalab/slide-in-cart.js
+++ novaalab/slide-in-cart.js
@@ -227,22 +227,22 @@ input.clac_qty {
     font-size: 16px;
     color: #212121;
 }
-.item_product__delate {
+.item_product__delete {
     border: 1px solid #E2E2E2;
     border-radius: 4px;
     width: 36px;
     height: 36px;
     padding: 0!important;
 }
-.item_product__delate svg {
+.item_product__delete svg {
     flex-shrink: 0;
     width: 12px;
     height: 14px;
 }
-.item_product__delate:hover svg path {
+.item_product__delete:hover svg path {
     fill: #773BD9;
 }
-.item_product__delate:hover {
+.item_product__delete:hover {
     border-color: #773BD9;
 }
 /* message block*/
@@ -617,9 +617,6 @@ function updateCart(id, qty = 0) {
         });
 }
 
-
-let elementPosition = 0;
-
 function getCart(cartDrawer = document.querySelector('.slide_in__cart')) {
     // get cart
     cartDrawer.querySelector('.slide_in__header > p').classList.add('loading')
@@ -808,8 +805,6 @@ function getCart(cartDrawer = document.querySelector('.slide_in__cart')) {
                         }
                     }
                 })
-                
-                cartDrawer.querySelector('.container').scrollTo(0, elementPosition);
 
                 let bundle = false;
                 for (let i = 0; i < items.length; i++) {
@@ -867,7 +862,7 @@ class ProductItem {
                             <input type="number" value="${this.qty}" class="clac_qty">
                             <button type="button" class="calc_action calc_action__plus"></button>
                         </div>
-                        <button type="button" class="item_product__delate d-flex">
+                        <button type="button" class="item_product__delete d-flex">
                             <svg class="m-auto" width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M0.857143 12.6875C0.857143 13.4258 1.41964 14 2.14286 14H9.85714C10.5536 14 11.1429 13.4258 11.1429 12.6875V3.5H0.857143V12.6875ZM8.14286 5.6875C8.14286 5.46875 8.33036 5.25 8.57143 5.25C8.78571 5.25 9 5.46875 9 5.6875V11.8125C9 12.0586 8.78571 12.25 8.57143 12.25C8.33036 12.25 8.14286 12.0586 8.14286 11.8125V5.6875ZM5.57143 5.6875C5.57143 5.46875 5.75893 5.25 6 5.25C6.21429 5.25 6.42857 5.46875 6.42857 5.6875V11.8125C6.42857 12.0586 6.21429 12.25 6 12.25C5.75893 12.25 5.57143 12.0586 5.57143 11.8125V5.6875ZM3 5.6875C3 5.46875 3.1875 5.25 3.42857 5.25C3.64286 5.25 3.85714 5.46875 3.85714 5.6875V11.8125C3.85714 12.0586 3.64286 12.25 3.42857 12.25C3.1875 12.25 3 12.0586 3 11.8125V5.6875ZM11.5714 0.875H8.35714L8.08929 0.382812C7.98214 0.164062 7.76786 0 7.52679 0H4.44643C4.20536 0 3.99107 0.164062 3.88393 0.382812L3.64286 0.875H0.428571C0.1875 0.875 0 1.09375 0 1.3125V2.1875C0 2.43359 0.1875 2.625 0.428571 2.625H11.5714C11.7857 2.625 12 2.43359 12 2.1875V1.3125C12 1.09375 11.7857 0.875 11.5714 0.875Z" fill="#BBBBBB"/>
                             </svg> 
@@ -915,8 +910,7 @@ class ProductItem {
         this.parent.appendChild(element);
        
         if (this.type != 'addToCart') {
-            this.parent.querySelector(`[data-variant-id="${this.variantId}"] .item_product__delate`).addEventListener('click', (e) => {
-                elementPosition = 0;
+            this.parent.querySelector(`[data-variant-id="${this.variantId}"] .item_product__delete`).addEventListener('click', (e) => {
                 updateCart(this.variantId)
                 pushDataLayer('Removal of product from the cart', this.name)
             })
@@ -957,14 +951,14 @@ class ProductItem {
                         addCart(this.variantId, 1, 39737414484022)
                     }
                     pushDataLayer('Add to cart button on the Bundle offer')
-
-                    elementPosition = 0;
+                    this.parent.closest('.container').scrollTo(0, 0);
 
                 } else {
-                    if (e.target.dataset.variantId == '40322897838134' || e.target.dataset.variantId == '39782656311350') {
-                        elementPosition = 0;
+                    if (e.target.closest('.may_like')) { 
+                        let bottom =  this.parent.closest('.container').scrollHeight;
+                        this.parent.closest('.container').scrollTo(0, bottom);
                     } else {
-                        elementPosition = e.target.getBoundingClientRect().top;
+                        this.parent.closest('.container').scrollTo(0, 0);
                     }
 
                     addCart(e.target.dataset.variantId, 1)
