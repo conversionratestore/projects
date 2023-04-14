968b2a0207fe08e6a164c7f96070d054a7a0a3c1
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-04-14 13:45:25 +0300

    fix

--- secretfoodtours/new_pdp.js
+++ secretfoodtours/new_pdp.js
@@ -2213,62 +2213,64 @@ let init = setInterval(() => {
 
                 document.querySelector('.tour-section .review .review-from > div > img').after(document.querySelector('.reviews-first-container'))
 
-            }
+            
 
-            if (customers_reviews) {
-                console.log('Reviews Array:', customers_reviews)
-                document.querySelector('.header_sticky_bottom .menu ul').insertAdjacentHTML('beforeend',`<li><a href="#review" onclick="menuToElement(event)">Reviews</a></li>`)
+                if (customers_reviews) {
+                    console.log('Reviews Array:', customers_reviews)
+                    document.querySelector('.header_sticky_bottom .menu ul').insertAdjacentHTML('beforeend',`<li><a href="#review" onclick="menuToElement(event)">Reviews</a></li>`)
 
-                for (let i = 0; i < customers_reviews.length; i++) {
-                    let slide = document.createElement('div');
-                    slide.classList.add('swiper-slide')
-                    slide.innerHTML = `
-                    <div class="d-flex align-items-center">
-                        <img src="${customers_reviews[i][0]}">
-                        <div>
-                            <p class="author">${customers_reviews[i][1]}</p>
-                            <div class="reviews-user-stars d-flex">
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
-                                </svg>
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
-                                </svg>
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
-                                </svg>
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
-                                </svg>
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
-                                </svg>
-                                                                                                                            
+                    for (let i = 0; i < customers_reviews.length; i++) {
+                        let slide = document.createElement('div');
+                        slide.classList.add('swiper-slide')
+                        slide.innerHTML = `
+                        <div class="d-flex align-items-center">
+                            <img src="${customers_reviews[i][0]}">
+                            <div>
+                                <p class="author">${customers_reviews[i][1]}</p>
+                                <div class="reviews-user-stars d-flex">
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
+                                    </svg>
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
+                                    </svg>
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
+                                    </svg>
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
+                                    </svg>
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.09375 1.5625L6.0625 5.71875L1.46875 6.375C0.65625 6.5 0.34375 7.5 0.9375 8.09375L4.21875 11.3125L3.4375 15.8438C3.3125 16.6562 4.1875 17.2812 4.90625 16.9062L9 14.75L13.0625 16.9062C13.7812 17.2812 14.6562 16.6562 14.5312 15.8438L13.75 11.3125L17.0312 8.09375C17.625 7.5 17.3125 6.5 16.5 6.375L11.9375 5.71875L9.875 1.5625C9.53125 0.84375 8.46875 0.8125 8.09375 1.5625Z" fill="#F4B75B"/>
+                                    </svg>
+                                                                                                                                
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <p class="text">
-                        <b>${customers_reviews[i][2]}</b>
-                        ${customers_reviews[i][3]}
-                    </p> `
-                    if (document.querySelector('.slider-review .swiper-wrapper') != null) {
-                        document.querySelector('.slider-review .swiper-wrapper').appendChild(slide)
+                        <p class="text">
+                            <b>${customers_reviews[i][2]}</b>
+                            ${customers_reviews[i][3]}
+                        </p> `
+                        if (document.querySelector('.slider-review .swiper-wrapper') != null) {
+                            document.querySelector('.slider-review .swiper-wrapper').appendChild(slide)
+                        }
                     }
+                
+                    new Swiper(".slider-review", {
+                        slidesPerView: 3,
+                        loop: true,
+                        spaceBetween: 20,
+                        pagination: {
+                            el: ".slider-review .swiper-pagination",
+                            type: "fraction",
+                        },
+                        navigation: {
+                            nextEl: ".slider-review .swiper-button-next",
+                            prevEl: ".slider-review .swiper-button-prev",
+                        }
+                    }); 
                 }
-            
-                new Swiper(".slider-review", {
-                    slidesPerView: 3,
-                    loop: true,
-                    spaceBetween: 20,
-                    pagination: {
-                        el: ".slider-review .swiper-pagination",
-                        type: "fraction",
-                    },
-                    navigation: {
-                        nextEl: ".slider-review .swiper-button-next",
-                        prevEl: ".slider-review .swiper-button-prev",
-                    }
-                }); 
+
             }
 
             if (typeof loadReviews !== 'function') return false
