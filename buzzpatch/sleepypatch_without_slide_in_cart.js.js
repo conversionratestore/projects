34ec19a6de8150e16b728a40ac86b48005ccdc88
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2023-04-19 14:18:38 +0300

    Update sleepypatch_without_slide_in_cart.js.js

--- buzzpatch/sleepypatch_without_slide_in_cart.js.js
+++ buzzpatch/sleepypatch_without_slide_in_cart.js.js
@@ -65,6 +65,20 @@ if (window.innerWidth <= 768) {
         body.slide{
             overflow-y: visible !important;
         }
+        .new_img_wrap{
+          position: relative;
+        }
+        .prod_free_shipping{
+          position: absolute;
+          z-index: 1;
+          width: 51px!important;
+          height: auto!important;
+          top: -11px;
+          right: 29px;
+          left: unset;
+          transform: rotate(-10deg);
+          border-radius: 50px;
+        }
       </style>
       `;
 
@@ -74,13 +88,10 @@ if (window.innerWidth <= 768) {
       if (!document.querySelector(".package .img_var_pack")) {
         document.querySelector(".package .ptext")?.insertAdjacentHTML(
           "afterend",
-          `<img
-          data-sizes="auto"
-          data-src="https://cdn.shopify.com/s/files/1/0387/0749/4956/files/sleepypatch-free-delivery.png?v=1640695454"
-          data-srcset="https://cdn.shopify.com/s/files/1/0387/0749/4956/files/sleepypatch-free-delivery.png?v=1640695454 350w,                                                                                                                                           https://cdn.shopify.com/s/files/1/0387/0749/4956/files/sleepypatch-free-delivery.png?v=1640695454 900w"
-          width="200"
-          class="js-mobile lazyload img_var_pack"
-        />`
+          `<div class="new_img_wrap">
+            <img data-sizes="auto" src="https://cdn.shopify.com/s/files/1/0387/0749/4956/products/SleepyPatch3Pack.jpg?v=1680784129&width=600" width="200" class="js-mobile lazyload img_var_pack"/>
+          <img src="//cdn.shopify.com/s/files/1/0387/0749/4956/files/Free_Shipping_B.svg?v=5006052283570441635" class="prod_free_shipping" width="64" height="64">
+            </div>`
         );
       }
 
@@ -102,6 +113,35 @@ if (window.innerWidth <= 768) {
           e.preventDefault();
           e.stopPropagation();
           console.log(`click >>>>>>>>>>>>>>>>>>>`);
+        });
+      });
+
+      document.querySelectorAll('[name="radios"]').forEach((i) => {
+        i.addEventListener("click", (e) => {
+          if (e.target.checked) {
+            switch (e.target.value) {
+              case "39836565143596":
+                document.querySelector(".package .img_var_pack").src = "https://cdn.shopify.com/s/files/1/0387/0749/4956/products/SleepyPatch1Pack.jpg?v=1680784129&width=600";
+                console.log(e.target.value, `!!!!!!!!!!!!!!!!!!!checked 1`);
+                break;
+              case "39836565176364":
+                document.querySelector(".package .img_var_pack").src = "https://cdn.shopify.com/s/files/1/0387/0749/4956/products/SleepyPatch2Pack.jpg?v=1680784129&width=600";
+                console.log(e.target.value, `!!!!!!!!!!!!!!!!!!!checked 2`);
+                break;
+              case "39836565110828":
+                document.querySelector(".package .img_var_pack").src = "https://cdn.shopify.com/s/files/1/0387/0749/4956/products/SleepyPatch3Pack.jpg?v=1680784129&width=600";
+                console.log(e.target.value, `!!!!!!!!!!!!!!!!!!!checked 3`);
+                break;
+              case "39836565209132":
+                document.querySelector(".package .img_var_pack").src = "https://cdn.shopify.com/s/files/1/0387/0749/4956/products/SleepyPatch4Pack.jpg?v=1680784129&width=600";
+                console.log(e.target.value, `!!!!!!!!!!!!!!!!!!!checked 4`);
+                break;
+              default:
+                document.querySelector(".package .img_var_pack").src = "https://cdn.shopify.com/s/files/1/0387/0749/4956/products/SleepyPatch3Pack.jpg?v=1680784129&width=600";
+                console.log(e.target.value, `!!!!!!!!!!!!!!!!!!!checked default`);
+                break;
+            }
+          }
         });
       });
 
