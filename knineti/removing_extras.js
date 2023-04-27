569abc216882b65a9cb8a3f21202b3f38cf0334f
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-04-27 13:00:48 +0300

    update

--- knineti/removing_extras.js
+++ knineti/removing_extras.js
@@ -1,13 +1,14 @@
 let removing = setInterval(() => {
     if (window.location.href.includes('//k9ti.org/mc') && document.querySelectorAll('.count_sec .row1030') && document.querySelector('#unlimited_personal_coaching') != null) {
         clearInterval(removing)
+        
         document.querySelectorAll('.count_sec .row1030').forEach(item => {
             if (item.innerHTML.includes('3 exclusive bonus classes')) {
-                item.nextElementSibling.remove()
-                item.remove()
+                item.nextElementSibling.classList.add('hidden')
+                item.classList.add('hidden')
             }
         })
-        document.querySelector('#unlimited_personal_coaching').remove()
+        document.querySelector('#unlimited_personal_coaching').classList.add('hidden')
     }
     if (window.location.href.includes('//k9ti.org/enroll') && document.querySelector('.payment_inform_box .payment_order') != null) {
         clearInterval(removing)
