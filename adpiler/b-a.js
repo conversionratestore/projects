936c0115df621bbed348518d36c8bc194b6339d0
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-05-25 18:40:03 +0300

    update

--- adpiler/b-a.js
+++ adpiler/b-a.js
@@ -1,8 +1,8 @@
 let init = setInterval(() => {
-    if (document.querySelector('.elementor-10 .elementor-element.elementor-element-f9e044b') && document.querySelector('.elementor-nav-menu--main ul li:nth-child(3) > a')) {
+    if (document.querySelector('.elementor-element-83989a8') && document.querySelector('.elementor-nav-menu--main ul li:nth-child(3) > a')) {
         clearInterval(init)
-        document.querySelector('.elementor-10 .elementor-element.elementor-element-f9e044b').remove()
-        document.querySelector('.elementor-10 .elementor-element.elementor-element-868c6f4 .elementor-button').classList.add('skewed')
+        document.querySelector('.elementor-element-83989a8').remove()
+        document.querySelector('.elementor-element-27d482a .elementor-button').classList.add('skewed')
         document.querySelector('.elementor-nav-menu--main ul li:nth-child(3) > a').href = 'https://adpiler.com/schedule-a-demo/'
     }   
 });
