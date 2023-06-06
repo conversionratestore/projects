c0deec32f90b3bd6c7a7e06052fa63ca5ca79bcd
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-06-06 17:47:20 +0300

    update

--- adpiler/b-a.js
+++ adpiler/b-a.js
@@ -5,6 +5,12 @@ let init = setInterval(() => {
         document.querySelector('.elementor-element-83989a8').remove()
         document.querySelector('.elementor-element-27d482a .elementor-button').classList.add('skewed')
     }   
+
+    if (window.location.href == 'https://adpiler.com/social-ad-mockups/' && document.querySelector('.elementor-element-c71f642') && document.querySelector('.elementor-element-a3783ba .elementor-button')) {
+        clearInterval(init)
+        document.querySelector('.elementor-element-c71f642').remove()
+        document.querySelector('.elementor-element-a3783ba .elementor-button').classList.add('skewed')
+    }
 });
 
 let linkChange = setInterval(() => {
