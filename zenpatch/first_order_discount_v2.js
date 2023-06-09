8ad4e9f7ccfc06c86501c981de9fd87cff3428bc
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2023-06-10 00:45:36 +0300

    Update first_order_discount_v2.js

--- zenpatch/first_order_discount_v2.js
+++ zenpatch/first_order_discount_v2.js
@@ -999,45 +999,47 @@ body .sidebar .btn_trigger_popup.applied_discount > p {
       }
 
       document.querySelector("#purchase .slide-packs>ul>li.active-slide").setAttribute("test", "test");
-
-      if (localStorage.getItem("appliedDiscount") && !localStorage.getItem("restartFunc")) {
-        document.querySelectorAll("#purchase .slide-packs>ul>li").forEach((el) => {
-          el.addEventListener("click", (e) => {
-            if (e.target.classList.contains("active-slide") && !e.target.getAttribute("test")) {
-              setTimeout(() => {
-                e.target.setAttribute("test", "test");
-              }, 4);
-            }
-            if (e.target.classList.contains("active-slide") && e.target.getAttribute("test")) {
-              document.querySelector("#addToCart").click();
-              e.target.removeAttribute("test");
-            }
-            document.querySelectorAll("#purchase .slide-packs>ul>li").forEach((i) => {
-              if (e.target !== i) {
-                i.removeAttribute("test");
+      onClickPacks();
+      function onClickPacks() {
+        if (localStorage.getItem("appliedDiscount") && !localStorage.getItem("restartFunc")) {
+          document.querySelectorAll("#purchase .slide-packs>ul>li").forEach((el) => {
+            el.addEventListener("click", (e) => {
+              if (e.target.classList.contains("active-slide") && !e.target.getAttribute("test")) {
+                setTimeout(() => {
+                  e.target.setAttribute("test", "test");
+                }, 4);
               }
+              if (e.target.classList.contains("active-slide") && e.target.getAttribute("test")) {
+                document.querySelector("#addToCart").click();
+                e.target.removeAttribute("test");
+              }
+              document.querySelectorAll("#purchase .slide-packs>ul>li").forEach((i) => {
+                if (e.target !== i) {
+                  i.removeAttribute("test");
+                }
+              });
             });
           });
-        });
 
-        document.querySelectorAll(".sidebar .list-packs").forEach((el) => {
-          el.addEventListener("click", (e) => {
-            if (e.target.classList.contains("active-slide") && !e.target.getAttribute("test")) {
-              setTimeout(() => {
-                e.target.setAttribute("test", "test");
-              }, 4);
-            }
-            if (e.target.classList.contains("active-slide") && e.target.getAttribute("test")) {
-              document.querySelector(".button-proceed").click();
-              e.target.removeAttribute("test");
-            }
-            document.querySelectorAll(".sidebar .list-packs").forEach((i) => {
-              if (e.target !== i) {
-                i.removeAttribute("test");
+          document.querySelectorAll(".sidebar .list-packs").forEach((el) => {
+            el.addEventListener("click", (e) => {
+              if (e.target.classList.contains("active-slide") && !e.target.getAttribute("test")) {
+                setTimeout(() => {
+                  e.target.setAttribute("test", "test");
+                }, 4);
               }
+              if (e.target.classList.contains("active-slide") && e.target.getAttribute("test")) {
+                document.querySelector(".button-proceed").click();
+                e.target.removeAttribute("test");
+              }
+              document.querySelectorAll(".sidebar .list-packs").forEach((i) => {
+                if (e.target !== i) {
+                  i.removeAttribute("test");
+                }
+              });
             });
           });
-        });
+        }
       }
 
       function changeVisabilityApplieddiscount() {
@@ -1178,6 +1180,7 @@ body .sidebar .btn_trigger_popup.applied_discount > p {
                   localStorage.setItem("appliedDiscount", "yes");
                   top.fbq("track", "Lead");
                   changeVisabilityApplieddiscount();
+                  onClickPacks();
                 }
               })
               .catch((error) => {
