48c031ee63a83678e3eeddad7faebfaca1573a41
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2023-06-30 16:41:17 +0300

    Update pricing_options.js

--- knineti/pricing_options.js
+++ knineti/pricing_options.js
@@ -949,7 +949,7 @@ if (window.location.pathname === "/mc/") {
       if (window.innerWidth <= 768) {
         document.querySelector("#unlimited_personal_coaching h2").innerHTML = "<b>Limited Time Offer:</b> add Personal Coaching for <b>only</b> <span>$398</span> <b>$90 more!</b>";
       } else {
-        document.querySelector("#unlimited_personal_coaching h2").innerHTML = "<b>Limited Time Offer:</b><br/> add Personal Coaching for <b>only</b> $398 <b>$90 more!</b>";
+        document.querySelector("#unlimited_personal_coaching h2").innerHTML = "<b>Limited Time Offer:</b><br/> add Personal Coaching for <b>only</b> <span>$398</span> <b>$90 more!</b>";
       }
       document.querySelector("#unlimited_personal_coaching .content").insertAdjacentHTML("afterbegin", newHtml);
       if (localStorage.getItem("personalCoaching")) {
