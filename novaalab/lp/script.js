a7d0fe56772f6d438362d8b6f2128aa9093ebee1
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2023-10-13 16:28:26 +0300

    scroll to el position

--- novaalab/lp/script.js
+++ novaalab/lp/script.js
@@ -3121,7 +3121,8 @@ padding: 8px 16px;
           function handleNavbarItemClick(event) {
             event.preventDefault()
 
-            let offsetTop = DEVICE === 'mobile' ? 0 : 74
+            var navbarListHeight = $('.navbar-list').height()
+            let offsetTop = navbarListHeight - (DEVICE === 'mobile' ? 15 : 0)
 
             if ($(this).closest('.navbar-list').length > 0) {
               pushDataLayer('exp_hopg_impr_b_header_menu_item', $(this).find('a').text(), 'Button', 'Header')
@@ -3209,10 +3210,6 @@ padding: 8px 16px;
   }, WAIT_INTERVAL_TIMEOUT)
 
   window._mfq.push(["setVariable", "home_page_improvements", "var1"])
-
-  waitForElement('.even-columns a').then(el => el.addEventListener('click', () => {
-    pushDataLayer('exp_hopg_impr_b_fs_arlt', 'About red light therapy', 'Button', 'First screen')
-  }))
 
   const waitForReviews = setInterval(() => {
     if (document.querySelectorAll('.reviews--plugin a')[1]) {
@@ -3728,7 +3725,8 @@ padding: 8px 16px;
       // Check if the target anchor element exists on the page.
       var $target = $('#' + targetAnchor)
 
-      let offsetTop = DEVICE === 'mobile' ? 0 : 74
+      var navbarListHeight = $('.navbar-list').height()
+      let offsetTop = navbarListHeight - (DEVICE === 'mobile' ? 15 : 0)
 
 
       if ($target.length) {
@@ -3748,8 +3746,8 @@ padding: 8px 16px;
       // Check if the target anchor element exists on the page.
       var $target = $('#' + targetAnchor)
 
-      let offsetTop = DEVICE === 'mobile' ? 0 : 74
-
+      var navbarListHeight = $('.navbar-list').height()
+      let offsetTop = navbarListHeight - (DEVICE === 'mobile' ? 15 : 0)
 
       if ($target.length) {
         $('html, body').animate({
@@ -3758,6 +3756,27 @@ padding: 8px 16px;
       }
 
       pushDataLayer('exp_hopg_impr_b_wwnlhy_cyk', 'Choose your kit 2', 'Button', 'When will Novaa Light help you?')
+    }))
+
+    waitForElement('.even-columns a').then(el => el.addEventListener('click', function (e) {
+      e.preventDefault()
+
+      // Get the 'href' attribute from the clicked element and remove the '#' character to get the target anchor.
+      var targetAnchor = $(this).attr('href').substring(1)
+
+      // Check if the target anchor element exists on the page.
+      var $target = $('#' + targetAnchor)
+
+      var navbarListHeight = $('.navbar-list').height() 
+      let offsetTop = navbarListHeight + (DEVICE === 'mobile' ? 0 : 74)
+
+      if ($target.length) {
+        $('html, body').animate({
+          scrollTop: $target.offset().top - offsetTop
+        }, 1000) // You can adjust the duration (in milliseconds) for smooth scrolling
+      }
+
+      pushDataLayer('exp_hopg_impr_b_fs_arlt', 'About red light therapy', 'Button', 'First screen')
     }))
   }
 
