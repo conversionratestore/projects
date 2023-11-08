0496f0b321b0612f7894eb6940bc96849adc11ae
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2023-11-08 11:44:33 +0200

    Update subscription_exit_popup.js

--- doyogawithme/subscription_exit_popup.js
+++ doyogawithme/subscription_exit_popup.js
@@ -1350,9 +1350,9 @@ margin: 0 0 12px;
           const element = document.querySelector("#edit-sidebar-coupon-redemption-form-apply");
           const events = ["mousedown", "focusin"];
           events.forEach((eventType) => element.dispatchEvent(new MouseEvent(eventType, { bubbles: true })));
-          setTimeout(() => {
-            window.location.reload();
-          }, 100);
+          // setTimeout(() => {
+          //   window.location.reload();
+          // }, 100);
         }
       }
     }, 100);
