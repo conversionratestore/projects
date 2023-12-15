675f834ac5a67025fcb8de574c18463bbaa6ce00
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-12-15 11:01:36 +0200

    fix hotjar id

--- 7879co/exit_popup.js
+++ 7879co/exit_popup.js
@@ -965,7 +965,7 @@ new ExitIntentPopup(device);
 
 (function(h,o,t,j,a,r){
   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-  h._hjSettings={hjid:1350427,hjsv:6};
+  h._hjSettings={hjid:2667925,hjsv:6};
   a=o.getElementsByTagName('head')[0];
   r=o.createElement('script');r.async=1;
   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
