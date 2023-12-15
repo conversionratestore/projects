a6a0481f2020f0c5c15feb42048b346b0f209b3e
Olga1001 <48210341+Olga1001@users.noreply.github.com>
2023-12-15 11:02:45 +0200

    fix

--- 7879co/exit_popup.js
+++ 7879co/exit_popup.js
@@ -971,4 +971,3 @@ new ExitIntentPopup(device);
   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
   a.appendChild(r);
 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-hj('event', 'exit_intent_popup');
