04ebb86b739055ee5a05721b3bf4002b2cef7795
dragonegor <39415597+dragonegor@users.noreply.github.com>
2023-12-19 18:37:51 +0200

    Update exit-intent.js

--- grantme/exit-intent.js
+++ grantme/exit-intent.js
@@ -334,19 +334,6 @@ class ExitIntentPopup {
             width: 100%;
             background: transparent;
           }
-          .popup2_1 input+svg+span {
-            display: none;
-          }
-          .popup2_1 input[value=""]+svg+span {
-            display: block;
-            position: absolute;
-            top: 50%;
-            left: 13px;
-            transform: translateY(-50%);
-            z-index: 5;
-            font-size: 16px;
-            color: #676767;
-          }
           .popup2_1 label svg {
             position: absolute;
             top: 50%;
@@ -365,7 +352,6 @@ class ExitIntentPopup {
         <label>
           <input type="date" value="${this.date.getFullYear()}-${this.date.getMonth() + 1}-${this.date.getDate()}">
           ${svgObj.calendar}
-          <span>Select date...</span>
         </label>
         <label>
           <input type="text" placeholder="Enter your name...">
