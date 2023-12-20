3da09e634379ea642b978f8f9f9d654f74ffc67e
dragonegor <39415597+dragonegor@users.noreply.github.com>
2023-12-20 12:57:38 +0200

    Update change_price.js

--- buzzpatch/buzz/change_price.js
+++ buzzpatch/buzz/change_price.js
@@ -1,62 +1,42 @@
-const targetPage = 'pages/buzzpatch';
-const USA_PRICE = '19.00';
-const AU_PRICE = '29.00';
+const targetPage = 'pages/buzzpatch'
+const USA_PRICE = '19.00'
+const AU_PRICE = '29.00'
 
-const url = location.href;
+const url = location.href
 
-const isTargetPage = url.includes(targetPage);
+const isTargetPage = url.includes(targetPage)
 
 function getCookie(name) {
-  let cookie = {};
+  let cookie = {}
   document.cookie.split(';').forEach(function (el) {
-    let split = el.split('=');
-    cookie[split[0].trim()] = split.slice(1).join('=');
-  });
-  return cookie[name];
+    let split = el.split('=')
+    cookie[split[0].trim()] = split.slice(1).join('=')
+  })
+  return cookie[name]
 }
 
-async function addToCart(items = []) {
-  const reponse = await fetch('https://natpat.com/cart/add.js', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      items,
-    }),
-  });
-  return reponse;
-}
-
-const isUSA = getCookie('localization') === 'US';
-const isAU = getCookie('localization') === 'AU';
+const isUSA = getCookie('localization') === 'US'
+const isAU = getCookie('localization') === 'AU'
 
 if (isTargetPage && (!isUSA || !isAU)) {
-  let price = isUSA ? USA_PRICE : AU_PRICE;
+  let price = isUSA ? USA_PRICE : AU_PRICE
 
-  const packsWrap = document.querySelector('.form-group');
-  const onePack = document.querySelector('#radios-3');
-  const onePackPrice = onePack.nextElementSibling.querySelector('span');
+  const packsWrap = document.querySelector('.form-group')
+  const onePack = document.querySelector('#radios-3')
+  const onePackPrice = onePack.nextElementSibling.querySelector('span')
 
-  const totalPrice = document.querySelector('.js-total');
-  const totalPricePrevContent = totalPrice.innerHTML;
+  const totalPrice = document.querySelector('.js-total')
+  const totalPricePrevContent = totalPrice.innerHTML
 
-  onePackPrice.textContent = `$${price} each`;
+  onePackPrice.textContent = `$${price} each`
+  onePack.value = '42279107952684'
 
-  packsWrap.addEventListener('click', async (event) => {
-    const target = event.target === onePack;
+  packsWrap.addEventListener('click', async event => {
+    const target = event.target === onePack
     if (target) {
-      totalPrice.textContent = `$${price}`;
-      const items = [
-        {
-          id: 42279107952684,
-          quantity: 1,
-        },
-      ];
-      const response = await addToCart(items);
-      console.log(await response.json());
+      totalPrice.textContent = `$${price}`
     } else {
-      totalPrice.innerHTML = totalPricePrevContent;
+      totalPrice.innerHTML = totalPricePrevContent
     }
-  });
+  })
 }
