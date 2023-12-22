32e5610d68462759c97c615a044cd90c9803c324
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2023-12-22 18:47:49 +0200

    Update subscription_exit_popup.js

--- doyogawithme/subscription_exit_popup.js
+++ doyogawithme/subscription_exit_popup.js
@@ -15,16 +15,16 @@ let startFunk = setInterval(() => {
     document.head.appendChild(scriptCustomSlider);
     //cdn flipclock
     const waitJquery = setInterval(() => {
-      if (typeof jQuery !== 'function') return;
+      if (typeof jQuery !== "function") return;
 
-      console.log('fire', jQuery)
+      console.log("fire", jQuery);
 
       let scriptCustomTimer = document.createElement("script");
       scriptCustomTimer.src = "https://cdnjs.cloudflare.com/ajax/libs/flipclock/0.7.0/flipclock.min.js";
       scriptCustomTimer.async = false;
       document.head.appendChild(scriptCustomTimer);
-      clearInterval(waitJquery)
-    }, 100)
+      clearInterval(waitJquery);
+    }, 100);
 
     let scriptCustomTimerStyle = document.createElement("link");
     scriptCustomTimerStyle.href = "https://cdnjs.cloudflare.com/ajax/libs/flipclock/0.7.0/flipclock.css";
@@ -59,6 +59,87 @@ let startFunk = setInterval(() => {
 
     let newStyle = /*html */ `
         <style>
+          .o-page--simpleCard .o-page__mainContent {
+  position: relative;
+  margin-top: 160px !important;
+}
+.o-page--simpleCard .o-page__mainContent > .o-page__preContent {
+  position: absolute;
+  top: -130px;
+}
+#block-samsara-maintenancemessage{
+  border-radius: 12px;
+  background: #fff;
+  box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.15);
+  padding: 12px 2em;
+  width: 100%;
+  margin: 0;
+}
+#block-samsara-maintenancemessage.block-block-contentb5dbe46b-4d13-44e4-81c8-9016e439dca2 > .field__item{
+  position: relative;
+  padding-left: 52px;
+}
+#block-samsara-maintenancemessage.block-block-contentb5dbe46b-4d13-44e4-81c8-9016e439dca2 .messages--status {
+  padding: 0;
+  background: unset;
+  box-shadow: none;
+  border: none;
+  color: #555;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 22px;
+}
+#block-samsara-maintenancemessage.block-block-contentb5dbe46b-4d13-44e4-81c8-9016e439dca2 > .field__item::before {
+  position: absolute;
+  content: "";
+  width: 40px;
+  height: 40px;
+  background: url(https://conversionratestore.github.io/projects/doyogawithme/img/kundalini_yoga_icon.svg) no-repeat;
+  background-size: contain;
+  top: 0;
+  left: 0;
+}
+.new_link_var{
+  color: #027DB8;
+  font-weight: 700;
+}
+.new_link_click_here{
+  max-width: max-content;
+  display: block;
+  color: #017922;
+  font-weight: 700;
+  text-decoration-line: underline;
+  margin-top: 8px;
+}
+.o-page:not(.o-page--simpleCard) #block-samsara-maintenancemessage{
+  margin: 2em auto;
+  width: unset;
+}
+.o-page:not(.o-page--simpleCard) #block-samsara-maintenancemessage{
+  border-radius: 0;
+}
+.o-page:not(.o-page--simpleCard) #block-samsara-maintenancemessage.block-block-contentb5dbe46b-4d13-44e4-81c8-9016e439dca2 > .field__item{
+  min-height: 50px;
+  display: flex;
+  align-items: center;
+}
+.o-page:not(.o-page--simpleCard) #block-samsara-maintenancemessage.block-block-contentb5dbe46b-4d13-44e4-81c8-9016e439dca2 > .field__item::before{
+}
+@media (max-width: 768px) {
+  #block-samsara-maintenancemessage{
+    padding: 1em;
+  }
+  #block-samsara-maintenancemessage.block-block-contentb5dbe46b-4d13-44e4-81c8-9016e439dca2 .messages--status{
+    max-width: 315px;
+  }
+  .o-page--simpleCard .o-page__mainContent > .o-page__preContent {
+    position: absolute;
+    top: -164px;
+  }
+  .o-page--simpleCard .o-page__mainContent {
+    margin-top: 180px !important;
+  }
+}
       #edit-sidebar-order-summary-summary .order-total-line__adjustment--promotion{
         display: none !important;
       }
@@ -1125,6 +1206,15 @@ margin: 0 0 12px;
     document.head.insertAdjacentHTML("beforeend", newStyle);
 
     console.log(`Promote subscription + Exit intent pop-up`);
+
+    let lookForBannerMessage = setInterval(() => {
+      if (document.querySelector("#block-samsara-maintenancemessage") && !document.querySelector(".new_link_var")) {
+        clearInterval(lookForBannerMessage);
+        document.querySelector("#block-samsara-maintenancemessage.block-block-contentb5dbe46b-4d13-44e4-81c8-9016e439dca2 .messages--status").innerHTML = `
+    Unlock the secrets of yoga's benefits with an exclusive <a class="new_link_var" href="https://www.doyogawithme.com/express-checkout/54?coupons=yoga3ny">40% off a 3-month premium</a> membership for new subscribers! <a class="new_link_click_here" href="https://www.doyogawithme.com/2024-ny-challenges?utm_campaign=ny">Click here to learn more.</a>
+    `;
+      }
+    }, 100);
 
     //Hypothesis #6 - Add exit-intent popup with limited-time offer
     // Show exit intent popups for users who are logged in but have not bought a paid plan (free users) on pages:
