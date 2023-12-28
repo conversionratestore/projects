6950262c255ef17c8097b1f0e6d87b33d58fb07e
dragonegor <39415597+dragonegor@users.noreply.github.com>
2023-12-28 14:14:06 +0200

    Create double_tap.js

---
+++ buzzpatch/buzz/double_tap.js
@@ -0,0 +1,63 @@
+console.log('%c EXP: Double tap (DEV: YK)', 'background: #3498eb; color: #fccf3a; font-size: 20px; font-weight: bold;')
+const $$el = selector => document.querySelectorAll(selector)
+const $el = selector => document.querySelector(selector)
+
+const clarityInterval = setInterval(function () {
+  if (typeof clarity == 'function') {
+    clearInterval(clarityInterval)
+    clarity('set', 'double_tap', 'variant_1')
+  }
+}, 1000)
+
+const pushDataLayer = (name, desc, type = '', loc = '') => {
+  window.dataLayer = window.dataLayer || []
+  window.dataLayer.push({
+    event: 'event-to-ga4',
+    event_name: name,
+    event_desc: desc,
+    event_type: type,
+    event_loc: loc
+  })
+  console.log(`Event: ${name} ${desc} ${type} ${loc}`)
+}
+
+class DoubleTap {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    this.doubleTap()
+  }
+
+  doubleTap() {
+    $$el('.js-packs').forEach(pack => {
+      if (pack.querySelector('input').checked) {
+        pack.classList.add('crs-double-tap')
+      } else {
+        pack.classList.remove('crs-double-tap')
+      }
+      pack.addEventListener('click', e => {
+        e.preventDefault()
+        pack.querySelector('input').checked = true
+        let hrefArr = $el('#addToCart').getAttribute('href').split('?')
+        let href = `/cart/${pack.querySelector('input').value}:1${hrefArr[1] ? '?' + hrefArr[1] : ''}`
+        $el('#addToCart').setAttribute('href', href)
+        if (pack.classList.contains('crs-double-tap')) {
+          pushDataLayer(
+            'exp_double_tap_click',
+            `Double tap ${pack.querySelector('label').childNodes[0].textContent.trim()}`,
+            'click',
+            'packs'
+          )
+          window.location.href = href
+        } else {
+          $el('.js-packs.crs-double-tap').classList.remove('crs-double-tap')
+          pack.classList.add('crs-double-tap')
+        }
+      })
+    })
+  }
+}
+
+const doubleTap = new DoubleTap()
