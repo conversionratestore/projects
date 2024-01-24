58abc7cfea04dcb9ca24cdceaaab354cdae21215
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2024-01-24 15:33:15 +0200

    Update exit_intent_pop_up.js

--- simify/exit_intent_pop_up.js
+++ simify/exit_intent_pop_up.js
@@ -155,12 +155,10 @@ const icons = {
       />
     </svg>
   `,
-  copied: /* HTML */ `
+  copied: /* html */ `
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-      <path
-        d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM16.78 9.7L11.11 15.37C10.97 15.51 10.78 15.59 10.58 15.59C10.38 15.59 10.19 15.51 10.05 15.37L7.22 12.54C6.93 12.25 6.93 11.77 7.22 11.48C7.51 11.19 7.99 11.19 8.28 11.48L10.58 13.78L15.72 8.64C16.01 8.35 16.49 8.35 16.78 8.64C17.07 8.93 17.07 9.4 16.78 9.7Z"
-        fill="#FEA900"
-      />
+      <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z" stroke="#FEA900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M7.75 11.9999L10.58 14.8299L16.25 9.16992" stroke="#FEA900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
   `,
   minus: /* HTML */ `
@@ -257,9 +255,16 @@ class IntentPopup {
 
   // IntentPopup
   intentPopupTriggers() {
+    // let o = 0
+    // setInterval(() => {
+    //   if (o < 20) {
+    //     console.log((o += 1), `TIME`)
+    //   }
+    // }, 1000)
     const timer = setTimeout(() => {
       this.showIntentPopup()
       clearTimeout(timer)
+      // console.log(`HELLO POPUP`)
     }, 20000)
 
     if (this.device === 'Mobile') {
@@ -361,7 +366,7 @@ class IntentPopup {
         .discount__code-container svg path {
           transition: all 0.5s ease 0s;
         }
-        .discount__code-container svg:hover path {
+        .discount__code-container:not(.is_active) svg:hover path {
           fill: #fea900;
         }
         .discount__code-container span {
@@ -803,7 +808,13 @@ class IntentPopup {
               'PDP Europe & UK eSIM (50 Countries) Footer'
             )
           }
-          $el('[data-action="add-to-cart"]')?.click()
+          let coverageElem = $el('.Product__InfoWrap .ProductForm')
+
+          if (window.innerWidth > 768) {
+            checkScrollPosition(140, coverageElem)
+          } else {
+            checkScrollPosition(120, coverageElem)
+          }
         }
         e.target.setAttribute('data-test', '1')
         setTimeout(() => {
