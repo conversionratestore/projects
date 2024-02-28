656e4ef60730ea34e76968483c6d442c4bbe2251
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2024-02-28 11:57:50 +0200

    regex

--- gadgetgone/checkout.js
+++ gadgetgone/checkout.js
@@ -1915,9 +1915,9 @@
     const inputs = document.querySelectorAll('.fields-container input:not([type="checkbox"]):not(#billing_address_2)')
     const agreementCheckbox = document.getElementById('text_agreement')
 
-    const phoneRegex = /^[\d\s-.()+]+$/
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    const postcodeRegex = /^\d{5}$/
+    const phoneRegex = new RegExp(/[\s\#0-9_\-\+\/\(\)\.]/g)
+    const emailRegex = new RegExp(/^([a-z\d!#$%&‘*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&‘*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|“((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?“)@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[0-9a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i)
+    const postcodeRegex = new RegExp(/^([0-9]{5})(?:-([0-9]{4}))?$/)
 
     let firstInvalidElement = null
 
