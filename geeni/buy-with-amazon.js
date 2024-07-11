bc9adb4b010b42088dd6c8bb824a880dcf17ac53
Andrii Kachmar <37595625+andriikachmar@users.noreply.github.com>
2024-07-11 11:53:32 +0300

    5000

--- geeni/buy-with-amazon.js
+++ geeni/buy-with-amazon.js
@@ -998,11 +998,11 @@
               p.addEventListener("click", (_) => {
                 _.preventDefault(), d("exp_amazon_pdp_u_buy_with_prime", "Buy with prime", "Button", "Product details"), setTimeout(() => {
                   console.log("click on iframePrimeBtn"), b.click();
-                }, 1e3);
+                }, 5e3);
               }), u.addEventListener("click", (_) => {
                 _.preventDefault(), d("exp_amazon_pdp_s_buy_with_prime", "Buy with prime", "Button", "Sticky button"), setTimeout(() => {
                   console.log("click on iframePrimeBtn"), b.click();
-                }, 1e3);
+                }, 5e3);
               });
             }
           }, 100);
