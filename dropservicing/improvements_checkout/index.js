ee1fdb811829da77640f6bb2fe27b3c300e6ccd6
Oleksandr Sventukh <79708807+OSventukh@users.noreply.github.com>
2024-08-29 08:50:16 +0300

    Update index.js

--- dropservicing/improvements_checkout/index.js
+++ dropservicing/improvements_checkout/index.js
@@ -1352,7 +1352,7 @@
   width: 20px;
   height: 20px;
   z-index: 1;
-  background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none"><g clip-path="url(%23clip0_131_478)"><path d="M8.00004 5.3335V8.00016M8.00004 10.6668H8.00671M14.6667 8.00016C14.6667 11.6821 11.6819 14.6668 8.00004 14.6668C4.31814 14.6668 1.33337 11.6821 1.33337 8.00016C1.33337 4.31826 4.31814 1.3335 8.00004 1.3335C11.6819 1.3335 14.6667 4.31826 14.6667 8.00016Z" stroke="%23F04438" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/></g><defs><clipPath id="clip0_131_478"><rect width="16" height="16" fill="white"/></clipPath></defs></svg>');
+  /* background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none"><g clip-path="url(%23clip0_131_478)"><path d="M8.00004 5.3335V8.00016M8.00004 10.6668H8.00671M14.6667 8.00016C14.6667 11.6821 11.6819 14.6668 8.00004 14.6668C4.31814 14.6668 1.33337 11.6821 1.33337 8.00016C1.33337 4.31826 4.31814 1.3335 8.00004 1.3335C11.6819 1.3335 14.6667 4.31826 14.6667 8.00016Z" stroke="%23F04438" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/></g><defs><clipPath id="clip0_131_478"><rect width="16" height="16" fill="white"/></clipPath></defs></svg>'); */
   background-repeat: no-repeat;
   background-size: contain;
 }
