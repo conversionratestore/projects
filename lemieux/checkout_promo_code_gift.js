eab06e55b437bbf487f9dfdfdea8eb27bf8c6723
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2024-09-11 12:23:28 +0300

    Update checkout_promo_code_gift.js

--- lemieux/checkout_promo_code_gift.js
+++ lemieux/checkout_promo_code_gift.js
@@ -1,27 +1,27 @@
 (function() {
   "use strict";
-  const m = (o, e, t, c = "") => {
+  const m = (o, t, e, c = "") => {
     window.dataLayer = window.dataLayer || [], window.dataLayer.push({
       event: "event-to-ga4",
       event_name: o,
-      event_desc: e,
-      event_type: t,
+      event_desc: t,
+      event_type: e,
       event_loc: c
-    }), console.dir(`Event: ${o} | ${e} | ${t} | ${c}`);
-  }, v = ({ name: o, dev: e }) => {
+    }), console.dir(`Event: ${o} | ${t} | ${e} | ${c}`);
+  }, v = ({ name: o, dev: t }) => {
     console.log(
-      `%c EXP: ${o} (DEV: ${e})`,
+      `%c EXP: ${o} (DEV: ${t})`,
       "background: #3498eb; color: #fccf3a; font-size: 20px; font-weight: bold;"
     );
   }, n = (o) => document.querySelector(o);
   function p(o) {
-    return new Promise((e) => {
+    return new Promise((t) => {
       if (document.querySelector(o))
-        return e(document.querySelector(o));
-      const t = new MutationObserver(() => {
-        document.querySelector(o) && (e(document.querySelector(o)), t.disconnect());
+        return t(document.querySelector(o));
+      const e = new MutationObserver(() => {
+        document.querySelector(o) && (t(document.querySelector(o)), e.disconnect());
       });
-      t.observe(document.documentElement, {
+      e.observe(document.documentElement, {
         childList: !0,
         subtree: !0,
         characterData: !0
@@ -30,12 +30,12 @@
   }
   (function(o) {
     o = o === void 0 ? {} : o;
-    let e, t, c, r, a = (o == null ? void 0 : o.delay) || 50;
+    let t, e, c, r, a = (o == null ? void 0 : o.delay) || 50;
     function s() {
-      e = null, r = 0;
+      t = null, r = 0;
     }
     return s(), function() {
-      return t = window.scrollY, e != null && (r = t - e), e = t, clearTimeout(c), c = setTimeout(s, a), r;
+      return e = window.scrollY, t != null && (r = e - t), t = e, clearTimeout(c), c = setTimeout(s, a), r;
     };
   })();
   const L = `[zippyname=basketTab] coupon-form,
@@ -59,6 +59,7 @@
 [zippyname=basketTab] #mmWrapper {
   padding: 0 24px 0 !important;
   margin-bottom: 1rem;
+  visibility: initial !important;
 }
 [zippyname=basketTab] + div {
   margin-top: 8px !important;
@@ -155,10 +156,10 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
   background: url("https://conversionratestore.github.io/projects/lemieux/img/promo_code_icon.svg") no-repeat;
   background-size: contain;
 }/*# sourceMappingURL=main.css.map */`;
-  (function(o, e, t, c, r, a) {
+  (function(o, t, e, c, r, a) {
     o.hj = o.hj || function() {
       (o.hj.q = o.hj.q || []).push(arguments);
-    }, o._hjSettings = { hjid: 2667925, hjsv: 6 }, r = e.getElementsByTagName("head")[0], a = e.createElement("script"), a.async = !0, a.src = t + o._hjSettings.hjid + c + o._hjSettings.hjsv, r && r.appendChild(a);
+    }, o._hjSettings = { hjid: 2667925, hjsv: 6 }, r = t.getElementsByTagName("head")[0], a = t.createElement("script"), a.async = !0, a.src = e + o._hjSettings.hjid + c + o._hjSettings.hjsv, r && r.appendChild(a);
   })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv="), window.hj("event", "exp_cart_page_step");
   class S {
     constructor() {
@@ -171,8 +172,8 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
       }), n(".crs_style") || document.head.insertAdjacentHTML("beforeend", `<style class="crs_style">${L}</style>`), this.initAllFunc();
     }
     initAllFunc() {
-      this.checkPage() === "checkout" && (n('[zippyname="basketTab"] #mmWrapper > div') || p("#mmWrapper > div").then((e) => {
-        p('[zippyname="basketTab"]').then((t) => {
+      this.checkPage() === "checkout" && (n('[zippyname="basketTab"] #mmWrapper > div') || p("#mmWrapper > div").then((t) => {
+        p('[zippyname="basketTab"]').then((e) => {
           p("mention-me-wrapper").then((c) => {
             console.log("mmWrapper>>>>"), this.toggleElementBetweenContainers(
               "#mmWrapper",
@@ -181,8 +182,8 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
             );
           });
         });
-      }), n('[zippyname="basketTab"] coupon-form') || p("coupon-form").then((e) => {
-        p('[zippyname="basketTab"]').then((t) => {
+      }), n('[zippyname="basketTab"] coupon-form') || p("coupon-form").then((t) => {
+        p('[zippyname="basketTab"]').then((e) => {
           p("mention-me-wrapper").then((c) => {
             console.log("coupon>>>>"), this.toggleElementBetweenContainers(
               "coupon-form",
@@ -191,8 +192,8 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
             );
           });
         });
-      }), n('[zippyname="basketTab"] giftcards-form') || p("giftcards-form").then((e) => {
-        p('[zippyname="basketTab"]').then((t) => {
+      }), n('[zippyname="basketTab"] giftcards-form') || p("giftcards-form").then((t) => {
+        p('[zippyname="basketTab"]').then((e) => {
           console.log("giftcards>>>>"), this.toggleElementBetweenContainers(
             "giftcards-form",
             "#checkout-step-payment .bg-col-w:nth-child(1)",
@@ -201,8 +202,8 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
         });
       }));
     }
-    toggleElementBetweenContainers(e, t, c) {
-      const r = n(e), a = n(t), s = n(c), u = n('[zippyname="basketTab"] h4'), h = n("#checkout-step-payment .bg-col-w.p-a-6 + div.bg-col-w.p-a-6");
+    toggleElementBetweenContainers(t, e, c) {
+      const r = n(t), a = n(e), s = n(c), u = n('[zippyname="basketTab"] h4'), h = n("#checkout-step-payment .bg-col-w.p-a-6 + div.bg-col-w.p-a-6");
       if (!r || !a || !s) {
         console.log(r, a, s), console.error("Element or containers not found");
         return;
@@ -271,9 +272,9 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
         n("giftcards-form [zippyclass='is-open']").removeEventListener("click", _), n("giftcards-form input").removeEventListener("change", x), n("giftcards-form action.button").removeEventListener("click", C), n("giftcards-form action.button-1").removeEventListener("click", E);
       }
       function T(i) {
-        i.contains(r) || (e === "coupon-form" ? i.insertAdjacentElement("afterend", r) : i.insertAdjacentElement("beforeend", r), i === s ? (e === "#mmWrapper" && g(), e === "coupon-form" && j(), e === "giftcards-form" && I()) : (e === "#mmWrapper" && P(), e === "coupon-form" && H(), e === "giftcards-form" && W()));
+        i.contains(r) || (t === "coupon-form" ? i.insertAdjacentElement("afterend", r) : i.insertAdjacentElement("beforeend", r), i === s ? (t === "#mmWrapper" && g(), t === "coupon-form" && j(), t === "giftcards-form" && I()) : (t === "#mmWrapper" && P(), t === "coupon-form" && H(), t === "giftcards-form" && W()));
       }
-      console.log("toggleElementBetweenContainers", e);
+      console.log("toggleElementBetweenContainers", t);
       const B = new IntersectionObserver((i) => {
         i.forEach((d) => {
           d.isIntersecting && (d.target === h ? (console.log("container1"), T(a)) : d.target === u && (console.log("container3"), T(s)));
@@ -282,24 +283,24 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
       B.observe(h), B.observe(u);
     }
     observePageChange() {
-      this.observerNew = new MutationObserver((t) => {
-        t.forEach((c) => {
+      this.observerNew = new MutationObserver((e) => {
+        e.forEach((c) => {
           window.location.pathname !== this.lastPath && (this.initAllFunc(), this.lastPath = window.location.pathname);
         });
       });
-      const e = { childList: !0, subtree: !0 };
-      this.observerNew.observe(document.body, e);
+      const t = { childList: !0, subtree: !0 };
+      this.observerNew.observe(document.body, t);
     }
     checkPage() {
-      const e = window.location.href;
+      const t = window.location.href;
       switch (!0) {
         case n("product-view-layout") !== null:
           return "product";
         case n("category-view-layout") !== null:
           return "listing";
-        case e.includes("/basket"):
+        case t.includes("/basket"):
           return "basket";
-        case e.includes("/checkout"):
+        case t.includes("/checkout"):
           return "checkout";
         default:
           return "other";
@@ -309,8 +310,8 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
   new S();
   class F {
     constructor() {
-      var e, t;
-      this.observer = null, this.countryCode = (t = (e = window == null ? void 0 : window.autoInitData) == null ? void 0 : e.website) == null ? void 0 : t.websiteCode, this.basketButtonHandler = () => {
+      var t, e;
+      this.observer = null, this.countryCode = (e = (t = window == null ? void 0 : window.autoInitData) == null ? void 0 : t.website) == null ? void 0 : e.websiteCode, this.basketButtonHandler = () => {
         const c = () => {
           this.country !== "other" && this.device === "desktop" && !location.href.includes("basket") && !location.href.includes("checkout") && this.waitForElementCustom('minibasket a[href*="/basket"]', (r) => {
             var u;
@@ -334,12 +335,12 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
     }
     updateDevice() {
       window.addEventListener("resize", () => {
-        const e = this.device;
-        this.device = window.innerWidth > 1024 ? "desktop" : "mobile", e !== this.device && this.init();
+        const t = this.device;
+        this.device = window.innerWidth > 1024 ? "desktop" : "mobile", t !== this.device && this.init();
       });
     }
     checkBasketPage() {
-      function e() {
+      function t() {
         const c = setInterval(function() {
           document.querySelectorAll("button").forEach((a) => {
             var s;
@@ -355,27 +356,27 @@ checkout-form coupon-form form [zippyclass=is-open]::before {
           });
         }, 100);
       }
-      function t() {
-        location.href.includes("basket") && e();
+      function e() {
+        location.href.includes("basket") && t();
       }
-      this.pageChangeHandler(t), t();
+      this.pageChangeHandler(e), e();
     }
-    pageChangeHandler(e) {
-      (function(t) {
-        const c = t.pushState, r = t.replaceState;
-        t.pushState = function(a) {
-          c.apply(t, arguments), e();
-        }, t.replaceState = function(a) {
-          r.apply(t, arguments), e();
+    pageChangeHandler(t) {
+      (function(e) {
+        const c = e.pushState, r = e.replaceState;
+        e.pushState = function(a) {
+          c.apply(e, arguments), t();
+        }, e.replaceState = function(a) {
+          r.apply(e, arguments), t();
         }, window.addEventListener("popstate", function(a) {
-          e();
+          t();
         });
       })(window.history);
     }
-    waitForElementCustom(e, t) {
+    waitForElementCustom(t, e) {
       this.observer && this.observer.disconnect(), this.observer = new MutationObserver((c, r) => {
-        const a = document.querySelector(e);
-        a && t(a);
+        const a = document.querySelector(t);
+        a && e(a);
       }), this.observer.observe(document.body, {
         childList: !0,
         subtree: !0,
