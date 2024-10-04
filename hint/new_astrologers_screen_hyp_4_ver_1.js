df50ab4553b8c0bacb98a4c3d91a9a1dc161dd4b
Svetlana Khvastova <67105641+SvetlanaKhvastova@users.noreply.github.com>
2024-10-04 17:24:24 +0300

    Update new_astrologers_screen_hyp_4_ver_1.js

--- hint/new_astrologers_screen_hyp_4_ver_1.js
+++ hint/new_astrologers_screen_hyp_4_ver_1.js
@@ -18,7 +18,7 @@
       typeof window.clarity == "function" && (clearInterval(n), window.clarity("set", t, e), console.log("set", t, e));
     }, 1e3);
   };
-  function l(t) {
+  function a(t) {
     return new Promise((e) => {
       if (document.querySelector(t))
         return e(document.querySelector(t));
@@ -34,12 +34,12 @@
   }
   (function(t) {
     t = t === void 0 ? {} : t;
-    let e, n, o, r, i = (t == null ? void 0 : t.delay) || 50;
-    function a() {
-      e = null, r = 0;
+    let e, n, o, i, r = (t == null ? void 0 : t.delay) || 50;
+    function l() {
+      e = null, i = 0;
     }
-    return a(), function() {
-      return n = window.scrollY, e != null && (r = n - e), e = n, clearTimeout(o), o = setTimeout(a, i), r;
+    return l(), function() {
+      return n = window.scrollY, e != null && (i = n - e), e = n, clearTimeout(o), o = setTimeout(l, r), i;
     };
   })();
   const p = "https://conversionratestore.github.io/projects/hint/img/", u = {
@@ -51,49 +51,47 @@
     </svg>
   `
     )
-  }, b = (
+  }, h = (
     /* HTML */
     `
   <div class="new_astrologers_screen">
     <h2 class="main_title">
-      <span class="accent_color_blue">Hint</span> partners with <span class="accent_color_blue">80+</span><br />
-      world-renowned astrologers <br />
-      and palm readers!
+      <span class="accent_color_blue">Hint</span> is co-created and operated with
+      <span class="accent_color_blue">80+</span> world-renowned astrologists and palm readers!
     </h2>
     <div class="astrologers_info_wrapper">
       <ul class="astrologers_list">
-        <li class="astrologer_item best_for_you">
-          <div class="img_wrapper">
-            <img src="${p}astrologer_jane.png" alt="astrologer photo" />
-          </div>
-          <div>
-            <h3 class="astrologer_name">Jane Watson</h3>
-            <p class="astrologer_description">6 years in palmistry readings and spiritual guidance.</p>
-          </div>
-        </li>
         <li class="astrologer_item">
           <div class="img_wrapper">
-            <img src="${p}astrologer_akho.png" alt="astrologer photo" />
+            <img src="${p}astrologer_akho_1.png" alt="astrologer photo" />
           </div>
           <div>
             <h3 class="astrologer_name">Akho</h3>
             <p class="astrologer_description">6 years in palmistry readings and spiritual guidance.</p>
           </div>
         </li>
+        <li class="astrologer_item best_for_you">
+          <div class="img_wrapper">
+            <img src="${p}astrologer_jane_1.png" alt="astrologer photo" />
+          </div>
+          <div>
+            <h3 class="astrologer_name">Jane Watson</h3>
+            <p class="astrologer_description">8 years in astrology and palmistry</p>
+          </div>
+        </li>
         <li class="astrologer_item decoration">
           <div class="img_wrapper">
-            <img src="${p}astrologer_kate.png" alt="astrologer photo" />
+            <img src="${p}astrologer_kate_1.png" alt="astrologer photo" />
           </div>
           <div>
             <h3 class="astrologer_name">Kate Smith</h3>
             <p class="astrologer_description">7 years in palmistry</p>
           </div>
         </li>
-        <li class="astrologer_item no_content"></li>
-        <li class="astrologer_item no_content_second"></li>
       </ul>
       <p class="txt_other_astrologers">
-        And other <span class="accent_color_blue">80+</span> astrologists and palm readers
+        And other <span class="accent_color_blue">80+</span> astrologists <br />
+        and palm readers
       </p>
     </div>
     <div class="new_sticky_block">
@@ -102,7 +100,14 @@
     </div>
   </div>
 `
-  ), h = `.subscription_plan_page .new_astrologers_screen + main {
+  ), b = `.subscription_plan_page header {
+  position: relative;
+  z-index: 0;
+}
+.subscription_plan_page header > button {
+  z-index: 1000000000000000000000000000000000000000000000000000;
+}
+.subscription_plan_page .new_astrologers_screen + main {
   opacity: 0;
 }
 .subscription_plan_page .new_astrologers_screen + main.is_hidden {
@@ -119,14 +124,15 @@
   padding: 20px 20px 140px;
 }
 .subscription_plan_page .new_astrologers_screen .accent_color_blue {
-  color: #066fde;
+  color: #8a2be2;
 }
 .subscription_plan_page .new_astrologers_screen .main_title {
   color: #121620;
   font-size: 24px;
   font-weight: 700;
   line-height: 32px;
-  margin: 0 0 16px;
+  margin: 0 0 24px;
+  max-width: 335px;
 }
 @media (max-width: 362px) {
   .subscription_plan_page .new_astrologers_screen .main_title {
@@ -146,11 +152,12 @@
 .subscription_plan_page .new_astrologers_screen .astrologers_info_wrapper {
   padding: 20px;
   border-radius: 12px;
-  background: #eff2fd;
+  background: #faf5ff;
 }
 .subscription_plan_page .new_astrologers_screen .astrologers_info_wrapper .txt_other_astrologers {
-  max-width: 240px;
-  margin: 12px auto 0;
+  padding-top: 16px;
+  margin: 16px auto 0;
+  border-top: 1px solid #ebecf0;
   color: #121620;
   text-align: center;
   font-size: 18px;
@@ -160,54 +167,17 @@
 .subscription_plan_page .new_astrologers_screen .astrologers_list {
   display: flex;
   flex-direction: column;
-  gap: 12px;
+  gap: 10px;
 }
 .subscription_plan_page .new_astrologers_screen .astrologers_list .astrologer_item {
   display: flex;
   align-items: center;
   gap: 16px;
-  padding: 8px;
-  border-radius: 12px;
-  border: 2px solid #c2cad8;
-  background: #dee5f9;
   position: relative;
   z-index: 2;
 }
-.subscription_plan_page .new_astrologers_screen .astrologers_list .astrologer_item.best_for_you {
-  position: relative;
-  border: 2px solid #04a777;
-}
-.subscription_plan_page .new_astrologers_screen .astrologers_list .astrologer_item.best_for_you::after {
-  content: "Best for you";
-  position: absolute;
-  top: -0.5px;
-  right: -0.4px;
-  padding: 0px 6px 3px;
-  border-radius: 0px 12px;
-  background: #04a777;
-  color: #fff;
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 16px;
-  letter-spacing: 0.01px;
-}
-.subscription_plan_page .new_astrologers_screen .astrologers_list .astrologer_item.no_content {
-  width: 95%;
-  height: 80px;
-  border-radius: 12px;
-  border: 2px solid #c2cad8;
-  background: #dee5f9;
-  margin: -80px auto 0;
-  z-index: 1;
-}
-.subscription_plan_page .new_astrologers_screen .astrologers_list .astrologer_item.no_content_second {
-  width: 90%;
-  height: 80px;
-  border-radius: 12px;
-  border: 2px solid #c2cad8;
-  background: #dee5f9;
-  margin: -80px auto 0;
-  z-index: 0;
+.subscription_plan_page .new_astrologers_screen .astrologers_list .astrologer_item:nth-child(2) .astrologer_description {
+  max-width: 100%;
 }
 .subscription_plan_page .new_astrologers_screen .astrologers_list .img_wrapper {
   width: 68px;
@@ -310,7 +280,7 @@
   border: 2px solid #858da5;
 }
 .subscription_plan_page .new_astrologers_screen .new_sticky_block {
-  border-top: 1px solid #dee5f9;
+  border-top: 1px solid #ebecf0;
   background: #fff;
   position: fixed;
   bottom: 0;
@@ -389,60 +359,60 @@
       s(".crs_inter_astrologers") || document.head.insertAdjacentHTML(
         "afterbegin",
         '<link class="crs_inter_astrologers" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">'
-      ), s(".crs_style_astrologers") || document.head.insertAdjacentHTML("beforeend", `<style class="crs_style_astrologers">${h}</style>`);
+      ), s(".crs_style_astrologers") || document.head.insertAdjacentHTML("beforeend", `<style class="crs_style_astrologers">${b}</style>`);
     }
     // allFunctions
     allFunctionsInitHandler() {
-      var n, o, r;
+      var n, o, i;
       const e = s("body");
       if (this.checkPage() === "paywall")
         (n = s(".new_btn_back")) == null || n.remove(), e.classList.contains("subscription_plan_page") && e.classList.remove("subscription_plan_page");
       else if (this.checkPage() === "subscriptionPlan") {
-        console.log("subscriptionPlan"), e.classList.add("subscription_plan_page");
-        let i = setInterval(() => {
-          s(".subscription_plan_page main") && (clearInterval(i), s(".subscription_plan_page main").classList.add("is_hidden"));
+        console.log("subscriptionPlan"), e.classList.contains("subscription_plan_page") || e.classList.add("subscription_plan_page");
+        let r = setInterval(() => {
+          s(".subscription_plan_page main") && (clearInterval(r), s(".subscription_plan_page main").classList.add("is_hidden"));
         }, 300);
         if (localStorage.getItem("newAstrologersScreen") || (s("main") && (s("main").style.display = "none"), this.renderNewAstrologersScreenSection()), localStorage.getItem("newAstrologersScreen")) {
           this.renderNewBtnBack();
-          let a = setInterval(() => {
-            s(".subscription_plan_page main.is_hidden") && (clearInterval(a), s(".subscription_plan_page main").classList.remove("is_hidden"));
+          let l = setInterval(() => {
+            s(".subscription_plan_page main.is_hidden") && (clearInterval(l), s(".subscription_plan_page main").classList.remove("is_hidden"));
           }, 300);
         }
       } else
-        console.log(this.checkPage()), (o = s(".new_btn_back")) == null || o.remove(), (r = s(".new_astrologers_screen")) == null || r.remove(), e.classList.contains("subscription_plan_page") && e.classList.remove("subscription_plan_page");
+        console.log(this.checkPage()), (o = s(".new_btn_back")) == null || o.remove(), (i = s(".new_astrologers_screen")) == null || i.remove(), e.classList.contains("subscription_plan_page") && (console.log("        body.classList.remove('subscription_plan_page')"), e.classList.remove("subscription_plan_page"));
     }
     // subscription-plan
     renderNewAstrologersScreenSection() {
-      l("main").then((e) => {
+      a("main").then((e) => {
         const n = e;
-        s(".new_astrologers_screen") || n.insertAdjacentHTML("beforebegin", b);
-      }), this.clickNewContinueBtnHandler(), this.clickRadioBtnsHandler(), l(".new_astrologers_screen").then((e) => {
+        s(".new_astrologers_screen") || n.insertAdjacentHTML("beforebegin", h);
+      }), this.clickNewContinueBtnHandler(), this.clickRadioBtnsHandler(), a(".new_astrologers_screen").then((e) => {
         c("exp_new_astro_visibility_01", "Screen view", "View", "palm readers");
       });
     }
     clickNewContinueBtnHandler() {
-      l("[data-nextBtn]").then((e) => {
+      a("[data-nextBtn]").then((e) => {
         const n = _("[data-nextBtn]"), o = s(".subscription_plan_page main");
-        n.forEach((r) => {
-          r.addEventListener("click", () => {
-            var i;
-            c("exp_new_astro_button_01", "Continue", "Click", "palm readers"), o.style.display = "block", o.classList.contains("is_hidden") && o.classList.remove("is_hidden"), (i = s(".new_astrologers_screen")) == null || i.remove(), localStorage.setItem("newAstrologersScreen", "yes"), this.renderNewBtnBack();
+        n.forEach((i) => {
+          i.addEventListener("click", () => {
+            var r;
+            c("exp_new_astro_button_01", "Continue", "Click", "palm readers"), o.style.display = "block", o.classList.contains("is_hidden") && o.classList.remove("is_hidden"), (r = s(".new_astrologers_screen")) == null || r.remove(), localStorage.setItem("newAstrologersScreen", "yes"), this.renderNewBtnBack();
           });
         });
       });
     }
     clickRadioBtnsHandler() {
-      l(".radio_item label").then((e) => {
+      a(".radio_item label").then((e) => {
         _(".radio_item label").forEach((o) => {
-          o.addEventListener("click", (r) => {
-            var i, a;
-            this.storage = r.currentTarget.previousElementSibling.getAttribute("value").trim(), localStorage.setItem("newAstrologerValue", this.storage), c("exp_new_astro_button_06", this.storage, "Click", "palm readers"), (i = s(".new_continue_btn")) != null && i.classList.contains("is_disabled") && ((a = s(".new_continue_btn")) == null || a.classList.remove("is_disabled"));
+          o.addEventListener("click", (i) => {
+            var r, l;
+            this.storage = i.currentTarget.previousElementSibling.getAttribute("value").trim(), localStorage.setItem("newAstrologerValue", this.storage), c("exp_new_astro_button_06", this.storage, "Click", "palm readers"), (r = s(".new_continue_btn")) != null && r.classList.contains("is_disabled") && ((l = s(".new_continue_btn")) == null || l.classList.remove("is_disabled"));
           });
         });
       });
     }
     renderNewBtnBack() {
-      l("header button").then((e) => {
+      a("header button").then((e) => {
         const n = e;
         setTimeout(() => {
           s(".new_btn_back") || n.insertAdjacentHTML("beforebegin", `<div class="new_btn_back">${u.arrowLeftHeaderIcon}</div>`), this.clickNewBtnBackHandler();
@@ -450,7 +420,7 @@
       });
     }
     clickNewBtnBackHandler() {
-      l(".new_btn_back").then((e) => {
+      a(".new_btn_back").then((e) => {
         const n = e, o = s(".subscription_plan_page main");
         n.addEventListener("click", () => {
           c("exp_new_astro_button_02", "Back", "Click", "palm readers"), localStorage.getItem("newAstrologersScreen") && localStorage.removeItem("newAstrologersScreen"), o.style.display = "none", o.classList.add("is_hidden"), this.renderNewAstrologersScreenSection(), n.remove();
@@ -478,13 +448,15 @@
     observeMain() {
       new MutationObserver((n) => {
         for (let o of n) {
-          window.location.pathname !== this.lastPath && (this.lastPath = window.location.pathname, this.checkPage() === "paywall" && this.allFunctionsInitHandler(), this.checkPage() === "subscriptionPlan" && !s("body").classList.contains("subscription_plan_page") && this.allFunctionsInitHandler());
-          for (let r of o.removedNodes)
-            r instanceof HTMLElement && r.tagName === "MAIN" && this.checkPage() === "email" && this.allFunctionsInitHandler();
+          window.location.pathname !== this.lastPath && (this.lastPath = window.location.pathname, this.checkPage() === "paywall" && this.allFunctionsInitHandler(), this.checkPage() === "subscriptionPlan" && !s("body").classList.contains("subscription_plan_page") && (console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>"), this.allFunctionsInitHandler()));
+          for (let i of o.removedNodes)
+            i instanceof HTMLElement && i.tagName === "MAIN" && this.checkPage() === "email" && (console.log(i, "NODE!!!!!!!!!!!!!!!!!!!!"), this.allFunctionsInitHandler());
         }
       }).observe(s("body"), { childList: !0, subtree: !0 });
     }
   }
-  new m(w);
+  a("body").then((t) => {
+    new m(w);
+  });
 })();
 //# sourceMappingURL=index.js.map
