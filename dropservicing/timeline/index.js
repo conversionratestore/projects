504e00cbfe74dcd4de80016242cf2b1be9920a35
Yehor Kofan <39415597+dragonegor@users.noreply.github.com>
2024-10-29 17:18:46 +0200

    Update index.js

--- dropservicing/timeline/index.js
+++ dropservicing/timeline/index.js
@@ -709,7 +709,7 @@
     },
     {
       start: "92",
-      time: "92",
+      time: "108",
       text: "<b>Q&A Session</b> - Addressing all of your common questions"
     }
   ], S = `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
