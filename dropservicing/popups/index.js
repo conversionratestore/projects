1889fd9906dfc00bb9903fbe3e0c5284d55c45ad
Yehor Kofan <39415597+dragonegor@users.noreply.github.com>
2024-11-01 16:37:55 +0200

    Update index.js

--- dropservicing/popups/index.js
+++ dropservicing/popups/index.js
@@ -383,21 +383,20 @@
     "This webinar is only available for a limited time, so watch it while you still can!",
     "You've unlocked an exclusive <b>$2,000 discount</b> on our Drop Servicing Partner Program!"
   ], b = [
-    "IN THE NEXT HOUR, YOU'LL DISCOVER:",
-    "IN THE NEXT 30 MINUTES, YOU'LL DISCOVER:",
+    "IN THE NEXT 15 MINUTES, YOU'LL DISCOVER:",
+    "IN THE NEXT 20 MINUTES, YOU’LL DISCOVER:",
     "In addition, you will receive the following for FREE:",
     "We offer a market-leading guarantee:"
   ], w = [
     [
       "How to create passive, <b>recurring income</b> with Drop Servicing.",
       "<b>Real-life success stories</b> of people who achieved financial freedom with this method.",
-      "<b>Get direct access</b> to Dylan's best freelancer teams and services.",
       "A step-by-step blueprint to <b>start earning $10,000+</b> per month from home."
     ],
     [
-      "Inspiring success stories of <b>people who achieved financial freedom</b> and how you can too.",
-      "<b>Inspiring success stories</b> of individuals who achieved financial freedom through Drop Servicing.",
-      "A detailed blueprint <b>to start making $10,000+</b> per month from home."
+      "How to get <b>your first high value client</b> fast (step-by-step tutorials).",
+      "<b>200+ copy-and-paste templates</b> to build your own Drop Servicing business and achieve financial freedom.",
+      "<b>Get direct access</b> to Dylan's best freelancer teams and services."
     ],
     [
       "<b>Everything Inside My Business</b> including my entire system with all the resources you need to replicate my results.",
@@ -410,7 +409,7 @@
     ]
   ], v = [
     "Stay until the end and receive a special bonus that could change your financial future!",
-    "Watch the entire webinar, because we have a special surprise for you at the end!",
+    "Watch the entire webinar, because we have a <b>special surprise</b> for you at the end!",
     "This offer is time limited so claim it while you can."
   ], c = "https://conversionratestore.github.io/projects/dropservicing", y = `<svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" fill="none">
 <g filter="url(#filter0_d_2047_2074)">
@@ -525,8 +524,8 @@
     }
     async showPopup() {
       const n = localStorage.getItem("popupShown"), e = i("video").elements[0].currentTime;
-      let t = Math.floor(e / (20 * 60));
-      t > 3 && (t = 3), !(n && +n === t) && (i(".crs_popup_wrapper").elements[0].innerHTML = $(t), i(".crs_popup_wrapper").addClass("active"), localStorage.setItem("popupShown", t.toString()), i(".crs_popup_wrapper .crs_close").on("click", () => {
+      let t = e < 25 * 60 ? 0 : e < 50 * 60 ? 1 : e < 70 * 60 ? 2 : 3;
+      n && +n === t || (i(".crs_popup_wrapper").elements[0].innerHTML = $(t), i(".crs_popup_wrapper").addClass("active"), localStorage.setItem("popupShown", t.toString()), i(".crs_popup_wrapper .crs_close").on("click", () => {
         i(".crs_popup_wrapper").removeClass("active"), p(`exp_exit_intent_popup${this.checkPopup()}_close`, "Close", "click", `Popup #${this.checkPopup()}`);
       }), i("button.crs_btn").on("click", () => {
         i(".crs_popup_wrapper").removeClass("active"), p(
