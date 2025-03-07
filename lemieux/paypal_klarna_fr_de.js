98d71bf264ac82af34da52853559f7c72612fac0
Oleksandr Sventukh <79708807+OSventukh@users.noreply.github.com>
2025-03-07 14:26:54 +0200

    Update paypal_klarna_fr_de.js

--- lemieux/paypal_klarna_fr_de.js
+++ lemieux/paypal_klarna_fr_de.js
@@ -1,12 +1,12 @@
 (function() {
   "use strict";
-  const B = ({ name: t, dev: e }) => {
+  const C = ({ name: t, dev: e }) => {
     console.log(
       `%c EXP: ${t} (DEV: ${e})`,
       "background: #3498eb; color: #fccf3a; font-size: 20px; font-weight: bold;"
     );
   };
-  function p(t) {
+  function y(t) {
     return new Promise((e) => {
       if (document.querySelector(t))
         return e(document.querySelector(t));
@@ -21,7 +21,7 @@
     });
   }
 
-  function C(t, e) {
+  function O(t, e) {
     var n = {};
     for (var r in t) Object.prototype.hasOwnProperty.call(t, r) && e.indexOf(r) < 0 && (n[r] = t[r]);
     if (t != null && typeof Object.getOwnPropertySymbols == "function")
@@ -30,11 +30,11 @@
     return n;
   }
   typeof SuppressedError == "function" && SuppressedError;
-  function O(t, e) {
+  function x(t, e) {
     var n = document.querySelector('script[src="'.concat(t, '"]'));
     if (n === null)
       return null;
-    var r = g(t, e), a = n.cloneNode();
+    var r = k(t, e), a = n.cloneNode();
     if (delete a.dataset.uidAuto, Object.keys(a.dataset).length !== Object.keys(r.dataset).length)
       return null;
     var o = !0;
@@ -42,63 +42,63 @@
       a.dataset[c] !== r.dataset[c] && (o = !1);
     }), o ? n : null;
   }
-  function P(t) {
-    var e = t.url, n = t.attributes, r = t.onSuccess, a = t.onError, o = g(e, n);
+  function q(t) {
+    var e = t.url, n = t.attributes, r = t.onSuccess, a = t.onError, o = k(e, n);
     o.onerror = a, o.onload = r, document.head.insertBefore(o, document.head.firstElementChild);
   }
-  function x(t) {
-    var e = t.sdkBaseUrl, n = t.environment, r = C(t, ["sdkBaseUrl", "environment"]), a = e || T(n), o = r, c = Object.keys(o).filter(function(u) {
+  function P(t) {
+    var e = t.sdkBaseUrl, n = t.environment, r = O(t, ["sdkBaseUrl", "environment"]), a = e || D(n), o = r, c = Object.keys(o).filter(function(u) {
       return typeof o[u] < "u" && o[u] !== null && o[u] !== "";
     }).reduce(function(u, s) {
       var l = o[s].toString();
-      return s = q(s), s.substring(0, 4) === "data" || s === "crossorigin" ? u.attributes[s] = l : u.queryParams[s] = l, u;
+      return s = I(s), s.substring(0, 4) === "data" || s === "crossorigin" ? u.attributes[s] = l : u.queryParams[s] = l, u;
     }, {
       queryParams: {},
       attributes: {}
     }), i = c.queryParams, d = c.attributes;
     return i["merchant-id"] && i["merchant-id"].indexOf(",") !== -1 && (d["data-merchant-id"] = i["merchant-id"], i["merchant-id"] = "*"), {
-      url: "".concat(a, "?").concat(I(i)),
+      url: "".concat(a, "?").concat(T(i)),
       attributes: d
     };
   }
-  function q(t) {
+  function I(t) {
     var e = function(n, r) {
       return (r ? "-" : "") + n.toLowerCase();
     };
     return t.replace(/[A-Z]+(?![a-z])|[A-Z]/g, e);
   }
-  function I(t) {
+  function T(t) {
     var e = "";
     return Object.keys(t).forEach(function(n) {
       e.length !== 0 && (e += "&"), e += n + "=" + t[n];
     }), e;
   }
-  function T(t) {
+  function D(t) {
     return t === "sandbox" ? "https://www.sandbox.paypal.com/sdk/js" : "https://www.paypal.com/sdk/js";
   }
-  function g(t, e) {
+  function k(t, e) {
     e === void 0 && (e = {});
     var n = document.createElement("script");
     return n.src = t, Object.keys(e).forEach(function(r) {
       n.setAttribute(r, e[r]), r === "data-csp-nonce" && n.setAttribute("nonce", e["data-csp-nonce"]);
     }), n;
   }
-  function D(t, e) {
-    if (e === void 0 && (e = Promise), S(t, e), typeof document > "u")
+  function L(t, e) {
+    if (e === void 0 && (e = Promise), E(t, e), typeof document > "u")
       return e.resolve(null);
-    var n = x(t), r = n.url, a = n.attributes, o = a["data-namespace"] || "paypal", c = k(o);
-    return a["data-js-sdk-library"] || (a["data-js-sdk-library"] = "paypal-js"), O(r, a) && c ? e.resolve(c) : M({
+    var n = P(t), r = n.url, a = n.attributes, o = a["data-namespace"] || "paypal", c = S(o);
+    return a["data-js-sdk-library"] || (a["data-js-sdk-library"] = "paypal-js"), x(r, a) && c ? e.resolve(c) : M({
       url: r,
       attributes: a
     }, e).then(function() {
-      var i = k(o);
+      var i = S(o);
       if (i)
         return i;
       throw new Error("The window.".concat(o, " global variable is not available."));
     });
   }
   function M(t, e) {
-    e === void 0 && (e = Promise), S(t, e);
+    e === void 0 && (e = Promise), E(t, e);
     var n = t.url, r = t.attributes;
     if (typeof n != "string" || n.length === 0)
       throw new Error("Invalid url.");
@@ -107,7 +107,7 @@
     return new e(function(a, o) {
       if (typeof document > "u")
         return a();
-      P({
+      q({
         url: n,
         attributes: r,
         onSuccess: function() {
@@ -120,10 +120,10 @@
       });
     });
   }
-  function k(t) {
+  function S(t) {
     return window[t];
   }
-  function S(t, e) {
+  function E(t, e) {
     if (typeof t != "object" || t === null)
       throw new Error("Expected an options object.");
     var n = t.environment;
@@ -167,23 +167,31 @@ basket-view paypal-pay-in-three-message {
 #crs-paypal-product {
   max-width: 100%;
   overflow: hidden;
+  min-height: 1.7rem;
 }
-#crs-paypal-basket,
+/* #crs-paypal-basket,
 #crs-paypal-product > div {
   min-width: 387px;
+
+} */
+
+#crs-paypal-product.active { 
+  background-color: #f6f5f5;
+  background-color: var(--atomic-background-color-5, #f6f5f5);
+  padding: 0.5rem;
 }
 `;
-  B({ name: "Klarna & PayPal BNPL", dev: "OS" }), function(t, e, n, r, a, o) {
+  C({ name: "Klarna & PayPal BNPL", dev: "OS" }), function(t, e, n, r, a, o) {
     t.hj = t.hj || function() {
       (t.hj.q = t.hj.q || []).push(arguments);
     }, t._hjSettings = { hjid: 2667925, hjsv: 6 }, a = e.getElementsByTagName("head")[0], o = e.createElement("script"), o.async = !0, o.src = n + t._hjSettings.hjid + r + t._hjSettings.hjsv, a && a.appendChild(o);
   }(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv="), window.hj("event", "exp_klarna_paypal");
-  class L {
+  class H {
     constructor() {
       var e, n;
       this.countryCode = (n = (e = window == null ? void 0 : window.autoInitData) == null ? void 0 : e.website) == null ? void 0 : n.websiteCode, this.basketButtonHandler = () => {
         (() => {
-          !location.href.includes("basket") && !location.href.includes("checkout") && p('minibasket a[href*="/basket"]').then((a) => {
+          !location.href.includes("basket") && !location.href.includes("checkout") && y('minibasket a[href*="/basket"]').then((a) => {
             var d;
             const o = a, c = o.href;
             this.country;
@@ -216,7 +224,7 @@ basket-view paypal-pay-in-three-message {
       let e = window.location.href;
       new MutationObserver((r) => {
         r.forEach((a) => {
-          e !== window.location.href && (window.location.pathname.includes("/basket"), e = window.location.href, this.observers.forEach((o) => o.disconnect()), this.addPaypalMessageBadgeOnBasketPage());
+          e !== window.location.href && (e = window.location.href, this.observers.forEach((o) => o.disconnect()), this.addPaypalMessageBadgeOnBasketPage());
         });
       }).observe(document.body, {
         childList: !0,
@@ -224,53 +232,56 @@ basket-view paypal-pay-in-three-message {
       });
     }
     async addPaypalMessageBadgeOnBasketPage() {
-      var n, r, a, o, c, i, d, u, s, l, E, j;
+      var n, r, a, o, c, i, d, u, s, l, j, B;
       if (window.location.pathname.includes("/basket")) {
-        const b = await p(
+        const b = await y(
           "paypal-pay-in-three-message"
         );
         if (b) {
-          b.insertAdjacentHTML(
+          document.querySelector("#crs-paypal-basket") || b.insertAdjacentHTML(
             "beforebegin",
             /* HTML */
             ` <div id="crs-paypal-basket">
           <div></div>
         </div>`
           );
-          const y = document.querySelector(
+          const v = document.querySelector(
             "#crs-paypal-basket"
-          ), f = (n = document.querySelector("[data-pp-amount]")) == null ? void 0 : n.getAttribute("data-pp-amount"), h = (o = (a = (r = window == null ? void 0 : window.autoInitData) == null ? void 0 : r.website) == null ? void 0 : a.currency) == null ? void 0 : o.base, m = (i = (c = window == null ? void 0 : window.autoInitData) == null ? void 0 : c.website) == null ? void 0 : i.defaultCountry;
+          ), f = (n = document.querySelector("[data-pp-amount]")) == null ? void 0 : n.getAttribute("data-pp-amount"), h = (o = (a = (r = window == null ? void 0 : window.autoInitData) == null ? void 0 : r.website) == null ? void 0 : a.currency) == null ? void 0 : o.base, g = (i = (c = window == null ? void 0 : window.autoInitData) == null ? void 0 : c.website) == null ? void 0 : i.defaultCountry;
           if (!f || !h) return;
-          const v = setInterval(() => {
-            window != null && window.paypal && y && (paypal.Messages({
+          const p = setInterval(() => {
+            window != null && window.paypal && v && (paypal.Messages({
               amount: f,
               currency: h,
-              buyerCountry: m,
+              buyerCountry: g,
               style: {
                 layout: "text"
               }
-            }).render("#crs-paypal-basket div"), clearInterval(v));
+            }).render("#crs-paypal-basket div"), clearInterval(p));
           }, 500);
         }
-      } else if (await p("product-view-add-to-basket")) {
-        const w = (d = document.querySelector("[data-pp-amount]")) == null ? void 0 : d.getAttribute("data-pp-amount"), y = (l = (s = (u = window == null ? void 0 : window.autoInitData) == null ? void 0 : u.website) == null ? void 0 : s.currency) == null ? void 0 : l.base, f = (j = (E = window == null ? void 0 : window.autoInitData) == null ? void 0 : E.website) == null ? void 0 : j.defaultCountry;
-        if (!w || !y) return;
-        const h = (
+      } else if (await y("product-view-add-to-basket")) {
+        const m = (d = document.querySelector("[data-pp-amount]")) == null ? void 0 : d.getAttribute("data-pp-amount"), w = (l = (s = (u = window == null ? void 0 : window.autoInitData) == null ? void 0 : u.website) == null ? void 0 : s.currency) == null ? void 0 : l.base, v = (B = (j = window == null ? void 0 : window.autoInitData) == null ? void 0 : j.website) == null ? void 0 : B.defaultCountry;
+        if (!m || !w) return;
+        const f = (
           /* HTML */
-          ' <div id="crs-paypal-product"><div></div></div>'
-        ), m = document.querySelector(
+          ` <div id="crs-paypal-product">
+          <div></div>
+        </div>`
+        ), h = document.querySelector(
           "paypal-pay-in-three-message"
         );
         setTimeout(() => {
-          document.querySelector("#crs-paypal-product") || m.insertAdjacentHTML("afterend", h);
+          document.querySelector("#crs-paypal-product") || h.insertAdjacentHTML("afterend", f);
         }, 1e3);
-        const v = setInterval(() => {
-          window != null && window.paypal && p("#crs-paypal-product").then(() => {
+        const g = setInterval(() => {
+          window != null && window.paypal && y("#crs-paypal-product").then(() => {
+            var p;
             paypal.Messages({
-              amount: w,
-              currency: y,
-              buyerCountry: f
-            }).render("#crs-paypal-product div"), clearInterval(v);
+              amount: m,
+              currency: w,
+              buyerCountry: v
+            }).render("#crs-paypal-product div"), clearInterval(g), (p = document.querySelector("#crs-paypal-product")) == null || p.classList.add("active");
           });
         }, 500);
       }
@@ -281,7 +292,7 @@ basket-view paypal-pay-in-three-message {
       if (e.includes("/checkout") || e.includes("/basket"))
         return;
       const n = (o = (a = window == null ? void 0 : window.autoInitData) == null ? void 0 : a.website) == null ? void 0 : o.locale, r = (d = (i = (c = window == null ? void 0 : window.autoInitData) == null ? void 0 : c.website) == null ? void 0 : i.currency) == null ? void 0 : d.base;
-      (s = (u = window == null ? void 0 : window.autoInitData) == null ? void 0 : u.website) == null || s.defaultCountry, D({
+      (s = (u = window == null ? void 0 : window.autoInitData) == null ? void 0 : u.website) == null || s.defaultCountry, L({
         clientId: "AaZlrvE0hFLH-X2KV8UzAollYEfKCnbyF0odBirm7WFXRaIoFVr1vgcO-Zqa7XmDMVqATCiOZ-0_3U9B",
         currency: r || "USD",
         locale: n,
@@ -299,5 +310,5 @@ basket-view paypal-pay-in-three-message {
       e.innerHTML = A, document.head.appendChild(e);
     }
   }
-  new L();
+  new H();
 })();
